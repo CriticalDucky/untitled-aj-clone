77bc05af934704b198c96d149cd49c3d82a271ce
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-07-11 22:28:49 -0400

    Add camera + movement

---
+++ src/StarterPlayer/StarterCharacterScripts/Movement.client.lua
@@ -0,0 +1,57 @@
+local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local ContextActionService = game:GetService("ContextActionService")
+
+local player = Players.LocalPlayer
+local character = script.Parent
+local humanoid = character:WaitForChild("Humanoid")
+
+local Fusion = require(ReplicatedStorage:WaitForChild("Fusion"))
+
+local Value = Fusion.Value
+local New = Fusion.New
+local Children = Fusion.Children
+local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
+local OnChange = Fusion.OnChange
+local Observer = Fusion.Observer
+local Tween = Fusion.Tween
+local Spring = Fusion.Spring
+local Hydrate = Fusion.Hydrate
+local unwrap = Fusion.unwrap
+
+local MOVE_ACTION_NAME = "clickToMove"
+
+local movementEnabled = false
+
+local function onClickAction(action, state, input)
+    if action == MOVE_ACTION_NAME then
+        if state == Enum.UserInputState.Begin then
+            print("Begin")
+            movementEnabled = true
+        elseif state == Enum.UserInputState.End then
+            print("End")
+            movementEnabled = false
+        end
+    end
+end
+
+local renderConnection, destroyingConnection do
+    local pathRendering = false
+
+    renderConnection = RunService.RenderStepped:Connect(function(deltaTime)
+        if movementEnabled and not pathRendering then
+            local mouse = player:GetMouse()
+
+            humanoid:MoveTo(mouse.Hit.Position)
+        end
+    end)
+
+    destroyingConnection = character.Destroying:Connect(function()
+        renderConnection:Disconnect()
+        destroyingConnection:Disconnect()
+    end)
+end
+
+ContextActionService:BindAction(MOVE_ACTION_NAME, onClickAction, false, Enum.UserInputType.MouseButton1)
---
+++ src/StarterPlayer/StarterPlayerScripts/Camera.client.lua
@@ -0,0 +1,45 @@
+local RunService = game:GetService("RunService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+
+player.CharacterAdded:Connect(function()
+    character = player.Character
+end)
+
+local Fusion = require(ReplicatedStorage:WaitForChild("Fusion"))
+
+local Value = Fusion.Value
+local New = Fusion.New
+local Children = Fusion.Children
+local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
+local OnChange = Fusion.OnChange
+local Observer = Fusion.Observer
+local Tween = Fusion.Tween
+local Spring = Fusion.Spring
+local Hydrate = Fusion.Hydrate
+local unwrap = Fusion.unwrap
+
+local UNIT_VECTOR = Vector3.new(1, 1.4, 1).Unit
+local DISTANCE = 100
+local FOV = 20
+
+RunService.RenderStepped:Connect(function(deltaTime)
+    local camera = workspace.CurrentCamera
+    if camera then
+        camera.CameraType = Enum.CameraType.Scriptable
+
+        if character and character.Parent then
+            local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+            if humanoidRootPart then
+                local position = humanoidRootPart.Position + UNIT_VECTOR * DISTANCE + Vector3.new(0, 1.5, 0)
+                camera.CFrame = CFrame.lookAt(position, humanoidRootPart.Position)
+                camera.FieldOfView = FOV
+            end
+        end
+    end
+end)
+
