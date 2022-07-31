f9998237e3fbc5d329672390c32b946d2de4d0de
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-07-31 14:46:43 -0400

    Simplify getting serverType to use placeId

--- src/shared/ServerStorage/ServerManagement/LocalServerInfo.lua
+++ src/shared/ServerStorage/ServerManagement/LocalServerInfo.lua
@@ -1,7 +1,4 @@
-local CACHE_UPDATE_INTERVAL = 60
-
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
 local ServerStorage = game:GetService("ServerStorage")
 
 local serverStorageShared = ServerStorage:WaitForChild("Shared")
@@ -9,39 +6,18 @@ local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
 local serverManagement = serverStorageShared:WaitForChild("ServerManagement")
 local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
 
-local ServerData = require(serverManagement:WaitForChild("ServerData"))
 local ServerTypeEnum = require(enumsFolder:WaitForChild("ServerType"))
-
-local privateServerId = game.PrivateServerId
-
-local lastCacheUpdate = 0
-local cachedData
+local Locations = require(serverManagement:WaitForChild("Locations"))
 
 local serverType do
-    if privateServerId == "" then
+    for _, locationInfo in pairs(Locations.info) do
+        if locationInfo.placeId == game.PlaceId then
+            serverType = ServerTypeEnum.location
+        end
+    end
+
+    if not serverType then
         serverType = ServerTypeEnum.routing
-        print("Server type: routing (due to no private server id)")
-    else
-        local serverData = ServerData.get()
-        local worlds = serverData.worlds
-
-        for _, world in ipairs(worlds) do
-            for enum, location in pairs(world.locations) do
-                if location.privateServerId == privateServerId then
-                    serverType = ServerTypeEnum.location
-                    break
-                end
-            end
-
-            if serverType then
-                break
-            end
-        end
-
-        if not serverType then
-            serverType = ServerTypeEnum.routing
-            print("Server type: routing (due to no matching private server id)")
-        end
     end
 end
 
