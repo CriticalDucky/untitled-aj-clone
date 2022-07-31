2acc3d3ade4a7c7dd0466cef385538a9105c8aac
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-07-31 14:44:08 -0400

    Rename "ServerData" to "WorldData"

--- src/shared/ServerStorage/ServerManagement/WorldData.lua
+++ src/shared/ServerStorage/ServerManagement/WorldData.lua
@@ -1,5 +1,4 @@
-local DATASTORE_MAX_RETRIES = 10
-local SERVERDATA_KEY = "serverData"
+local WORLDS_KEY = "worlds"
 
 local DataStoreService = game:GetService("DataStoreService")
 local TeleportService = game:GetService("TeleportService")
@@ -10,61 +9,28 @@ local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
 local serverStorageShared = ServerStorage:WaitForChild("Shared")
 local serverManagement = serverStorageShared:WaitForChild("ServerManagement")
 local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
+local utilityFolder = serverStorageShared:WaitForChild("Utility")
 
 local Locations = require(serverManagement:WaitForChild("Locations"))
 local WorldNames = require(serverManagement:WaitForChild("WorldNames"))
 local FillStatusEnum = require(enumsFolder:WaitForChild("FillStatus"))
+local DataStore = require(utilityFolder:WaitForChild("DataStore"))
+local WorldFillData = require(serverManagement:WaitForChild("WorldFillData"))
+local LocalServerInfo = require(serverManagement:WaitForChild("LocalServerInfo"))
 
-local serversDatastore = DataStoreService:GetDataStore("Servers")
+local worldsDataStore = DataStoreService:GetDataStore("Worlds")
 
-local ServerData = {}
+local WorldData = {}
 
-local function updateAsyncSafe(key, transformFunction)
-    local function try()
-        return pcall(function()
-            return serversDatastore:UpdateAsync(key, transformFunction)
-        end)
-    end
-
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success = try()
-
-        if success then
-            return true
-        end
-    end
-
-    warn("Failed to update data store")
-    return false
+function WorldData.get()
+    return DataStore.safeGet(worldsDataStore, WORLDS_KEY)
 end
 
-local function getAsyncSafe(key)
-    local function try()
-        return pcall(function()
-            return serversDatastore:GetAsync(key)
-        end)
-    end
-
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success, serverData = try()
-
-        if success then
-            return if serverData == nil then {} else serverData
-        end
-    end
-
-    return
+function WorldData.update(transformFunction)
+    return DataStore.safeUpdate(worldsDataStore, WORLDS_KEY, transformFunction)
 end
 
-function ServerData.get()
-    return getAsyncSafe(SERVERDATA_KEY)
-end
-
-function ServerData.update(transformFunction)
-    return updateAsyncSafe(SERVERDATA_KEY, transformFunction)
-end
-
-function ServerData.addWorld()
+function WorldData.addWorld()
     local world do
         world = {
             locations = {},
@@ -77,39 +43,39 @@ function ServerData.addWorld()
             local locationTable = {
                 serverCode = serverCode,
                 privateServerId = privateServerId,
-                fillStatus = FillStatusEnum.notFilled,
             }
 
             world.locations[enum] = locationTable
         end
     end
 
-    return ServerData.update(function(serverData)
-        local worlds = serverData.worlds
+    return WorldData.update(function(worlds)
         local worldIndex = #worlds + 1
 
         world.name = WorldNames[worldIndex] or ("World " .. worldIndex)
         table.insert(worlds, world)
 
-        return serverData
+        return worlds
     end), world
 end
 
-function ServerData.findAvailableWorldAndLocation(forcedLocation)
-    local serverData = ServerData.get()
+function WorldData.findAvailable(forcedLocation)
+    local worlds = WorldData.get()
 
-    if serverData == nil then
+    if worlds == nil then
         warn("No server data found")
         return
     end
 
     local availableWorld, availableLocationEnum do
-        for _, world in ipairs(serverData.worlds) do
+        for worldIndex, world in ipairs(worlds) do
             for _, locationEnum in ipairs(Locations.priority) do
-                local locationData = Locations.info[locationEnum]
-                local location = world.locations[locationEnum]
+                local fillData = WorldFillData.get(worldIndex, locationEnum)
 
-                if location.fillStatus ~= FillStatusEnum.filled and locationData.spawnable and (if forcedLocation then locationEnum == forcedLocation else true) then
+                if
+                    (if forcedLocation then locationEnum == forcedLocation else true) and
+                    (if fillData then (fillData == FillStatusEnum.notFilled) else true)
+                then
                     availableWorld = world
                     availableLocationEnum = locationEnum
 
@@ -123,7 +89,7 @@ function ServerData.findAvailableWorldAndLocation(forcedLocation)
         end
 
         if not availableWorld then
-            local success, newWorld = ServerData.addWorld()
+            local success, newWorld = WorldData.addWorld()
 
             if success then
                 availableWorld = newWorld
@@ -138,4 +104,4 @@ function ServerData.findAvailableWorldAndLocation(forcedLocation)
     return availableWorld, availableLocationEnum
 end
 
-return ServerData
+return WorldData
