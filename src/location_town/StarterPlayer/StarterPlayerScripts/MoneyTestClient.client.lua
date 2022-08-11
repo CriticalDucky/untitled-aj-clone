1b56d024ca44f7c7b2c6e473906e2938d8a700db
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-08-10 22:05:21 -0400

    Test PlayerData

---
+++ src/location_town/StarterPlayer/StarterPlayerScripts/MoneyTestClient.client.lua
@@ -0,0 +1,42 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
+
+local ClientPlayerData = require(replicatedStorageShared:WaitForChild("Data"):WaitForChild("ClientPlayerData"))
+
+local Fusion = require(replicatedStorageShared:WaitForChild("Fusion"))
+local Value = Fusion.Value
+local New = Fusion.New
+local Children = Fusion.Children
+local Computed = Fusion.Computed
+local OnEvent = Fusion.OnEvent
+local OnChange = Fusion.OnChange
+local Observer = Fusion.Observer
+local Tween = Fusion.Tween
+local Spring = Fusion.Spring
+local Hydrate = Fusion.Hydrate
+local unwrap = Fusion.unwrap
+
+local player = Players.LocalPlayer
+local playerGui = player:WaitForChild("PlayerGui")
+local screenGui = playerGui:WaitForChild("ScreenGui")
+
+local playerValue = ClientPlayerData.getDataValue(player, true)
+
+New "TextLabel" {
+    Text = Computed(function()
+        return playerValue:get().currency.money or 0
+    end),
+    Size = UDim2.new(0, 100, 0, 100),
+    Parent = screenGui
+}
+
+New "TextLabel" {
+    Text = Computed(function()
+        return playerValue:get().awards.points or 0
+    end),
+    Size = UDim2.new(0, 100, 0, 100),
+    Position = UDim2.new(0, 0, 0, 100),
+    Parent = screenGui
+}
