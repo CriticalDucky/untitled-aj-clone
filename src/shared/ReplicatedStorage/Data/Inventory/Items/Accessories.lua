e38822062f9d23323bf4a2d589ceec8f441dc812
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-08-24 08:58:58 -0400

    Add price

--- src/shared/ReplicatedStorage/Data/Inventory/Items/Accessories.lua
+++ src/shared/ReplicatedStorage/Data/Inventory/Items/Accessories.lua
@@ -1,7 +1,18 @@
-local AccessoryTypeEnum = require(game.ReplicatedStorage.Shared.Enums.AccessoryType)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local replicatedStorageShared = ReplicatedStorage.Shared
+local enumsFolder = replicatedStorageShared.Enums
+
+local AccessoryTypeEnum = require(enumsFolder.AccessoryType)
 
 return {
-    [AccessoryTypeEnum.hat] = {
+    [AccessoryTypeEnum.hat_var1] = {
         name = "Hat",
-    }
+        price = 30
+    },
+
+    [AccessoryTypeEnum.hat_var2] = {
+        name = "Hat",
+        price = 30
+    },
 }
