8ddfdb98839a079b9cc78e16660e4e9ac30ec90e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-08-30 21:38:57 -0700

    Complete ShopManager prototype

--- src/shared/ServerStorage/Data/Shopping/ShopManager.lua
+++ src/shared/ServerStorage/Data/Shopping/ShopManager.lua
@@ -6,30 +6,57 @@ local dataFolder = serverStorageShared.Data
 local shoppingFolder = dataFolder.Shopping
 local inventoryFolder = dataFolder.Inventory
 local replicatedStorageShared = ReplicatedStorage.Shared
+local replicatedStorageSharedData = replicatedStorageShared.Data
 local enumsFolder = replicatedStorageShared.Enums
 
 local ShopType = require(enumsFolder.ShopType)
-local ShopItemStatus = require(replicatedStorageShared.Data.ShopInfo.ShopItemStatus)
+local ShopItemStatus = require(replicatedStorageSharedData.ShopInfo.ShopItemStatus)
 local ActiveShops = require(shoppingFolder.ActiveShops)
 local Shops = require(shoppingFolder.Shops)
+local PlayerData = require(dataFolder.PlayerData)
+local InventoryManager = require(inventoryFolder.InventoryManager)
+local Items = require(replicatedStorageSharedData.Inventory.Items)
+local Currency = require(dataFolder.Currency.Currency)
 
 local ShopManager = {}
 
-function ShopManager.purchaseItem(player, shop, itemIndex)
-    if not (shop and ShopType[shop] and ActiveShops[shop]) then
-        warn("Shop must be a valid shop type")
-        return
-    end
-    
-    if not (itemIndex and Shops[ActiveShops[shop]][itemIndex]) then
-        warn("Item must be a valid item")
-        return
-    end
-    
-    local isForSale = ShopItemStatus.get(itemIndex)
+function ShopManager.playerCanBuyItem(player, shopEnum, itemIndex)
+    local shopInfoTable = Shops[shopEnum]
+    local shopItem = shopInfoTable.Items[itemIndex]
+    local itemInfo = Items[shopItem.itemType]
 
-    
+    local playerData = PlayerData.get(player)
+
+    if not playerData then
+        return false
+    end
+
+    if not ShopItemStatus.get(shopItem) then
+        return false
+    end
+
+    if not Currency.has(player, itemInfo.currencyType, itemInfo.price) then
+        return false
+    end
+
+    if not InventoryManager.isInventoryFull(player, shopItem.itemType, 1) then
+        return false
+    end
+
+    return true
+end
+
+function ShopManager.purchaseShopItem(player, shopEnum, itemIndex)
+    local shopInfoTable = Shops[shopEnum]
+    local shopItem = shopInfoTable.items[itemIndex]
+
+    local itemInfo = Items[shopItem.itemType]
+
+    if ShopManager.playerCanBuyItem(player, shopEnum, itemIndex) then
+        if InventoryManager.newItemInInventory(shopItem.itemType, shopItem.item, player) then
+            return Currency.increment(player, itemInfo.priceCurrencyType, itemInfo.price)
+        end
+    end
 end
 
 return ShopManager
-
