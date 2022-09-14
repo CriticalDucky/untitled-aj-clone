57edfc43d62f0374c55119adb2c5ff1ddcd7215c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-14 16:13:32 -0400

    Implement ReplicaRequest

--- src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
+++ src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
@@ -8,68 +8,38 @@ local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
 local dataFolder = replicatedStorageShared:WaitForChild("Data")
 local shopInfoFolder = dataFolder:WaitForChild("ShopInfo")
 local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
+local requestsFolder = replicatedStorageShared:WaitForChild("Requests")
 
 local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
 local ActiveShops = require(shopInfoFolder:WaitForChild("ActiveShopsClient"))
 local ShopItemStatus = require(shopInfoFolder:WaitForChild("ShopItemStatus"))
-local ShopType = require(enumsFolder:WaitForChild("ShopType"))
 local PurchaseResponseType = require(enumsFolder:WaitForChild("PurchaseResponseType"))
+local ReplicaRequest = require(requestsFolder:WaitForChild("ReplicaRequest"))
 
 local Purchase = {}
 
 function Purchase.request(shopEnum, itemIndex)
-    print(ActiveShops, ActiveShops[shopEnum], ActiveShops["-100"])
-
     assert(shopEnum and ActiveShops[shopEnum], "shop must be a valid shop type")
-    assert(itemIndex and ActiveShops[shopEnum][itemIndex], "item must be a valid item")
+    assert(itemIndex and ActiveShops[shopEnum].items[itemIndex], "item must be a valid item")
 
     local PurchaseRequest = ReplicaCollection.get("PurchaseRequest")
-    local PurchaseResponse = ReplicaCollection.get("PurchaseResponse")
 
-    if not PurchaseRequest or not PurchaseResponse then
+    if not PurchaseRequest then
+        warn("PurchaseRequest not found")
         return
     end
 
-    local isForSale = ShopItemStatus.get(itemIndex)
+    local item = ActiveShops[shopEnum].items[itemIndex]
+    local isForSale = ShopItemStatus.get(item)
 
     if not isForSale then
+        warn("Item is not for sale")
         return
     end
 
     local requestCode = HttpService:GenerateGUID(false)
 
-    PurchaseRequest:FireServer(shopEnum, itemIndex, requestCode)
-
-    local response
-
-    local connection do
-        connection = PurchaseResponse:ConnectOnClientEvent(function(requestCode, success)
-            if requestCode == requestCode then
-                connection:Disconnect()
-                
-                if success then
-                    response = PurchaseResponseType.success
-                else
-                    response = PurchaseResponseType.failure
-                end
-            end
-        end) 
-    end
-
-    local startTime = time()
-
-    while not response do
-        task.wait()
-
-        if time() - startTime > REQUEST_TIMEOUT then
-            connection:Disconnect()
-            response = PurchaseResponseType.timeout
-
-            break
-        end
-    end
-
-    return response
+    return ReplicaRequest.new(PurchaseRequest, shopEnum, itemIndex, requestCode) or PurchaseResponseType.timeout
 end
 
 return Purchase
