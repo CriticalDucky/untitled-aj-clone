6cab86bc37a3f084df30619b5c5842eca09ed03c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-15 19:24:12 -0400

    Add Event utility

---
+++ src/shared/ReplicatedStorage/Utility/Event.lua
@@ -0,0 +1,53 @@
+-- This script creates a connection object that can be used to connect to an event.
+
+local Connection = {}
+Connection.__index = Connection
+
+function Connection.new(event, callback)
+    local self = setmetatable({}, Connection)
+
+    self._event = event
+    self._callback = callback
+
+    return self
+end
+
+function Connection:Disconnect()
+    self._event:Disconnect(self._callback)
+end
+
+local Event = {}
+Event.__index = Event
+
+function Event.new()
+    local self = setmetatable({}, Event)
+
+    self._connections = {}
+
+    return self
+end
+
+function Event:Connect(callback)
+    local connection = Connection.new(self, callback)
+
+    table.insert(self._connections, connection)
+
+    return connection
+end
+
+function Event:Disconnect(callback)
+    for index, connection in ipairs(self._connections) do
+        if connection._callback == callback then
+            table.remove(self._connections, index)
+            break
+        end
+    end
+end
+
+function Event:Fire(...)
+    for _, connection in ipairs(self._connections) do
+        connection._callback(...)
+    end
+end
+
+return Event
