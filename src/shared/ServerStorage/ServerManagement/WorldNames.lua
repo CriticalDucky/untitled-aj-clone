172d3d729e61b400ed4d8c8d672ed1cdc582a6d0
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-15 19:30:23 -0400

    Add get function

--- src/shared/ServerStorage/ServerManagement/WorldNames.lua
+++ src/shared/ServerStorage/ServerManagement/WorldNames.lua
@@ -1,7 +1,13 @@
-local Names = {
+local names = {
     "name1",
     "name2",
     "name3",
 }
 
-return Names
+local WorldNames = {}
+
+function WorldNames.get(index)
+    return names[index] or ("World " .. index)
+end
+
+return names
