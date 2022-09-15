17b3f160d61ec0de0f53404e5a8b97ef2b6cf30f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-15 19:30:00 -0400

    Rewrite findAvailable to be smarter

--- src/shared/ServerStorage/ServerManagement/WorldData.lua
+++ src/shared/ServerStorage/ServerManagement/WorldData.lua
@@ -1,32 +1,52 @@
 local WORLDS_KEY = "worlds"
+local CACHE_COOLDOWN = 30
 
 local DataStoreService = game:GetService("DataStoreService")
+local RunService = game:GetService("RunService")
 local TeleportService = game:GetService("TeleportService")
 local ServerStorage = game:GetService("ServerStorage")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local serverStorageShared = ServerStorage:WaitForChild("Shared")
-local serverManagement = serverStorageShared:WaitForChild("ServerManagement")
-local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
-local utilityFolder = serverStorageShared:WaitForChild("Utility")
+local replicatedStorageShared = ReplicatedStorage.Shared
+local serverStorageShared = ServerStorage.Shared
+local serverManagement = serverStorageShared.ServerManagement
+local enumsFolder = replicatedStorageShared.Enums
+local utilityFolder = serverStorageShared.Utility
+local replicatedStorageUtility = replicatedStorageShared.Utility
 
-local Locations = require(serverManagement:WaitForChild("Locations"))
-local WorldNames = require(serverManagement:WaitForChild("WorldNames"))
-local FillStatusEnum = require(enumsFolder:WaitForChild("FillStatus"))
-local DataStore = require(utilityFolder:WaitForChild("DataStore"))
-local WorldFillData = require(serverManagement:WaitForChild("WorldFillData"))
-local LocalServerInfo = require(serverManagement:WaitForChild("LocalServerInfo"))
+local Locations = require(serverManagement.Locations)
+local DataStore = require(utilityFolder.DataStore)
+local GameServerData = require(serverManagement.GameServerData)
+local Math = require(replicatedStorageUtility.Math)
+local Table = require(replicatedStorageUtility.Table)
+local Constants = require(serverManagement.Constants)
+local Event = require(replicatedStorageUtility.Event)
 
 local worldsDataStore = DataStoreService:GetDataStore("Worlds")
+local cachedWorlds
+local lastDatastoreRequest = 0
 
 local WorldData = {}
 
-function WorldData.get()
-    return DataStore.safeGet(worldsDataStore, WORLDS_KEY)
+WorldData.WorldsUpdated = Event.new()
+
+local function retrieveDatastore()
+    lastDatastoreRequest = time()
+    cachedWorlds = DataStore.safeGet(worldsDataStore, WORLDS_KEY)
+    WorldData.WorldsUpdated:Fire(cachedWorlds)
+end
+
+function WorldData.get(getUpdated)
+    if getUpdated or not cachedWorlds then
+        retrieveDatastore()
+    end
+
+    return cachedWorlds
 end
 
 function WorldData.update(transformFunction)
+    cachedWorlds = transformFunction(cachedWorlds)
+
     return DataStore.safeUpdate(worldsDataStore, WORLDS_KEY, transformFunction)
 end
 
@@ -34,7 +54,6 @@ function WorldData.addWorld()
     local world do
         world = {
             locations = {},
-            name = "",
         }
 
         for enum, location in pairs(Locations.info) do
@@ -50,9 +69,6 @@ function WorldData.addWorld()
     end
 
     return WorldData.update(function(worlds)
-        local worldIndex = #worlds + 1
-
-        world.name = WorldNames[worldIndex] or ("World " .. worldIndex)
         table.insert(worlds, world)
 
         return worlds
@@ -67,41 +83,102 @@ function WorldData.findAvailable(forcedLocation)
         return
     end
 
-    local availableWorld, availableLocationEnum do
-        for worldIndex, world in ipairs(worlds) do
-            for _, locationEnum in ipairs(Locations.priority) do
-                local fillData = WorldFillData.get(worldIndex, locationEnum)
+    local function newWorldAndLocation()
+        local success, world = WorldData.addWorld()
 
-                if
-                    (if forcedLocation then locationEnum == forcedLocation else true) and
-                    (if fillData then (fillData == FillStatusEnum.notFilled) else true)
-                then
-                    availableWorld = world
-                    availableLocationEnum = locationEnum
-
-                    break
-                end
-            end
-
-            if availableWorld then
-                break
-            end
-        end
-
-        if not availableWorld then
-            local success, newWorld = WorldData.addWorld()
-
-            if success then
-                availableWorld = newWorld
-                availableLocationEnum = Locations.priority[1]
-            else
-                warn("Failed to add world")
-                return
-            end
+        if success then
+            return world, Locations.priority[1]
         end
     end
 
-    return availableWorld, availableLocationEnum
+    local worldIndex do
+        local rarities = {}
+
+        for worldIndex, world in ipairs(worlds) do
+            local population = 0
+            local worldIsSuitable = true
+
+            for locationEnum, _ in pairs(world.locations) do
+                local serverInfo = GameServerData.getLocation(worldIndex, locationEnum)
+
+                if serverInfo then
+                    local locationPopulation = #serverInfo.players
+
+                    if (forcedLocation == locationEnum) and (locationPopulation >= Constants.location_maxPlayers) then
+                        worldIsSuitable = false
+                        break
+                    end
+
+                    population += locationPopulation
+                end
+            end
+
+            if population >= Constants.world_maxRecommendedPlayers or population >= (Table.dictLen(world.locations) * Constants.location_maxRecommendedPlayers) then
+                worldIsSuitable = false
+            end
+
+            if not worldIsSuitable then
+                print("WorldData.findAvailable: World " .. worldIndex .. " is not suitable")
+                continue
+            end
+
+            local chance do
+                if population == 0 then
+                    chance = 0.001
+                else
+                    chance = population
+                end
+            end
+
+            print("WorldData.findAvailable: World " .. worldIndex .. " has a chance of " .. chance)
+
+            rarities[worldIndex] = chance
+        end
+
+        worldIndex = Math.weightedChance(rarities)
+    end
+
+    if worldIndex == nil then
+        print("No suitable world found, creating new world")
+        return newWorldAndLocation()
+    end
+
+    local locationEnum do
+        if not forcedLocation then
+            for _, locationType in pairs(Locations.priority) do
+                local serverInfo = GameServerData.getLocation(worldIndex, locationType)
+
+                if serverInfo then
+                    local locationPopulation = #serverInfo.players
+
+                    if locationPopulation < Constants.location_maxRecommendedPlayers then
+                        locationEnum = locationType
+                        break
+                    end
+                else -- No server info, so location is available
+                    locationEnum = locationType
+                    break
+                end
+            end
+        else
+            locationEnum = forcedLocation
+        end
+    end
+
+    print("Found world", worldIndex, "with location", locationEnum)
+
+    if locationEnum == nil then
+        print("No location found, creating new world")
+        return newWorldAndLocation()
+    end
+
+    return worldIndex, locationEnum
 end
+
+RunService.Heartbeat:Connect(function()
+    if time() - lastDatastoreRequest > CACHE_COOLDOWN then
+        retrieveDatastore()
+    end
+end)
 
 return WorldData
