51e31f6ad800fea69af7ff1169a2080b9cf52420
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-16 17:44:34 -0400

    Add ClientWorldData reciever

---
+++ src/shared/ReplicatedStorage/Server/ClientWorldData.lua
@@ -0,0 +1,19 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
+local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
+local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
+
+local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
+local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
+
+local ClientWorldData = ReplicaCollection.get("Worlds", true)
+
+local worldDataValue = Fusion.Value(ClientWorldData.Data)
+
+ClientWorldData:ListenToRaw(function()
+    worldDataValue:set(ClientWorldData.Data)
+end)
+
+return worldDataValue
