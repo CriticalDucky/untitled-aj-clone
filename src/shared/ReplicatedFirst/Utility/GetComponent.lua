dce9ef5d8a5128d5f4c67afe938ae2a26eeb0c78
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-19 19:41:32 -0400

    Make GetComponent

--- src/shared/ReplicatedFirst/Utility/GetComponent.lua
+++ src/shared/ReplicatedFirst/Utility/GetComponent.lua
@@ -2,8 +2,10 @@ local ReplicatedFirst = game:GetService("ReplicatedFirst")
 
 local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+local componentsFolder = replicatedFirstShared:WaitForChild("UI"):WaitForChild("Components")
 
+local WaitForDescendant = require(utilityFolder:WaitForChild("WaitForDescendant"))
 
 return function(Name)
-    return require(WaitForDescendant(Components, Name))
+    return require(WaitForDescendant(componentsFolder, Name))
 end
