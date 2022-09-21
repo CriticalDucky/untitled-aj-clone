08d7533ec463ed820583ef51ff8df04a485f666f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-09-20 20:28:09 -0400

    Start ClientTeleport

--- src/shared/ReplicatedStorage/Requests/Teleportation/ClientTeleport.lua
+++ src/shared/ReplicatedStorage/Requests/Teleportation/ClientTeleport.lua
@@ -1,25 +1,33 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
+local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
 local requestsFolder = replicatedStorageShared:WaitForChild("Requests")
+local serverFolder = replicatedStorageShared:WaitForChild("Server")
+local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
 
 local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
 local ReplicaRequest = require(requestsFolder:WaitForChild("ReplicaRequest"))
+local ClientWorldData = require(serverFolder:WaitForChild("ClientWorldData"))
+local Table = require(utilityFolder:WaitForChild("Table"))
+local ServerTypeEnum = require(enumsFolder:WaitForChild("ServerType"))
+local TeleportRequestType = require(enumsFolder:WaitForChild("TeleportRequestType"))
+
+local TeleportRequest = ReplicaCollection.get("TeleportRequest")
 
 local Teleport = {}
 
-function Teleport.request(...)
-    local TeleportRequest = ReplicaCollection.get("TeleportRequest")
+function Teleport.request(teleportRequestType, ...)
+    assert(Table.hasValue(TeleportRequestType, teleportRequestType), "Teleport.request() serverType must be a valid ServerTypeEnum value")
 
-    if not TeleportRequest then
-        warn("TeleportRequest not found")
-        return
-    end
-
-    return ReplicaRequest.new(TeleportRequest, ...)
+    return ReplicaRequest.new(TeleportRequest, teleportRequestType, ...)
 end
 
-
+function Teleport.toWorld(worldIndex)
+    return Teleport.request(TeleportRequestType.toWorld, worldIndex)
+end
 
 return Teleport
