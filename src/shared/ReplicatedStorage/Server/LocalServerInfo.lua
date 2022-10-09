fbefc8b0209ca3950f3946a66a8bd713b880e014
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-08 23:31:23 -0400

    Move LocalServerInfo to ReplicatedStorage

--- src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
+++ src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
@@ -1,16 +1,11 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerStorage = game:GetService("ServerStorage")
 
-local serverStorageShared = ServerStorage.Shared
 local replicatedStorageShared = ReplicatedStorage.Shared
-local serverManagement = serverStorageShared.ServerManagement
-local dataFolder = serverStorageShared.Data
 local enumsFolder = replicatedStorageShared.Enums
 
 local ServerTypeEnum = require(enumsFolder.ServerType)
 local Locations = require(replicatedStorageShared.Server.Locations)
 local Homes = require(replicatedStorageShared.Data.Inventory.Items.Homes)
-local ReplicaService = require(dataFolder.ReplicaService)
 
 local serverType do
     for _, locationInfo in pairs(Locations.info) do
@@ -33,11 +28,5 @@ end
 local localServerInfo = {}
 
 localServerInfo.serverType = serverType
-
-ReplicaService.NewReplica({
-    ClassToken = ReplicaService.NewClassToken("ServerInfo"),
-    Data = localServerInfo,
-    Replication = "All",
-})
 
 return localServerInfo
