705d284d331113a568e0e6c16b8e055ebccde0e5
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-11 20:43:00 -0400

    Remove ClientServerInfo replication

--- src/shared/ReplicatedStorage/Server/ClientServerInfo.lua
+++
@@ -1,8 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
-
-local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
-
-return ReplicaCollection.get("ServerInfo", true).Data
