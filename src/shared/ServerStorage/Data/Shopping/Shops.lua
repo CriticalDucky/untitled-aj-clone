702f2d46a35001b0098b5c5de0d75a02d482ce95
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-24 18:35:11 -0400

    Use TimeRanges in Shops

--- src/shared/ServerStorage/Data/Shopping/Shops.lua
+++ src/shared/ServerStorage/Data/Shopping/Shops.lua
@@ -1,13 +1,20 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local replicatedStorageShared = ReplicatedStorage.Shared
+local replicatedFirstShared = ReplicatedFirst.Shared
 local enumsFolder = replicatedStorageShared.Enums
+local utilityFolder = replicatedFirstShared.Utility
+
 
 local ShopTypeEnum = require(enumsFolder.ShopType)
 local ItemType = require(enumsFolder.ItemType)
-
 local AccessoryTypeEnum = require(enumsFolder.AccessoryType)
 local FurnitureTypeEnum = require(enumsFolder.FurnitureType)
 local HomeTypeEnum = require(enumsFolder.HomeType)
+local TimeRange = require(utilityFolder.TimeRange)
+
+local timeRange = TimeRange.new
+local group = TimeRange.newGroup
 
 return {
     [ShopTypeEnum.test1] = {
@@ -16,20 +23,27 @@ return {
             { -- Beach Ball
                 itemType = ItemType.furniture,
                 item = FurnitureTypeEnum.beachBall,
-                sellingTime = {
-                    {
-                        introduction = {
-                            day = 1,
+                sellingTime = group {
+                    timeRange(
+                        {
+                            year = 2020,
                             month = 1,
-                            year = 1970
-                        },
-                        closing = {
                             day = 1,
-                            month = 1,
-                            year = 2025
+                            hour = 0,
+                            min = 0,
+                            sec = 0
                         },
-                    },
-                },
+
+                        {
+                            year = 2025,
+                            month = 1,
+                            day = 1,
+                            hour = 0,
+                            min = 0,
+                            sec = 0
+                        }
+                    )
+                }
             }
         },
     }
