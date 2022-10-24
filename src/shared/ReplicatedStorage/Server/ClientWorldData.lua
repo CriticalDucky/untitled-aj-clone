dd88d8e42485c98562bb7fb2386ea4050c1f72d5
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-24 18:30:32 -0400

    Remove ClientWorldDataHelper and merge

--- src/shared/ReplicatedStorage/Server/ClientWorldData.lua
+++ src/shared/ReplicatedStorage/Server/ClientWorldData.lua
@@ -4,17 +4,48 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
 local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
+local serverFolder = replicatedStorageShared:WaitForChild("Server")
 
 local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
 local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
-local Table = require(replicatedFirstShared:WaitForChild("Utility"):WaitForChild("Table"))  
+local Table = require(replicatedFirstShared:WaitForChild("Utility"):WaitForChild("Table"))
+local Locations = require(serverFolder:WaitForChild("Locations"))
+local Constants = require(serverFolder:WaitForChild("Constants"))
 
-local ClientWorldData = ReplicaCollection.get("Worlds", true)
+local ClientWorldData = {}
 
-local worldDataValue = Fusion.Value(ClientWorldData.Data)
+local replica = ReplicaCollection.get("Worlds", true)
 
-ClientWorldData:ListenToRaw(function()
-    worldDataValue:set(ClientWorldData.Data)
+local worldDataValue = Fusion.Value(replica.Data)
+
+replica:ListenToRaw(function()
+    worldDataValue:set(replica.Data)
 end)
 
-return worldDataValue
+function ClientWorldData:get()
+    return worldDataValue:get()
+end
+
+function ClientWorldData.getWorldPopulation(worldIndex)
+    local worldData = ClientWorldData:get()[worldIndex]
+
+    local population = 0
+
+    for _, locationData in pairs(worldData) do
+        population += if locationData.serverInfo then locationData.serverInfo.players else 0
+    end
+
+    return population
+end
+
+function ClientWorldData.isWorldFull(worldIndex)
+    return ClientWorldData.getWorldPopulation(worldIndex) >= #Locations.priority * Constants.location_maxPlayers
+end
+
+function ClientWorldData.isLocationFull(worldIndex, locationEnum)
+    local locationData = ClientWorldData:get()[worldIndex][locationEnum]
+
+    return locationData.serverInfo and locationData.serverInfo.players >= Constants.location_maxPlayers
+end
+
+return ClientWorldData
