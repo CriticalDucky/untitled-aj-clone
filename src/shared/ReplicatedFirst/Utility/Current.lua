48ab711ebdab2cfe16c45ae5052b9c5989c09342
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-24 18:26:54 -0400

    Add Current data values

---
+++ src/shared/ReplicatedFirst/Utility/Current.lua
@@ -0,0 +1,40 @@
+local ServerUnixTime = require(game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("ServerUnixTime"))
+
+local function date(string)
+    return tonumber(os.date(string, ServerUnixTime.evaluateTime()))
+end
+
+local function currentYear()
+    return date("%Y")
+end
+
+local function currentMonth()
+    return date("%m")
+end
+
+local function currentDay()
+    return date("%d")
+end
+
+local function currentHour()
+    return date("%H")
+end
+
+local function currentMinute()
+    return date("%M")
+end
+
+local function currentSecond()
+    return date("%S")
+end
+
+current = {
+    year = currentYear,
+    month = currentMonth,
+    day = currentDay,
+    hour = currentHour,
+    min = currentMinute,
+    sec = currentSecond,
+}
+
+return current
