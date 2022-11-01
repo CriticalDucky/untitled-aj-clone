7fe8833f2048e017f617ed37990bc62db3332a88
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-31 20:16:48 -0400

    Add ServerGroup enum

---
+++ src/shared/ReplicatedStorage/Enums/ServerGroup.lua
@@ -0,0 +1,10 @@
+return {
+    hasWorldOrigin = 1,
+    isLocation = 2,
+    isParty = 3,
+    isHome = 4,
+    isRouting = 5,
+    hasWorldInfo = 6,
+    needsIdentity = 7,
+    isWorldBased = 8,
+}
