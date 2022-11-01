1e32af425a7b39114f0b811fe4419ea61fc70315
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-31 20:24:30 -0400

    Remove direct ServerTypeEnum usage

--- src/shared/ServerScriptService/Servermanagement/GameServerDataPublisher.server.lua
+++ src/shared/ServerScriptService/Servermanagement/GameServerDataPublisher.server.lua
@@ -10,28 +10,28 @@ local serverManagementShared = serverStorageShared.ServerManagement
 local serverFolder = replicatedStorageShared.Server
 
 local GameServerData = require(serverManagementShared.GameServerData)
-local LocalServerInfo = require(serverFolder.LocalServerInfo)
-local ServerTypeEnum = require(enumsFolder.ServerType)
+local ServerGroupEnum = require(enumsFolder.ServerGroup)
+local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
 local Teleport = require(serverStorageShared.Teleportation.Teleport)
 local Fingerprint = require(serverStorageShared.Utility.Fingerprint)
 
 local indexInfo do
-    if LocalServerInfo.serverType == ServerTypeEnum.routing then
+    if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
         indexInfo = {
             jobId = game.JobId,
         }
-    elseif LocalServerInfo.serverType == ServerTypeEnum.location then
+    elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
         local LocalWorldInfo = require(ServerStorage.Location.ServerManagement.LocalWorldInfo)
 
         indexInfo = {
             worldIndex = LocalWorldInfo.worldIndex,
             locationEnum = LocalWorldInfo.locationEnum,
         }
-    elseif LocalServerInfo.serverType == ServerTypeEnum.home then
+    elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
         indexInfo = {
-            userId = LocalServerInfo.userId,
+
         }
-    elseif LocalServerInfo.serverType == ServerTypeEnum.party then
+    elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
         local LocalPartyInfo = require(ReplicatedStorage.Party.Server.LocalPartyInfo)
 
         indexInfo = {
@@ -39,6 +39,20 @@ local indexInfo do
             privateServerId = game.PrivateServerId
         }
     end
+end
+
+local party_serverCode
+
+if ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
+    party_serverCode = Fingerprint.trace(game.PrivateServerId)
+
+    if not party_serverCode then
+        Teleport.rejoin(Players:GetPlayers())
+
+        Players.PlayerAdded:Connect(function(player)
+            Teleport.rejoin(player) -- Shut down the server.
+        end)
+    end -- Boot players if the server code is not found
 end
 
 local runServiceConnection = RunService.Heartbeat:Connect(function()
@@ -55,31 +69,21 @@ local runServiceConnection = RunService.Heartbeat:Connect(function()
             return userIds
         end
 
-        if LocalServerInfo.serverType == ServerTypeEnum.routing then
+        if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
             serverInfo = {
                 
             }
-        elseif LocalServerInfo.serverType == ServerTypeEnum.location then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
             serverInfo = {
                 
             }
-        elseif LocalServerInfo.serverType == ServerTypeEnum.home then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
             serverInfo = {
                 
             }
-        elseif LocalServerInfo.serverType == ServerTypeEnum.party then
-            local serverCode = Fingerprint.trace(game.PrivateServerId)
-
-            if not serverCode then
-                Teleport.rejoin(Players:GetPlayers())
-
-                Players.PlayerAdded:Connect(function(player)
-                    Teleport.rejoin(player) -- Shut down the server.
-                end)
-            end -- Boot players if the server code is not found
-
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
             serverInfo = {
-                serverCode = serverCode,
+                serverCode = party_serverCode,
             }
         end
 
