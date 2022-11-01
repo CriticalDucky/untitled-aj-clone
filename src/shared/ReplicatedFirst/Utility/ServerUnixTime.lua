3f8d97b6a98c9c4a125aad27bcf5744f3a7c4b56
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-10-31 20:13:31 -0400

    Change ServerUnixTime to be fusion-friendly

--- src/shared/ReplicatedFirst/Utility/ServerUnixTime.lua
+++ src/shared/ReplicatedFirst/Utility/ServerUnixTime.lua
@@ -1,27 +1,30 @@
 local RunService = game:GetService("RunService")
 
+local Fusion = require(game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Fusion"))
+local Value = Fusion.Value
+
 local ServerUnixTime = {}
 
 if RunService:IsClient() then
     local ReplicaCollection = require(game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Replication"):WaitForChild("ReplicaCollection"))
 
-    local timeReplica = ReplicaCollection.get("ServerUnixTime")
+    local timeReplica = ReplicaCollection.get("ServerUnixTime", true)
 
-    local cachedTime = timeReplica.Data.time
+    local cachedTime = Value(timeReplica.Data.time)
 
     timeReplica:ListenToChange({"time"}, function(newTime)
-        cachedTime = newTime
+        cachedTime:set(newTime)
     end)
 
     task.spawn(function()
         while true do
-            cachedTime = cachedTime + 1
+            cachedTime:set(cachedTime:get() + 1)
             task.wait(1)
         end
     end)
 
     local function getUnixTime()
-        return cachedTime
+        return cachedTime:get()
     end
 
     ServerUnixTime.__call = getUnixTime
