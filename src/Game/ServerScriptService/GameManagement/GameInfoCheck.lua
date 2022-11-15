9a1f5d4bf9c80192a28327c838ddbaec4b676d09
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 18:41:02 -0500

    Create rough GameInfoCheck

---
+++ src/Game/ServerScriptService/GameManagement/GameInfoCheck.lua
@@ -0,0 +1,12 @@
+local Players = game:GetService("Players")
+local ServerStorage = game:GetService("ServerStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local serverStorageShared = ServerStorage.Shared
+
+local LocalHomeInfo = require(ReplicatedStorage.Home.Server.LocalHomeInfo)
+local Teleport = require(serverStorageShared.Teleportation.Teleport)
+
+if not LocalHomeInfo.homeOwner then
+    Teleport.bootServer("An internal server error occurred. Please try again later. (err code 2)")
+end
