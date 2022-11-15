9af988b92a3dff2d1ef9db4e7c71d9935506ce97
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 18:49:07 -0500

    Add model recieving

--- src/shared/ReplicatedStorage/Data/Inventory/Items/Furniture.lua
+++ src/shared/ReplicatedStorage/Data/Inventory/Items/Furniture.lua
@@ -1,14 +1,22 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 
 local enumsFolder = ReplicatedStorage.Shared.Enums
 
-local homeItemTypeEnum = require(enumsFolder.FurnitureType)
+local FurnitureType = require(enumsFolder.FurnitureType)
 local CurrencyTypeEnum = require(enumsFolder.CurrencyType)
+local Model = require(ReplicatedFirst.Shared.Utility.Model)
+local ModelType = require(enumsFolder.ModelType)
+
+local function model(name)
+    return Model(ModelType.furniture, name)
+end
 
 return {
-    [homeItemTypeEnum.beachBall] = {
+    [FurnitureType.beachBall] = {
         name = "Beach Ball",
         priceCurrencyType = CurrencyTypeEnum.money,
+        model = model("BeachBall"),
         price = 200
     }
 }
