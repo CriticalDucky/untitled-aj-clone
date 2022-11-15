03ff82fee73f5dd0e8f666b3ba39c44c9b995844
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 18:47:59 -0500

    Add Table utilities

--- src/shared/ReplicatedFirst/Utility/Table.lua
+++ src/shared/ReplicatedFirst/Utility/Table.lua
@@ -1,5 +1,7 @@
 local Table = {}
 
+_G.Table = Table
+
 function Table.dictLen(dict)
     local count = 0
 
@@ -83,6 +85,64 @@ function Table.print(t, note)
     print("Printing", note or tostring(t))
 
     printTable(t, "")
+end
+
+function Table.merge(...)
+    local merged = {}
+
+    for _, t in pairs({...}) do
+        for k, v in pairs(t) do
+            merged[k] = v
+        end
+    end
+
+    return merged
+end
+
+function Table.isEqualTo(t1, t2)
+    if type(t1) ~= "table" or type(t2) ~= "table" then
+        return t1 == t2
+    end
+
+    for k, v in pairs(t1) do
+        if not Table.isEqualTo(v, t2[k]) then
+            return false
+        end
+    end
+
+    for k, v in pairs(t2) do
+        if not Table.isEqualTo(v, t1[k]) then
+            return false
+        end
+    end
+
+    return true
+end
+
+function Table.recursiveIterate(t, callback)
+    local function recursiveIterate(t1, path)
+        for k, v in pairs(t1) do
+            local newPath = Table.copy(path)
+            table.insert(newPath, k)
+
+            if type(v) == "table" then
+                callback(Table.copy(newPath), v) -- Copy the path so it doesn't get modified by the callback
+                recursiveIterate(v, newPath)
+            else
+                callback(Table.copy(newPath), v)
+            end
+        end
+    end
+
+    recursiveIterate(t, {})
+end
+
+function Table.findFirstKey(t, callback) -- callback can be nil
+    for k, v in pairs(t) do
+        if not callback or callback(k, v) then
+            return k, v
+        end
+    end
 end
 
 return Table
