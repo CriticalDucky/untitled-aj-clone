e3c32398663f439f59604b481ae4ae90b6a99005
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 18:50:31 -0500

    Add model recieving for homes

--- src/shared/ReplicatedStorage/Data/Inventory/Items/Homes.lua
+++ src/shared/ReplicatedStorage/Data/Inventory/Items/Homes.lua
@@ -1,14 +1,22 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local enumsFolder = ReplicatedStorage.Shared.Enums
 
 local homeTypeEnum = require(enumsFolder.HomeType)
 local CurrencyTypeEnum = require(enumsFolder.CurrencyType)
+local Model = require(ReplicatedFirst.Shared.Utility.Model)
+local ModelType = require(enumsFolder.ModelType)
+
+local function model(name)
+    return Model(ModelType.home, name)
+end
 
 return {
-    [homeTypeEnum.testHome] = {
-        name = "Test House",
+    [homeTypeEnum.defaultHome] = {
+        name = "Default Home",
         priceCurrencyType = CurrencyTypeEnum.money,
-        price = 30
+        price = 30,
+        model = model("DefaultHome"),
     }
 }
