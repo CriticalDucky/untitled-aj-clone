d31f93450a3b5df854c99703fb8c1b8271e953d9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 19:05:11 -0500

    Add home server type identification

--- src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
+++ src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
@@ -1,11 +1,18 @@
+local Players = game:GetService("Players")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 
+local replicatedFirstShared = ReplicatedFirst.Shared
 local replicatedStorageShared = ReplicatedStorage.Shared
 local enumsFolder = replicatedStorageShared.Enums
+local serverFolder = replicatedStorageShared.Server
 
 local ServerTypeEnum = require(enumsFolder.ServerType)
-local Locations = require(replicatedStorageShared.Server.Locations)
-local Parties = require(replicatedStorageShared.Server.Parties)
+local Locations = require(serverFolder.Locations)
+local Parties = require(serverFolder.Parties)
+local GameSettings = require(replicatedFirstShared.Settings.GameSettings)
+
 
 local serverType do
     for _, locationInfo in pairs(Locations.info) do
@@ -18,6 +25,10 @@ local serverType do
         if partyInfo.placeId == game.PlaceId then
             serverType = ServerTypeEnum.party
         end
+    end
+
+    if GameSettings.homePlaceId == game.PlaceId then
+        serverType = ServerTypeEnum.home
     end
 
     if not serverType then
