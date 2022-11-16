04d3db3b439c3ae5366ffbd0248952bda597417c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 19:11:49 -0500

    Add prop reconcilation

--- src/shared/ServerStorage/Data/Inventory/InventoryManager.lua
+++ src/shared/ServerStorage/Data/Inventory/InventoryManager.lua
@@ -1,4 +1,4 @@
-local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
@@ -8,115 +8,147 @@ local replicatedStorageShared = ReplicatedStorage.Shared
 local serverStorageShared = ServerStorage.Shared
 local dataFolder = serverStorageShared.Data
 local inventoryFolder = dataFolder.Inventory
-
-local enumsFolder = replicatedStorageShared.Enums
+local utilityFolder = replicatedFirstShared.Utility
+local replicatedStorageData = replicatedStorageShared.Data
+local replicatedStorageInventory = replicatedStorageData.Inventory
 
 local PlayerData = require(dataFolder.PlayerData)
-local Items = require(replicatedStorageShared.Data.Inventory.Items)
-local ItemType = require(enumsFolder.ItemType)
-local Table = require(replicatedFirstShared.Utility.Table)
+local Items = require(replicatedStorageInventory.Items)
+local Table = require(utilityFolder.Table)
+local Event = require(utilityFolder.Event)
+local ItemProps = require(inventoryFolder.ItemProps)
+local GameSettings = require(replicatedFirstShared.Settings.GameSettings)
+local MiniId = require(utilityFolder.MiniId)
 
-local LIMITS = {
-    [ItemType.furniture] = 1000,
-    [ItemType.accessory] = 1000,
-    [ItemType.home] = 200,
-}
+local function addPropsToItem(item)
+    local itemCategory = item.itemCategory
 
-local PROPS = {
-    [ItemType.home] = {
-        placedItems = {},
-    },
-}
+    local props = ItemProps[itemCategory]
 
-local function addPropsToTable(itemType, t)
-    if PROPS[itemType] then
-        for k, v in pairs(PROPS[itemType]) do
-            if not t[k] then
-                t[k] = Table.deepCopy(v)
+    if props then
+        for propName, propValue in pairs(props) do
+            if item[propName] == nil then
+                item[propName] = Table.deepCopy(propValue)
             end
         end
     end
 
-    return t
+    return item
 end
 
 local InventoryManager = {}
 
-function InventoryManager.getInventory(player)
+InventoryManager.itemPlacingInInventory = Event.new()
+InventoryManager.itemRemovedFromInventory = Event.new()
+
+function InventoryManager.getInventory(player: Player | number)
     local playerData = PlayerData.get(player)
+    local profile = if playerData then playerData.profile else PlayerData.viewPlayerData(player, true)
 
-    return playerData and playerData.profile.Data.inventory
+    return profile and profile.Data.inventory
 end
 
-function InventoryManager.getItemInventory(player, itemType)
+function InventoryManager.getItemInventory(player: Player | number, itemCategory)
     local inventory = InventoryManager.getInventory(player)
 
-    return inventory and inventory[itemType]
+    return inventory and inventory[itemCategory]
 end
 
-function InventoryManager.reconcileInventory(player)
-    local playerData = PlayerData.get(player, true)
-
-    local inventory = InventoryManager.getInventory(player)
-
-    for itemType, items in pairs(inventory) do
-        for i, item in ipairs(items) do
-            playerData:arraySet({"inventory", itemType}, i, addPropsToTable(itemType, item))
-        end
-    end
-end
-
-function InventoryManager.newItem(itemType, itemReferenceId)
-    local itemsTable = Items[itemType]
-
-    if not itemsTable then
-        warn("Invalid item type: " .. itemType)
-
-        return
-    end
-
-    local item = itemsTable[itemReferenceId]
-
-    if not item then
-        warn("Invalid item id: " .. itemReferenceId)
-
-        return
-    end
-
-    return addPropsToTable(itemType, {
-        id = HttpService:GenerateGUID(false),
-        itemType = itemType,
-        itemReferenceId = itemReferenceId
-    })
-end
-
-function InventoryManager.isInventoryFull(player, itemType, numItemsToAdd)
-    local inventory = InventoryManager.getItemInventory(player, itemType)
+function InventoryManager.isInventoryFull(player: Player | number, itemCategory, numItemsToAdd)
+    local inventory = InventoryManager.getItemInventory(player, itemCategory)
     local numItems = #inventory
 
     numItemsToAdd = numItemsToAdd or 0
 
-    if numItems == LIMITS[itemType] then
+    local limit = GameSettings.inventoryLimits[itemCategory]
+
+    if numItems == limit then
         return true
     end
 
-    if numItems + numItemsToAdd > LIMITS[itemType] then
+    if numItems + numItemsToAdd > limit then
         return true
     end
+end
+
+function InventoryManager._removeItem(playerData, itemCategory, itemIndex)
+    if not playerData then
+        warn("InventoryManager._removeItem: No player data found")
+
+        return
+    end
+
+    local item = InventoryManager.getItemInventory(playerData.player, itemCategory)[itemIndex]
+
+    if not item then
+        warn("InventoryManager._removeItem: No item found")
+
+        return
+    end
+
+    playerData:arrayRemove({"inventory", itemCategory}, itemIndex)
+
+    InventoryManager.itemRemovedFromInventory:Fire(playerData.player, itemCategory, itemIndex, item)
+end
+
+function InventoryManager._addItem(playerData, itemCategory, item)
+    if not playerData then
+        warn("InventoryManager._addItem: No player data found")
+
+        return
+    end
+
+    local player = playerData.player
+
+    if InventoryManager.isInventoryFull(player, itemCategory, 1) then
+        warn("InventoryManager._addItem: Inventory is full")
+
+        return
+    end
+
+    InventoryManager.itemPlacingInInventory:Fire(player, itemCategory, item)
+
+    playerData:arrayInsert({"inventory", itemCategory}, item)
+end
+
+function InventoryManager.newItem(itemCategory, itemEnum, props)
+    local itemsTable = Items[itemCategory]
+
+    if not itemsTable then
+        warn("Invalid item type: " .. itemCategory)
+
+        return
+    end
+
+    local item = itemsTable[itemEnum]
+
+    if not item then
+        warn("Invalid item id: " .. itemEnum)
+
+        return
+    end
+
+    item = addPropsToItem{
+        id = MiniId(8),
+        itemCategory = itemCategory,
+        itemEnum = itemEnum
+    }
+
+    return if props then Table.merge(if table.isfrozen(props) then Table.deepCopy(props) else props, item) else item
 end
 
 function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, newOwner: Player | nil)
     local function checkIfInventoryWouldBeFull()
         for _, item in pairs(items) do
-            local otherItemsOfSameType = {} do
+            local otherItemsOfSameCategory = {} do
                 for _, otherItem in pairs(items) do
-                    if otherItem.itemType == item.itemType then
-                        table.insert(otherItemsOfSameType, otherItem)
+                    if otherItem.itemCategory == item.itemCategory then
+                        table.insert(otherItemsOfSameCategory, otherItem)
                     end
                 end
             end
             
-            return InventoryManager.isInventoryFull(newOwner, item.itemType, #otherItemsOfSameType)
+            return InventoryManager.isInventoryFull(newOwner, item.itemCategory, #otherItemsOfSameCategory)
         end
     end
 
@@ -152,10 +184,10 @@ function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, 
             end
     
             for _, item in pairs(items) do
-                local inventory = InventoryManager.getItemInventory(currentOwner, item.itemType)
+                local inventory = InventoryManager.getItemInventory(currentOwner, item.itemCategory)
 
                 if not inventory then
-                    warn("Player does not have an inventory of type " .. item.itemType)
+                    warn("Player does not have an inventory of type " .. item.itemCategory)
     
                     return
                 end
@@ -187,7 +219,7 @@ function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, 
                 if #itemsWithMatchingId > 1 then
                     warn("Player has multiple items with id " .. item.id)
 
-                    currentOwnerData:arrayRemove({"inventory", item.itemType}, itemIndex)
+                    InventoryManager._removeItem(currentOwnerData, item.itemCategory, itemIndex)
     
                     return
                 end
@@ -213,15 +245,15 @@ function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, 
 
         for _, item in pairs(items) do
             local itemIndex do
-                for i, v in ipairs(InventoryManager.getItemInventory(currentOwner, item.itemType)) do
+                for i, v in ipairs(InventoryManager.getItemInventory(currentOwner, item.itemCategory)) do
                     if v.id == item.id then
                         itemIndex = i
                     end
                 end
             end
 
-            currentOwnerData:arrayRemove({"inventory", item.itemType}, itemIndex)
-            newOwnerData:arrayInsert({"inventory", item.itemType}, item)
+            InventoryManager._removeItem(currentOwnerData, item.itemCategory, itemIndex)
+            InventoryManager._addItem(newOwnerData, item.itemCategory, item)
         end
 
         return true
@@ -236,14 +268,14 @@ function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, 
 
         for _, item in pairs(items) do
             local itemIndex do
-                for i, v in ipairs(InventoryManager.getItemInventory(currentOwner, item.itemType)) do
+                for i, v in ipairs(InventoryManager.getItemInventory(currentOwner, item.itemCategory)) do
                     if v.id == item.id then
                         itemIndex = i
                     end
                 end
             end
 
-            playerData:arrayRemove({"inventory", item.itemType}, itemIndex)
+            InventoryManager._removeItem(playerData, item.itemCategory, itemIndex)
         end
 
         return true
@@ -263,7 +295,7 @@ function InventoryManager.changeOwnerOfItems(items, currentOwner: Player | nil, 
         end
 
         for _, item in pairs(items) do
-            playerData:arrayInsert({"inventory", item.itemType}, item)
+            InventoryManager._addItem(playerData, item.itemCategory, item)
         end
         
         return true
@@ -274,22 +306,84 @@ function InventoryManager.addItemsToInventory(items, player)
     return InventoryManager.changeOwnerOfItems(items, nil, player)
 end
 
-function InventoryManager.deleteItemsFromInventory(items, player)
+function InventoryManager.removeItemsFromInventory(items, player)
     return InventoryManager.changeOwnerOfItems(items, player, nil)
 end
 
-function InventoryManager.newItemInInventory(itemType, itemReferenceId, player)
-    assert(itemType and itemReferenceId and player, "InventoryManager.newItemInInventory: Missing argument(s)")
+function InventoryManager.newItemInInventory(itemCategory, itemEnum, player, props)
+    assert(itemCategory and itemEnum and player, "InventoryManager.newItemInInventory: Missing argument(s)")
 
-    local item = InventoryManager.newItem(itemType, itemReferenceId)
+    local item = InventoryManager.newItem(itemCategory, itemEnum, props)
 
     if not item then
-        warn("Failed to create new item with type " .. itemType .. " and id " .. itemReferenceId)
+        warn("Failed to create new item with type " .. itemCategory .. " and id " .. itemEnum)
     
         return
     end
 
     return InventoryManager.addItemsToInventory({item}, player)
 end
+
+function InventoryManager.reconcileItems(playerData) -- Takes in the player data, and any props that are missing from any item's template are added
+    for itemCategory, items in pairs(InventoryManager.getInventory(playerData.player) or {}) do
+        local propTemplate = ItemProps[itemCategory]
+
+        if not propTemplate then
+            continue
+        end
+        
+        for itemIndex, item in ipairs(items) do
+            Table.recursiveIterate(propTemplate, function(path, value)
+                local prop
+
+                local function index(t, indexPath)
+                    local element = t
+
+                    for _, index in ipairs(indexPath) do
+                        element = element[index]
+                    end
+
+                    return element
+                end
+
+                prop = index(item, Table.copy(path))
+
+                if prop == nil then
+                    playerData:setValue({"inventory", itemCategory, itemIndex, table.unpack(path)}, Table.deepCopy(value))
+                end
+            end)
+        end
+    end
+end
+
+function InventoryManager.getItemWithId(itemSource: Player | table | number, itemId) -- takes in: (player: Player, id) or (itemInventory: Table, id)
+    if typeof(itemSource) == "Instance" or typeof(itemSource) == "number" then
+        local inventory = InventoryManager.getInventory(itemSource)
+
+        for _, items in pairs(inventory) do
+            local item = InventoryManager.getItemWithId(items, itemId)
+
+            if item then
+                return item
+            end
+        end
+    elseif typeof(itemSource) == "table" then
+        for _, item in pairs(itemSource) do
+            if item.id == itemId then
+                return item
+            end
+        end
+    else
+        warn("InventoryManager.searchWithId: Invalid argument #1")
+    
+        return
+    end
+end
+
+function InventoryManager.playerOwnsItem(player: Player | number, itemId)
+    return InventoryManager.getItemWithId(player, itemId) ~= nil
+end
+
+PlayerData.forAllPlayerData(InventoryManager.reconcileItems)
 
 return InventoryManager
