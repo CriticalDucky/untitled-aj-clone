faec8ddf68e1463660f268f3f3a049a1dca2c233
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-15 19:00:45 -0500

    Remove constants

--- src/shared/ReplicatedStorage/Server/Constants.lua
+++
@@ -1,9 +0,0 @@
-local Constants = {
-    location_maxPlayers = 20,
-    location_maxRecommendedPlayers = 15,
-    world_maxRecommendedPlayers = 50,
-    party_maxPlayers = 20,
-    party_maxRecommendedPlayers = 15,
-}
-
-return Constants
