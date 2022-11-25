6c6909b841b8ac0eef2de6777f3f1de6be1bd4a3
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-11-25 12:22:11 -0500

    Finalize new ServerData changes

--- src/shared/ServerStorage/ServerManagement/ServerData.lua
+++ src/shared/ServerStorage/ServerManagement/ServerData.lua
@@ -1,6 +1,7 @@
+local SERVERS_DATASTORE = "Servers"
 local WORLDS_KEY = "worlds"
 local PARTIES_KEY = "parties"
-local 
+local GAMES_KEY = "games"
 local CACHE_COOLDOWN = 30
 
 local DataStoreService = game:GetService("DataStoreService")
@@ -14,59 +15,146 @@ local replicatedStorageShared = ReplicatedStorage.Shared
 local replicatedFirstShared = ReplicatedFirst.Shared
 local serverStorageShared = ServerStorage.Shared
 local serverManagement = serverStorageShared.ServerManagement
+local serverFolder = replicatedStorageShared.Server
 local utilityFolder = serverStorageShared.Utility
 local replicatedFirstUtility = replicatedFirstShared.Utility
 
-local Locations = require(replicatedStorageShared.Server.Locations)
+local Locations = require(serverFolder.Locations)
+local Parties = require(serverFolder.Parties)
+local Games = require(serverFolder.Games)
 local DataStore = require(utilityFolder.DataStore)
-local GameServerData = require(serverManagement.GameServerData)
+local LiveServerData = require(serverManagement.LiveServerData)
 local Math = require(replicatedFirstUtility.Math)
 local Table = require(replicatedFirstUtility.Table)
 local Event = require(replicatedFirstUtility.Event)
+local PlayerData = require(serverStorageShared.Data.PlayerData)
 
-local serverDataStore = DataStoreService:GetDataStore("Servers")
-local cachedWorlds = {}
-local lastDatastoreRequest = 0
+local serverDataStore = DataStoreService:GetDataStore(SERVERS_DATASTORE)
 
-local WorldData = {}
+local cachedData = {
+    [WORLDS_KEY] = {
+        --[[
+            [worldIndex] = {
+                locations = {
+                    [locationEnum] = {
+                        privateServerId = privateServerId,
+                        serverCode = serverCode,
+                    }
+                }
+            }
+        ]]
+    },
+    [PARTIES_KEY] = {
+        --[[
+            [partyEnum] = {
+                [partyIndex] = {
+                    privateServerId = privateServerId,
+                    serverCode = serverCode,
+                }
+            }
+        ]]
+    },
+    [GAMES_KEY] = {
+        --[[
+            [gameEnum] = {
+                [gameIndex] = {
+                    privateServerId = privateServerId,
+                    serverCode = serverCode,
+                }
+            }
+        ]]
+    },
+--[[
+    [PrivateServerId] = {
+        privateServerId = privateServerId,
+        serverCode = serverCode,
+        [any] = any
+    }
+]]
+}
 
-WorldData.WorldsUpdated = Event.new()
+local lastDatastoreRequest = {
+    [WORLDS_KEY] = 0,
+    [PARTIES_KEY] = 0,
+    [GAMES_KEY] = 0,
+}
 
-local function retrieveDatastore()
-    lastDatastoreRequest = time()
-    local lastCachedWorlds = cachedWorlds
+local constantKeys = {
+    [WORLDS_KEY] = true,
+    [PARTIES_KEY] = true,
+    [GAMES_KEY] = true,
+}
 
-    local success, worlds = DataStore.safeGet(worldsDataStore, WORLDS_KEY)
+local isRetrieving = {}
+
+local ServerData = {}
+
+ServerData.WorldsUpdated = Event.new()
+ServerData.WORLDS_KEY = WORLDS_KEY
+ServerData.PARTIES_KEY = PARTIES_KEY
+ServerData.GAMES_KEY = GAMES_KEY
+
+local function retrieveDatastore(key)
+    if isRetrieving[key] then
+        repeat task.wait() until not isRetrieving[key]
+    end
+
+    lastDatastoreRequest[key] = time()
+    local lastCached = cachedData[key]
+    isRetrieving[key] = true
+
+    local success, data = DataStore.safeGet(serverDataStore, key)
+
+    isRetrieving[key] = false
     
-    for i, v in ipairs(worlds or {}) do
-        cachedWorlds[i] = v
-    end
+    if success then
+        cachedData[key] = data or {}
 
-    if #cachedWorlds ~= #lastCachedWorlds then
-        WorldData.WorldsUpdated:Fire(cachedWorlds)
+        if key == WORLDS_KEY and not Table.compare(lastCached, data) then
+            ServerData.WorldsUpdated:Fire(data)
+        end
     end
 end
 
-function WorldData.get(getUpdated)
-    if getUpdated or #cachedWorlds == 0 then
-        retrieveDatastore()
+function ServerData.get(key)
+    local data = cachedData[key]
+
+    if not data or not Table.hasAnything(data) then
+        retrieveDatastore(key)
     end
 
-    return cachedWorlds
+    return cachedData[key]
 end
 
-function WorldData.update(transformFunction)
-    local success = DataStore.safeUpdate(worldsDataStore, WORLDS_KEY, transformFunction)
+function ServerData.getWorlds()
+    return ServerData.get(WORLDS_KEY)
+end
+
+function ServerData.getParties()
+    return ServerData.get(PARTIES_KEY)
+end
+
+function ServerData.getGames()
+    return ServerData.get(GAMES_KEY)
+end
+
+function ServerData.update(key, transformFunction)
+    local success = DataStore.safeUpdate(serverDataStore, key, transformFunction)
+
+    local data = cachedData[key]
 
     if success then
-        transformFunction(cachedWorlds)
-        WorldData.WorldsUpdated:Fire(cachedWorlds)
+        transformFunction(data)
+        
+        if key == WORLDS_KEY then
+            ServerData.WorldsUpdated:Fire(data)
+        end
     end
 
     return success
 end
 
-function WorldData.addWorld()
+function ServerData.addWorld()
     local world do
         world = {
             locations = {},
@@ -84,22 +172,152 @@ function WorldData.addWorld()
         end
     end
 
-    return WorldData.update(function(worlds)
+    return ServerData.update(WORLDS_KEY, function(worlds)
         table.insert(worlds, world)
 
         return worlds
-    end), #cachedWorlds
+    end), #cachedData[WORLDS_KEY]
 end
 
-function WorldData.findAvailableLocation(worldIndex)
-    if not worldIndex then
-        warn("WorldData.findAvailableLocation: worldIndex is nil")
+function ServerData.addParty(partyEnum)
+    local party do
+        local serverCode, privateServerId = TeleportService:ReserveServer(Parties[partyEnum].placeId)
 
-        return
+        party = {
+            serverCode = serverCode,
+            privateServerId = privateServerId,
+        }
     end
+
+    return ServerData.update(PARTIES_KEY, function(parties)
+        parties[partyEnum] = parties[partyEnum] or {}
+
+        table.insert(parties[partyEnum], party)
+
+        return parties
+    end), #(cachedData[PARTIES_KEY][partyEnum] or {})
+end
+
+function ServerData.addGame(gameEnum)
+    local newGame do
+        local serverCode, privateServerId = TeleportService:ReserveServer(Games[gameEnum].placeId)
+
+        newGame = {
+            serverCode = serverCode,
+            privateServerId = privateServerId,
+        }
+    end
+
+    return ServerData.update(GAMES_KEY, function(games)
+        games[gameEnum] = games[gameEnum] or {}
+
+        table.insert(games[gameEnum], newGame)
+
+        return games
+    end), #(cachedData[GAMES_KEY][gameEnum] or {})
+end
+
+function ServerData.stampHomeServer(owner: Player)
+    local playerData = PlayerData.get(owner)
+
+    if playerData then
+        local homeServerInfo = playerData.profile.Data.playerInfo.homeServerInfo
+        local privateServerId = homeServerInfo.privateServerId
+
+        local success = DataStore.safeSet(serverDataStore, privateServerId, {
+            owner = owner.UserId,
+        })
+
+        if success then
+            cachedData[privateServerId] = {
+                owner = owner.UserId,
+            }
+
+            playerData:setValue({"playerInfo", "homeInfoStamped"}, true)
+        end
+    end
+end
+
+function ServerData.traceServer(privateServerId)
+    privateServerId = privateServerId or game.PrivateServerId
+
+    local serverInfo = cachedData[privateServerId]
+
+    if not serverInfo or not Table.hasAnything(serverInfo) then
+        retrieveDatastore(privateServerId)
+    end
+
+    return cachedData[privateServerId]
+end
+
+function ServerData.getServerInfo(key)
+    if constantKeys[key] then
+        local data = ServerData.get(key)
+
+        local info
+
+        if key == WORLDS_KEY then
+            for i, world in ipairs(data) do
+                for enum, location in pairs(world.locations) do
+                    if location.privateServerId == game.PrivateServerId then
+                        info = {
+                            worldIndex = i,
+                            locationEnum = enum,
+                        }
+
+                        break
+                    end
+                end
+
+                if info then
+                    break
+                end
+            end
+        elseif key == PARTIES_KEY then
+            for _, parties in pairs(data) do
+                for i, party in ipairs(parties) do
+                    if party.privateServerId == game.PrivateServerId then
+                        info = {
+                            partyIndex = i,
+                        }
+
+                        break
+                    end
+                end
+
+                if info then
+                    break
+                end
+            end
+        elseif key == GAMES_KEY then
+            for _, games in pairs(data) do
+                for i, game in ipairs(games) do
+                    if game.privateServerId == game.PrivateServerId then
+                        info = {
+                            gameIndex = i,
+                        }
+
+                        break
+                    end
+                end
+
+                if info then
+                    break
+                end
+            end
+        end
+
+        return info
+    else
+        return ServerData.traceServer(key)
+    end
+end
+
+function ServerData.findAvailableLocation(worldIndex)
+    assert(worldIndex, "No world index provided")
     
     local locationEnum
-    local worldPopulationInfo = GameServerData.getWorldPopulationInfo(worldIndex)
+    local worldPopulationInfo = LiveServerData.getWorldPopulationInfo(worldIndex)
 
     for _, locationType in pairs(Locations.priority) do
         if worldPopulationInfo then
@@ -123,8 +341,8 @@ function WorldData.findAvailableLocation(worldIndex)
     return locationEnum
 end
 
-function WorldData.findAvailableWorld(forcedLocation)
-    local worlds = WorldData.get()
+function ServerData.findAvailableWorld(forcedLocation)
+    local worlds = ServerData.getWorlds()
 
     if worlds == nil then
         warn("No server data found")
@@ -135,7 +353,7 @@ function WorldData.findAvailableWorld(forcedLocation)
         local rarities = {}
 
         for worldIndex, world in ipairs(worlds) do
-            local worldPopulationInfo = GameServerData.getWorldPopulationInfo(worldIndex)
+            local worldPopulationInfo = LiveServerData.getWorldPopulationInfo(worldIndex)
 
             local worldIsSuitable = true
 
@@ -149,7 +367,7 @@ function WorldData.findAvailableWorld(forcedLocation)
                     end
                 end
     
-                if not WorldData.findAvailableLocation(worldIndex) then
+                if not ServerData.findAvailableLocation(worldIndex) then
                     worldIsSuitable = false
                 end
     
@@ -159,7 +377,7 @@ function WorldData.findAvailableWorld(forcedLocation)
             end
 
             if not worldIsSuitable then
-                print("WorldData.findAvailable: World " .. worldIndex .. " is not suitable")
+                print("ServerData.findAvailableWorld: World " .. worldIndex .. " is not suitable")
                 continue
             end
 
@@ -173,7 +391,7 @@ function WorldData.findAvailableWorld(forcedLocation)
                 end
             end
 
-            print("WorldData.findAvailable: World " .. worldIndex .. " has a chance of " .. chance)
+            print("ServerData.findAvailableWorld: World " .. worldIndex .. " has a chance of " .. chance)
 
             rarities[worldIndex] = chance
         end
@@ -184,7 +402,7 @@ function WorldData.findAvailableWorld(forcedLocation)
     if worldIndex == nil then
         print("No suitable world found, creating new world")
 
-        local success, worldIndex = WorldData.addWorld()
+        local success, worldIndex = ServerData.addWorld()
 
         return success and worldIndex
     end
@@ -192,27 +410,27 @@ function WorldData.findAvailableWorld(forcedLocation)
     return worldIndex
 end
 
-function WorldData.findAvailable(forcedLocation)
-    local worlds = WorldData.get()
+function ServerData.findAvailableWorldAndLocation(forcedLocation)
+    local worlds = ServerData.getWorlds()
 
     if worlds == nil then
         warn("No server data found")
         return
     end
 
-    local worldIndex = WorldData.findAvailableWorld(forcedLocation)
-    local locationEnum = forcedLocation or WorldData.findAvailableLocation(worldIndex)
+    local worldIndex = ServerData.findAvailableWorld(forcedLocation)
+    local locationEnum = forcedLocation or ServerData.findAvailableLocation(worldIndex)
 
     print("Found world", worldIndex, "with location", locationEnum)
 
     if locationEnum == nil then
         print("No available location found, creating new world")
         
-        local success, newWorldIndex = WorldData.addWorld()
+        local success, newWorldIndex = ServerData.addWorld()
 
         if success then
             worldIndex = newWorldIndex
-            locationEnum = WorldData.findAvailableLocation(worldIndex)
+            locationEnum = ServerData.findAvailableLocation(worldIndex)
         else -- Failed to create new world
             warn("Failed to create new world")
             return
@@ -223,9 +441,11 @@ function WorldData.findAvailable(forcedLocation)
 end
 
 RunService.Heartbeat:Connect(function()
-    if time() - lastDatastoreRequest > CACHE_COOLDOWN then
-        retrieveDatastore()
+    for constantKey, _ in pairs(constantKeys) do
+        if time() - lastDatastoreRequest[constantKey] > CACHE_COOLDOWN then
+            retrieveDatastore(constantKey)
+        end
     end
 end)
 
-return WorldData
+return ServerData
