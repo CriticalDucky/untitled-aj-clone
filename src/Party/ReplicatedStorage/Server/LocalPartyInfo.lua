01d31674727c62316a89d554326210d14b7aac4e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-12-07 19:37:51 -0500

    Add replication

--- src/Party/ReplicatedStorage/Server/LocalPartyInfo.lua
+++ src/Party/ReplicatedStorage/Server/LocalPartyInfo.lua
@@ -1,4 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 
 local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
 local serverFolder = replicatedStorageShared:WaitForChild("Server")
@@ -12,6 +13,31 @@ for partyType, partyInfo in pairs(Parties) do
         LocalPartyInfo.partyType = partyType
         break
     end
+end
+
+if RunService:IsClient() then
+    local ReplicaCollection = require(replicatedStorageShared.Replication.ReplicaCollection)
+
+    LocalPartyInfo.partyIndex = ReplicaCollection.get("PartyIndex", true).Data.partyIndex
+elseif RunService:IsServer() then
+    local ServerStorage = game:GetService("ServerStorage")
+
+    local serverStorageShared = ServerStorage.Shared
+    local serverManagementFolder = serverStorageShared.ServerManagement
+
+    local ReplicaService = require(serverStorageShared.Data.ReplicaService)
+    local ServerData = require(serverManagementFolder.ServerData)
+
+    local serverData = ServerData.traceServer()
+    LocalPartyInfo.partyIndex = serverData and serverData.partyIndex
+
+    ReplicaService.NewReplica({
+        ClassToken = ReplicaService.NewClassToken("PartyIndex"),
+        Data = {
+            partyIndex = LocalPartyInfo.partyIndex,
+        },
+        Replication = "All",
+    })
 end
 
 return LocalPartyInfo
