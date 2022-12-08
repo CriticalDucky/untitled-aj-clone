e59e350dad843392320a55f697c5288032e4c6aa
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-12-07 19:37:07 -0500

    Remove fingerprinting

--- src/Home/ReplicatedStorage/Server/LocalHomeInfo.lua
+++ src/Home/ReplicatedStorage/Server/LocalHomeInfo.lua
@@ -8,21 +8,23 @@ local homeOwner do
     if RunService:IsClient() then
         local ReplicaCollection = require(replicatedStorageShared.Replication.ReplicaCollection)
 
-        homeOwner = ReplicaCollection.get("HomeOwner", true).Data.userId
+        homeOwner = ReplicaCollection.get("HomeOwner", true).Data.homeOwner
     elseif RunService:IsServer() then
         local ServerStorage = game:GetService("ServerStorage")
     
         local serverStorageShared = ServerStorage.Shared
+        local serverManagementFolder = serverStorageShared.ServerManagement
     
         local ReplicaService = require(serverStorageShared.Data.ReplicaService)
-        local Fingerprint = require(serverStorageShared.Utility.Fingerprint)
-    
-        _, homeOwner = Fingerprint.trace(game.PrivateServerId)
+        local ServerData = require(serverManagementFolder.ServerData)
+
+        local serverData = ServerData.traceServer()
+        homeOwner = serverData and serverData.homeOwner
 
         ReplicaService.NewReplica({
             ClassToken = ReplicaService.NewClassToken("HomeOwner"),
             Data = {
-                userId = homeOwner,
+                homeOwner = homeOwner,
             },
             Replication = "All",
         })
