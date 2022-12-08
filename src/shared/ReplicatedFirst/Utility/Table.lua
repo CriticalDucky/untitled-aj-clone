97721c7359478e9725dfd8bbd32aeefe2e7b56a7
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2022-12-07 19:39:42 -0500

    Table.print improvements

--- src/shared/ReplicatedFirst/Utility/Table.lua
+++ src/shared/ReplicatedFirst/Utility/Table.lua
@@ -70,14 +70,30 @@ function Table.findMin(t)
     return k, v
 end
 
-function Table.print(t, note)
+function Table.print(t, note, printTypes)
+    local MAX_PRINTS = 300
+    local prints = 0
+
     local function printTable(t, indent)
+        if prints >= MAX_PRINTS then
+            return
+        end
+
         for k, v in pairs(t) do
+            local baseString = (printTypes and " (%s)" or "")
+            local keyType = baseString:format(typeof(k))
+            local valueType = baseString:format(typeof(v))
+
             if type(v) == "table" then
-                print(indent .. tostring(k) .. " :")
+                print(indent .. tostring(k) .. keyType .. ":")
+
+                prints += 1
+
                 printTable(v, indent .. "    ")
             else
-                print(indent .. tostring(k) .. " : " .. tostring(v))
+                print(indent .. tostring(k) .. keyType .. " : " .. tostring(v) .. valueType)
+
+                prints += 1
             end
         end
     end
@@ -99,19 +115,19 @@ function Table.merge(...)
     return merged
 end
 
-function Table.isEqualTo(t1, t2)
+function Table.compare(t1, t2)
     if type(t1) ~= "table" or type(t2) ~= "table" then
         return t1 == t2
     end
 
     for k, v in pairs(t1) do
-        if not Table.isEqualTo(v, t2[k]) then
+        if not Table.compare(v, t2[k]) then
             return false
         end
     end
 
     for k, v in pairs(t2) do
-        if not Table.isEqualTo(v, t1[k]) then
+        if not Table.compare(v, t1[k]) then
             return false
         end
     end
@@ -144,5 +160,116 @@ function Table.findFirstKey(t, callback) -- callback can be nil
         end
     end
 end
+
+function Table.hasAnything(t)
+    for _, _ in pairs(t) do
+        return true
+    end
+
+    return false
+end
+
+function Table.deepReconcile(template, t)
+    local function deepReconcile(t1, t2)
+        for k, v in pairs(t1) do
+            if type(v) == "table" then
+                if type(t2[k]) ~= "table" then
+                    t2[k] = Table.deepCopy(v)
+                else
+                    deepReconcile(v, t2[k])
+                end
+            else
+                if t2[k] == nil then
+                    t2[k] = v
+                end
+            end
+        end
+    end
+
+    deepReconcile(template, t)
+end
+
+function Table.safeIndex(t, ...)
+    local value = t
+
+    for _, key in pairs({...}) do
+        if type(value) == "table" then
+            value = value[key]
+        else
+            return nil
+        end
+    end
+
+    return value
+end
+
+function Table.deepToNumber(t, copy)
+    t = if copy then Table.deepCopy(t) else t
+
+    local function deepToNumber(t1)
+        for k, v in pairs(t1) do
+            if type(v) == "table" then
+                deepToNumber(v)
+            else
+                t1[k] = tonumber(v) or v
+            end
+        end
+    end
+
+    deepToNumber(t)
+
+    return t
+end
+
+function Table.deepToNumberKeys(t, copy): table
+    t = if copy then Table.deepCopy(t) else t
+
+    local function deepToNumberKeys(t1)
+        for k, v in pairs(Table.copy(t1)) do
+            if type(v) == "table" then
+                deepToNumberKeys(v)
+            end
+
+            if type(k) == "string" then
+                local numberKey = tonumber(k)
+
+                if numberKey then
+                    t1[numberKey] = v
+                    t1[k] = nil
+                end
+            end
+        end
+    end
+
+    deepToNumberKeys(t)
+
+    return t
+end
+
+function Table.selectWithKeys(t, keys) -- takes in an array of keys and returns a table with only those keys
+    local selected = {}
+
+    for _, key in pairs(keys) do
+        selected[key] = t[key]
+    end
+
+    return selected
+end
+
+
+local testTable = { -- has keys of all types and values of all types and many depths
+    a = {
+        b = 1,
+        c = "hello",
+        d = false,
+        ["2"] = "number key",
+        [1] = "number value",
+    },
+    b = 1,
+    c = "hello",
+    d = false,
+    ["2"] = "number key",
+    [1] = "number value",
+}
 
 return Table
