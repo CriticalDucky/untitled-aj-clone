583ab765c896d839ae9cebe45ba1e0187fa236ae
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-12 23:55:18 -0500

    Promisify DataStore

--- src/shared/ServerStorage/Utility/DataStore.lua
+++ src/shared/ServerStorage/Utility/DataStore.lua
@@ -1,105 +1,85 @@
+-- Helper functions for simple datastore requests for actions that don't require major wrappers like ProfileService
+
 local DATASTORE_MAX_RETRIES = 10
+
+local Promise = require(game:GetService("ReplicatedFirst").Shared.Utility.Promise)
 
 local DataStore = {}
 
 function DataStore.safeUpdate(dataStore, key, transformFunction, extra)
     print("DATASTORE: safeUpdate")
 
-    local possibleError
-
     local function try()
-        return pcall(function()
+        return Promise.try(function()
             return dataStore:UpdateAsync(key, transformFunction, extra)
         end)
     end
 
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success, err = try()
-
-        possibleError = err
-
-        if success then
-            return true
-        end
-    end
-
-    warn("Failed to update data store", possibleError)
-    return false
+    return Promise.retry(try, DATASTORE_MAX_RETRIES)
+        :andThen(function(...)
+            return Promise.resolve(select(2, ...))
+        end)
+        :catch(function(err)
+            warn("Failed to update data store:", tostring(err))
+            return Promise.reject(err)
+        end)
 end
 
 function DataStore.safeSet(dataStore, key, value, extra)
     print("DATASTORE: safeSet")
 
-    local possibleError
-
     local function try()
-        return pcall(function()
+        return Promise.try(function()
             return dataStore:SetAsync(key, value, extra)
         end)
     end
 
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success, err = try()
-
-        possibleError = err
-
-        if success then
-            return true
-        end
-    end
-
-    warn("Failed to set data store", possibleError)
-    return false
+    return Promise.retry(try, DATASTORE_MAX_RETRIES)
+        :andThen(function(...)
+            return Promise.resolve(select(2, ...))
+        end)
+        :catch(function(err)
+            warn("Failed to set data store:", tostring(err))
+            return Promise.reject(err)
+        end)
 end
 
 function DataStore.safeGet(dataStore, key, extra)
     print("DATASTORE: safeGet", key)
 
-    local possibleError
-
     local function try()
-        return pcall(function()
+        return Promise.try(function()
             return dataStore:GetAsync(key, extra)
         end)
     end
 
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success, data = try()
-
-        possibleError = data
-
-        if success then
-            return true, data
-        end
-    end
-
-    warn("Failed to get data store", possibleError)
-    return false
+    return Promise.retry(try, DATASTORE_MAX_RETRIES)
+        :andThen(function(...)
+            return Promise.resolve(select(2, ...))
+        end)
+        :catch(function(err)
+            warn("Failed to get data store:", tostring(err))
+            return Promise.reject(err)
+        end)
 end
 
 function DataStore.safeRemove(dataStore, key, extra)
     print("DATASTORE: safeRemove")
 
-    local possibleError
-
     local function try()
-        return pcall(function()
+        return Promise.try(function()
             return dataStore:RemoveAsync(key, extra)
         end)
     end
 
-    for _ = 1, DATASTORE_MAX_RETRIES do
-        local success, err = try()
-
-        possibleError = err
-
-        if success then
-            return true
-        end
-    end
-
-    warn("Failed to remove data store", possibleError)
-    return false
+    return Promise.retry(try, DATASTORE_MAX_RETRIES)
+        :andThen(function(...)
+            return Promise.resolve(select(2, ...))
+        end)
+        :catch(function(err)
+            warn("Failed to remove data store:", tostring(err))
+            return Promise.reject(err)
+        end)
 end
 
 return DataStore
