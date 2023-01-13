eafa5f669134658c68404210357368cc5e7dc751
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-12 23:50:13 -0500

    Promisify ClientServerData

--- src/shared/ReplicatedStorage/Server/ClientServerData.lua
+++ src/shared/ReplicatedStorage/Server/ClientServerData.lua
@@ -10,20 +10,75 @@ local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
 
 local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
+local Promise = require(replicatedFirstShared:WaitForChild("Utility"):WaitForChild("Promise"))
+local Table = require(replicatedFirstShared:WaitForChild("Utility"):WaitForChild("Table"))
+
 local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
+local Value = Fusion.Value
+local Observer = Fusion.Observer
 
-local replica = ReplicaCollection.get("ServerData", true)
+local privateServerId = game.PrivateServerId
 
-local serverDataValue = Fusion.Value(replica.Data)
+local replicaPromise = ReplicaCollection.get("ServerData", true)
+    :andThen(function(replica)
+        local serverDataValue = Fusion.Value(replica.Data)
 
-replica:ListenToRaw(function()
-    serverDataValue:set(replica.Data)
-end)
+        replica:ListenToRaw(function()
+            serverDataValue:set(replica.Data)
+        end)
+
+        return serverDataValue
+    end)
+
+local serverInfoPromise = replicaPromise
+    :andThen(function(serverDataValue)
+        return Promise.new(function(resolve)
+            local disconnect
+
+            local function find()
+                local serverData = serverDataValue:get()
+
+                if serverData[privateServerId] then
+                    disconnect()
+                    resolve(serverData[privateServerId])
+                end
+
+                Table.recursiveIterate(serverData, function(path, value)
+                    if type(value) == "table" and value.privateServerId == privateServerId then
+                        disconnect()
+
+                        local constantKey = path[1]
+
+                        if constantKey == WORLDS_KEY then -- the path is [WORLDS_KEY, worldIndex, "locations", locationEnum]
+                            resolve {
+                                worldIndex = path[2],
+                                locationEnum = path[4],
+                            }
+                        elseif constantKey == PARTIES_KEY then -- the path is [PARTIES_KEY, partyType, partyIndex]
+                            resolve {
+                                partyType = path[2],
+                                partyIndex = path[3],
+                            }
+                        elseif constantKey == GAMES_KEY then -- the path is [GAMES_KEY, gameType, gameIndex]
+                            resolve {
+                                gameType = path[2],
+                                gameIndex = path[3],
+                            }
+                        end
+                    end
+                end)
+            end
+
+            disconnect = Observer(serverDataValue):onChange(find)
+
+            find()
+        end)
+    end)
 
 local ClientServerData = {}
 
 function ClientServerData.get()
-    return serverDataValue:get()
+    return ClientServerData:get()
 end
 
 function ClientServerData.getWorlds()
@@ -38,6 +93,10 @@ function ClientServerData.getGames()
     return ClientServerData:get()[GAMES_KEY]
 end
 
+function ClientServerData.promise()
+    return replicaPromise
+end
+
 function ClientServerData.worldHasLocation(worldIndex, locationEnum)
     local worlds = ClientServerData.getWorlds()
 
@@ -48,6 +107,10 @@ function ClientServerData.worldHasLocation(worldIndex, locationEnum)
             return true
         end
     end
+end
+
+function ClientServerData.getServerInfo()
+    return serverInfoPromise
 end
 
 return ClientServerData
