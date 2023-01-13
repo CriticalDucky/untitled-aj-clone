8138a1b730e434d2ae7f30c1d47d13faa420f731
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-12 23:52:31 -0500

    Rename GameServerDataPublisher

---
+++ src/shared/ServerScriptService/Servermanagement/LiveServerDataPublisher.server.lua
@@ -0,0 +1,106 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ServerStorage = game:GetService("ServerStorage")
+
+local replicatedStorageShared = ReplicatedStorage.Shared
+local replicatedFirstShared = ReplicatedFirst.Shared
+local serverStorageShared = ServerStorage.Shared
+local utilityFolder = replicatedFirstShared.Utility
+local enumsFolder = replicatedStorageShared.Enums
+local serverFolder = replicatedStorageShared.Server
+
+local LiveServerData = require(serverFolder.LiveServerData)
+local LocalServerInfo = require(serverFolder.LocalServerInfo)
+local ServerGroupEnum = require(enumsFolder.ServerGroup)
+local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
+local Promise = require(utilityFolder.Promise)
+
+Promise.resolve()
+    :andThen(function()
+        if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
+            return {
+                jobId = game.JobId,
+            }
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
+            return LocalServerInfo.getServerInfo()
+                :andThen(function(serverInfo)
+                    return {
+                        worldIndex = serverInfo.worldIndex,
+                        locationEnum = serverInfo.locationEnum,
+                    }
+                end)
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
+            return LocalServerInfo.getServerInfo()
+                :andThen(function(serverInfo)
+                    return {
+                        homeOwner = serverInfo.homeOwner,
+                    }
+                end)
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
+            return LocalServerInfo.getServerInfo()
+                :andThen(function(serverInfo)
+                    return {
+                        partyType = serverInfo.partyType,
+                        partyIndex = serverInfo.partyIndex,
+                    }
+                end)
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isGame) then
+            return LocalServerInfo.getServerInfo()
+                :andThen(function(serverInfo)
+                    return {
+                        gameType = serverInfo.gameType,
+                        gameIndex = serverInfo.gameIndex,
+                    }
+                end)
+        end
+    end)
+    :andThen(function(indexInfo)
+        local runServiceConnection = RunService.Heartbeat:Connect(function()
+            if LiveServerData.canPublish() then
+                local serverInfo
+        
+                local function getUserIds()
+                    local userIds = {}
+        
+                    for _, player in pairs(Players:GetPlayers()) do
+                        table.insert(userIds, player.UserId)
+                    end
+        
+                    return userIds
+                end
+        
+                serverInfo = {}
+        
+                --#region Optional custom server info
+        
+                if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
+        
+                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
+        
+                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
+        
+                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
+        
+                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isGame) then
+        
+                end
+        
+                --#endregion
+        
+                serverInfo.players = getUserIds()
+        
+                LiveServerData.publish(serverInfo, indexInfo)
+            end
+        end)
+        
+        game:BindToClose(function()
+            runServiceConnection:Disconnect() -- Disconnect heartbeat connection so that no other publish requests are made after this
+
+            task.delay(1, function()
+                LiveServerData.publish(nil, indexInfo)
+            end)
+        end)
+    end)
+
