c31495d045d02abf6bc9d9a6f21a192216bc5885
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-12 23:54:14 -0500

    Add documentation

--- src/shared/ServerStorage/Data/Inventory/ItemProps.lua
+++ src/shared/ServerStorage/Data/Inventory/ItemProps.lua
@@ -4,6 +4,8 @@ local enumsFolder = ReplicatedStorage.Shared.Enums
 
 local ItemCategory = require(enumsFolder.ItemCategory)
 
+-- If you add something here, make sure to add it to the InventoryItem type in src\Shared\ReplicatedFirst\Utility\Types.lua
+
 return {
     [ItemCategory.home] = {
         placedItems = {},
