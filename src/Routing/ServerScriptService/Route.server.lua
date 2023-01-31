dafe17fe6633b3078727c88faae86cef2edc76d5
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:43:07 -0500

    Convert route into primary script

--- src/Routing/ServerScriptService/Route.server.lua
+++ src/Routing/ServerScriptService/Route.server.lua
@@ -4,22 +4,46 @@ local ServerStorage = game:GetService("ServerStorage")
 local serverStorageShared = ServerStorage.Shared
 local teleportation = serverStorageShared.Teleportation
 
-local Route = require(teleportation.Route)
+local Teleport = require(teleportation.Teleport)
+local ServerData = require(serverStorageShared.ServerManagement.ServerData)
+
+local function onRouteFailure(player)
+	Teleport.rejoin(player, "An internal server error occurred. Please try again later. (err code R1)")
+end
+
+local function routeToWorld(player)
+	return ServerData.findAvailableWorld():andThen(function(worldIndex)
+		return Teleport.toWorld(player, worldIndex, function(player)
+			onRouteFailure(player)
+		end)
+	end):catch(function()
+        onRouteFailure(player)
+    end)
+end
 
 local function playerAdded(player: Player)
-    if player.FollowUserId == 0 then
-        if not Route.routeToWorld(player) then
-            Route.onRouteFailure()
-        end
-    elseif player.FollowUserId ~= 0 then
-        if not (Route.routeToPlayer(player) or Route.routeToWorld(player)) then
-            Route.onRouteFailure()
-        end
-    end
+	local followUserId = player.FollowUserId
+
+	if followUserId == 0 then
+		routeToWorld(player)
+	elseif followUserId ~= 0 then
+		Teleport.Authorize
+			.toPlayer(player, followUserId)
+			:andThen(function()
+				Teleport.toPlayer(player, followUserId, function()
+					routeToWorld(player)
+				end):catch(function()
+					routeToWorld(player)
+				end)
+			end)
+			:catch(function()
+				routeToWorld(player)
+			end)
+	end
 end
 
 for _, player in pairs(Players:GetPlayers()) do
-    playerAdded(player)
+	playerAdded(player)
 end
 
 Players.PlayerAdded:Connect(playerAdded)
