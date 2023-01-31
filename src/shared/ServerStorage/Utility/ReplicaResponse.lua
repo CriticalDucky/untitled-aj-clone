a5b772f872b45f416656b713653a34b9dca96def
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:56:13 -0500

    Add ReplicaReponse

---
+++ src/shared/ServerStorage/Utility/ReplicaResponse.lua
@@ -0,0 +1,24 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
+
+local Types = require(ReplicatedFirst:WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("Types"))
+
+type Promise = Types.Promise
+
+local ReplicaResponse = {}
+
+--[[
+    This function is used for replica responses on a server. Pass in a Replica,
+	and a callback that takes a player and any number of arguments, and returns a Promise.
+	
+	When the replica is fired, the callback will be called with the player and the arguments.
+	Values returned from the callback will be passed to the client.
+]]
+function ReplicaResponse.listen(requestReplica, callback: (Player, any) -> Promise)
+	requestReplica.OnServerEvent:Connect(function(player, requestCode, ...)
+		callback(player, ...):finally(function(...)
+			requestReplica:FireClient(player, requestCode, ...)
+		end)
+	end)
+end
+
+return ReplicaResponse
