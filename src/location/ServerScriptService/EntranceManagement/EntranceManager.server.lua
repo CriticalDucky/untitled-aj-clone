4c8e6f1f1f7c0ba39c23f013b950138d91717422
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:42:21 -0500

    Compatability changes

--- src/location/ServerScriptService/EntranceManagement/EntranceManager.server.lua
+++ src/location/ServerScriptService/EntranceManagement/EntranceManager.server.lua
@@ -4,29 +4,25 @@ local Players = game:GetService("Players")
 local serverStorageShared = ServerStorage.Shared
 local entranceDataFolder = ServerStorage.EntranceData
 local teleportation = serverStorageShared.Teleportation
-local dataFolder = serverStorageShared.Data
 
 local Entrances = require(entranceDataFolder.Entrances)
 local Teleport = require(teleportation.Teleport)
-local PlayerData = require(dataFolder.PlayerData)
-local Route = require(teleportation.Route)
 
 local playersTouched = {}
 
 for enum, entranceComponents in pairs(Entrances.groups) do
     entranceComponents.exit.Touched:Connect(function(touchedPart)
-        if touchedPart and touchedPart.Parent and touchedPart.Parent:FindFirstChild("Humanoid") then
-            local player = Players:GetPlayerFromCharacter(touchedPart.Parent)
+        local player = Players:GetPlayerFromCharacter(touchedPart.Parent)
 
-            if player and not playersTouched[player] then
-                playersTouched[player] = true
+        if player and not playersTouched[player] then
+            playersTouched[player] = true
 
-                local success = Teleport.toLocation(player, enum)
-
-                if not success then
-                    -- Stuff to do if teleport fails
-                end
-            end
+            Teleport.toLocation(player, enum, nil, function()
+                playersTouched[player] = nil
+            end):catch(function(err)
+                warn(err)
+                playersTouched[player] = nil
+            end)
         end
     end)
 end
