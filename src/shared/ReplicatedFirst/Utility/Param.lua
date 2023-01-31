49903bf6e076fd32f84b90d757ebe8aeb0f4f7f9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:49:49 -0500

    Add Param.expect

--- src/shared/ReplicatedFirst/Utility/Param.lua
+++ src/shared/ReplicatedFirst/Utility/Param.lua
@@ -12,10 +12,48 @@ local PlayerFormat = require(enumsFolder:WaitForChild("PlayerFormat"))
 local Types = require(utilityFolder:WaitForChild("Types"))
 local LocalServerInfo = require(serverFolder:WaitForChild("LocalServerInfo"))
 local Promise = require(utilityFolder:WaitForChild("Promise"))
+local ResponseType = require(enumsFolder:WaitForChild("ResponseType"))
 
 local serverInfoPromise = LocalServerInfo.getServerInfo()
 
 local Param = {}
+
+--[[
+	Takes in a list of lists. Each list begins with an object, and the rest of the list is a list of possible types for that object.
+	Possible types can also be instances.
+	This will return a rejected promise if any objects are not of the correct type.
+	Example:
+	```lua
+	Param.expect({1, "number"}, {"hello", "string"}, {true, "boolean", "Part"}) -- returns a rejected promise
+	```
+]]
+function Param.expect(...) -- desired types are put after the object in a list. Example: {1, "number", "string"}
+	local t = {...}
+	
+	for _, v in ipairs(t) do
+		local obj = v[1]
+		local types = table.remove(v, 1) and v
+
+		assert(#types > 0, "No types provided")
+
+		local objType = typeof(obj)
+
+		local found = false
+
+		for _, typ in ipairs(types) do
+			if objType == typ or (objType == "Instance" and obj:IsA(typ)) then
+				found = true
+				break
+			end
+		end
+
+		if not found then
+			return Promise.reject(ResponseType.invalid)
+		end
+	end
+
+	return Promise.resolve()
+end
 
 function Param.playerParam(playerParam: Types.PlayerParam, format: Types.UserEnum, useHomeOwner: boolean): Types.Promise
 	return (if useHomeOwner then serverInfoPromise else Promise.resolve())
@@ -33,7 +71,7 @@ function Param.playerParam(playerParam: Types.PlayerParam, format: Types.UserEnu
 			end
 		end)
 		:andThen(function(player)
-			return player or Promise.reject("Player not found")
+			return player or Promise.reject(ResponseType.invalid)
 		end)
 		:catch(function(err)
 			warn("Param.playerParam failed:", err)
