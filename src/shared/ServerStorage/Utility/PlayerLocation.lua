78884e54048a614280bafd8f4c2089c19dc04616
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:55:52 -0500

    Promisify

--- src/shared/ServerStorage/Utility/PlayerLocation.lua
+++ src/shared/ServerStorage/Utility/PlayerLocation.lua
@@ -1,87 +1,93 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 
+local replicatedFirstShared = ReplicatedFirst.Shared
 local replicatedStorageShared = ReplicatedStorage.Shared
 local enumsFolder = replicatedStorageShared.Enums
+local utilityFolder = replicatedFirstShared.Utility
 
 local LiveServerData = require(replicatedStorageShared.Server.LiveServerData)
 local ServerTypeEnum = require(enumsFolder.ServerType)
+local Types = require(utilityFolder.Types)
+
+type Promise = Types.Promise
 
 local PlayerLocation = {}
 
-function PlayerLocation.get(playerId)
-    local serverData = LiveServerData.get()
+function PlayerLocation.get(playerId): Promise
+    return LiveServerData.get():andThen(function(serverData)
+        local playerLocationTable
 
-    local playerLocationTable
-
-    for serverType, serverTypeData in pairs(serverData) do
-        if serverType == ServerTypeEnum.routing then
-            for jobId, serverInfo in pairs(serverTypeData) do
-                if table.find(serverInfo.players, playerId) then
-                    playerLocationTable = {
-                        serverType = serverType,
-                        jobId = jobId,
-                    }
-
-                    break
-                end
-            end
-        elseif serverType == ServerTypeEnum.location then
-            for worldIndex, worldData in pairs(serverTypeData) do
-                for locationEnum, serverInfo in pairs(worldData) do
+        for serverType, serverTypeData in pairs(serverData) do
+            if serverType == ServerTypeEnum.routing then
+                for jobId, serverInfo in pairs(serverTypeData) do
                     if table.find(serverInfo.players, playerId) then
                         playerLocationTable = {
                             serverType = serverType,
-                            worldIndex = worldIndex,
-                            locationEnum = locationEnum,
+                            jobId = jobId,
                         }
 
                         break
                     end
                 end
-            end
-        elseif serverType == ServerTypeEnum.home then
-            for userId, serverInfo in pairs(serverTypeData) do
-                if table.find(serverInfo.players, playerId) then
-                    playerLocationTable = {
-                        serverType = serverType,
-                        homeOwner = userId,
-                    }
+            elseif serverType == ServerTypeEnum.location then
+                for worldIndex, worldData in pairs(serverTypeData) do
+                    for locationEnum, serverInfo in pairs(worldData) do
+                        if table.find(serverInfo.players, playerId) then
+                            playerLocationTable = {
+                                serverType = serverType,
+                                worldIndex = worldIndex,
+                                locationEnum = locationEnum,
+                            }
 
-                    break
+                            break
+                        end
+                    end
                 end
-            end
-        elseif serverType == ServerTypeEnum.party then
-            for partyType, partyTypeData in pairs(serverTypeData) do
-                for partyIndex, serverInfo in pairs(partyTypeData) do
+            elseif serverType == ServerTypeEnum.home then
+                for userId, serverInfo in pairs(serverTypeData) do
                     if table.find(serverInfo.players, playerId) then
                         playerLocationTable = {
                             serverType = serverType,
-                            partyType = partyType,
-                            partyIndex = partyIndex,
+                            homeOwner = userId,
                         }
 
                         break
                     end
                 end
-            end
-        elseif serverType == ServerTypeEnum.game then
-            for gameType, gameTypeData in pairs(serverTypeData) do
-                for gameIndex, serverInfo in pairs(gameTypeData) do
-                    if table.find(serverInfo.players, playerId) then
-                        playerLocationTable = {
-                            serverType = serverType,
-                            gameType = gameType,
-                            gameIndex = gameIndex,
-                        }
+            elseif serverType == ServerTypeEnum.party then
+                for partyType, partyTypeData in pairs(serverTypeData) do
+                    for partyIndex, serverInfo in pairs(partyTypeData) do
+                        if table.find(serverInfo.players, playerId) then
+                            playerLocationTable = {
+                                serverType = serverType,
+                                partyType = partyType,
+                                partyIndex = partyIndex,
+                            }
 
-                        break
+                            break
+                        end
+                    end
+                end
+            elseif serverType == ServerTypeEnum.game then
+                for gameType, gameTypeData in pairs(serverTypeData) do
+                    for gameIndex, serverInfo in pairs(gameTypeData) do
+                        if table.find(serverInfo.players, playerId) then
+                            playerLocationTable = {
+                                serverType = serverType,
+                                gameType = gameType,
+                                gameIndex = gameIndex,
+                            }
+
+                            break
+                        end
                     end
                 end
             end
         end
-    end
 
-    return playerLocationTable
+        return playerLocationTable
+    end)
 end
 
 return PlayerLocation
