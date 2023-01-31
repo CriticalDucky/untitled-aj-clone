1829a2afe905813feab9d674081e8440f15b916c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-01-31 10:42:53 -0500

    Remove TempDataTemplate

--- src/Locations/Town/ServerStorage/Test/TempDataTemplate.lua
+++
@@ -1,8 +0,0 @@
-local replicationType = require(game.ReplicatedStorage.Shared.Enums.ReplicationType)
-
-return {
-    awards = {
-        _replication = replicationType.private,
-        points = 0
-    }
-}
