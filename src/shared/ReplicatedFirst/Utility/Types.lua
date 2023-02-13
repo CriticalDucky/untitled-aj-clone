7ab7f4e0efacebac808f821ca5b59dedcdee53ed
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-13 15:14:17 -0500

    Add time types

--- src/shared/ReplicatedFirst/Utility/Types.lua
+++ src/shared/ReplicatedFirst/Utility/Types.lua
@@ -1,56 +1,85 @@
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local RunService = game:GetService("RunService")
 
-local Promise = require(ReplicatedFirst:WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("Promise"))
+local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
+local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+
+local Promise = require(utilityFolder:WaitForChild("Promise"))
 
 local isServer = RunService:IsServer()
 
 export type UserEnum = string | number
 
 export type PlayerData = {
-    setValue: (PlayerData, path: {}, value: any) -> nil,
-    setValues: (PlayerData, path: {}, values: {}) -> nil,
-    arrayInsert: (PlayerData, path: {}, value: any) -> nil,
-    arraySet: (PlayerData, path: {}, index: number, value: any) -> nil,
-    arrayRemove: (PlayerData, path: {}, index: number) -> nil,
-    player: Player,
-    profile: table,
+	setValue: (PlayerData, path: {}, value: any) -> nil,
+	setValues: (PlayerData, path: {}, values: {}) -> nil,
+	arrayInsert: (PlayerData, path: {}, value: any) -> nil,
+	arraySet: (PlayerData, path: {}, index: number, value: any) -> nil,
+	arrayRemove: (PlayerData, path: {}, index: number) -> nil,
+	player: Player,
+	profile: table,
+}
+
+export type TimeRange = {
+	introduction: number | table,
+	closing: number | table,
+	isInRange: (TimeRange, time: number?) -> boolean,
+	distanceToClosing: (TimeRange, time: number?) -> number,
+	distanceToIntroduction: (TimeRange, time: number?) -> number,
+}
+
+export type TimeInfo = number | (
+) -> TimeInfo | {
+	year: number?,
+	month: number?,
+	day: number?,
+	hour: number?,
+	min: number?,
+	sec: number?,
+}
+
+export type PartyUnit = {
+    partyType: UserEnum,
+    halfHourId: number,
+    time: TimeRange,
 }
 
 export type HomeServerInfo = {
-    serverCode: string,
-    privateServerId: string,
+	serverCode: string,
+	privateServerId: string,
 }
 
 export type InventoryItem = {
 	id: string,
 	itemCategory: UserEnum,
 	itemEnum: string | number,
-    placedItems: {}?,
-    permanent: boolean?,
+	placedItems: {}?,
+	permanent: boolean?,
 }
 
 export type PlacedItem = {
-    itemId: string,
-    pivotCFrame: CFrame | table
+	itemId: string,
+	pivotCFrame: CFrame | table,
 }
 
 export type PlayerParam = Player | number
 
 export type HomeOwnerParam = PlayerParam | nil
 
+export type LocalPlayerParam = PlayerParam | nil
+
 export type Promise = typeof(Promise.new(function() end))
 
 export type ServerIdentifier = {
-    serverType: UserEnum,
-    jobId: string?,
-    worldIndex: number?,
-    locationEnum: UserEnum?,
-    homeOwner: number?,
-    partyType: UserEnum?,
-    partyIndex: number?,
-    gameType: UserEnum?,
-    gameIndex: number?,
+	serverType: UserEnum,
+	jobId: string?,
+	worldIndex: number?,
+	locationEnum: UserEnum?,
+	homeOwner: number?,
+	partyType: UserEnum?,
+	partyIndex: number?,
+	gameType: UserEnum?,
+	gameIndex: number?,
 }
 
 return nil
