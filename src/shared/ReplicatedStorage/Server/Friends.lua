6dd1aec05dd2b3bcdc6bab90b73dd15a06e64fe9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-17 16:55:18 -0500

    Remove bloaty Friends script

--- src/shared/ReplicatedStorage/Server/Friends.lua
+++
@@ -1,5 +0,0 @@
-local Players = game:GetService("Players")
-
-local friends = require(game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("GetFriends"))(Players.LocalPlayer.UserId)
-
-return friends
