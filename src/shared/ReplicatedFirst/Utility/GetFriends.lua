4571a60647f82ca1ce6e15e5a044f5d765d40f7e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-19 13:16:27 -0500

    Promisify GetFriends

--- src/shared/ReplicatedFirst/Utility/GetFriends.lua
+++ src/shared/ReplicatedFirst/Utility/GetFriends.lua
@@ -1,45 +1,66 @@
+--[[
+    Returns a cached list of friends for a player.
+    Items in the list are as follows:
+
+    Id	        int64	    The user ID of the friend
+    Username	string	    The current username of the friend
+    IsOnline	boolean	    Whether or not the user is presently online.
+]]
+
 local Players = game:GetService("Players")
+
+local utilityFolder = game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Utility")
+local enumsFolder = game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Enums")
+
+local Promise = require(utilityFolder:WaitForChild("Promise"))
+local Types = require(utilityFolder:WaitForChild("Types"))
+local Param = require(utilityFolder:WaitForChild("Param"))
+local PlayerFormat = require(enumsFolder:WaitForChild("PlayerFormat"))
+
+type LocalPlayerParam = Types.LocalPlayerParam
 
 local cachedFriends = {}
 
-local getFriends = function(playerId)
-    local function fetchFriends()
-        local function iterPageItems(pages: FriendPages)
-            return coroutine.wrap(function()
-                local pagenum = 1
-                while true do
-                    for _, item in ipairs(pages:GetCurrentPage()) do
-                        coroutine.yield(item, pagenum)
-                    end
-                    if pages.IsFinished then
-                        break
-                    end
-                    pages:AdvanceToNextPageAsync()
-                    pagenum = pagenum + 1
-                end
-            end)
-        end
-    
-        local success, friendPages = pcall(function()
-            return Players:GetFriendsAsync(playerId)
-        end)
+local getFriends = function(player: LocalPlayerParam)
+	return Param.localPlayerParam(player, PlayerFormat.userId):andThen(function(userId)
+		local function fetchFriends()
+			local function iterPageItems(pages: FriendPages)
+				return coroutine.wrap(function()
+					local pagenum = 1
+					while true do
+						for _, item in ipairs(pages:GetCurrentPage()) do
+							coroutine.yield(item, pagenum)
+						end
+						if pages.IsFinished then
+							break
+						end
+						pages:AdvanceToNextPageAsync()
+						pagenum = pagenum + 1
+					end
+				end)
+			end
 
-        if not success then
-            return {}
-        end
-    
-        local list = {}
-    
-        for item, _ in iterPageItems(friendPages) do
-            table.insert(list, item)
-        end
+			local success, friendPages = pcall(function()
+				return Players:GetFriendsAsync(userId)
+			end)
 
-        cachedFriends[playerId] = list
+			if not success then
+				return {}
+			end
 
-        return list
-    end
+			local list = {}
 
-    return cachedFriends[playerId] or fetchFriends()
+			for item, _ in iterPageItems(friendPages) do
+				table.insert(list, item)
+			end
+
+			cachedFriends[player] = list
+
+			return list
+		end
+
+		return Promise.resolve(cachedFriends[player]) or Promise.try(fetchFriends)
+	end)
 end
 
 return getFriends
