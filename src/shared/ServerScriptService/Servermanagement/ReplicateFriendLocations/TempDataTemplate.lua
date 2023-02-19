8a342f2b523ee824ffc3ee4586472f9871f86e1e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-19 13:16:57 -0500

    Remove overwritten TempDataTemplate

--- src/shared/ServerScriptService/Servermanagement/ReplicateFriendLocations/TempDataTemplate.lua
+++
@@ -1,8 +0,0 @@
-local ReplicationType = require(game:GetService("ReplicatedStorage").Shared.Enums.ReplicationType)
-
-return {
-    friendLocations = {
-        _replication = ReplicationType.private,
-        locations = {}
-    }
-}
