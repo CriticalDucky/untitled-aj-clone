7615a780b27c37e757ec404ef7235d9abd6e1c8c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-19 13:16:46 -0500

    Improve FriendLocations

--- src/shared/ReplicatedStorage/Server/FriendLocations.lua
+++ src/shared/ReplicatedStorage/Server/FriendLocations.lua
@@ -4,15 +4,25 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
 local dataFolder = replicatedStorageShared:WaitForChild("Data")
+local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
 
 local ClientPlayerData = require(dataFolder:WaitForChild("ClientPlayerData"))
 local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
+local Types = require(utilityFolder:WaitForChild("Types"))
+local Table = require(utilityFolder:WaitForChild("Table"))
 local Computed = Fusion.Computed
 
-local playerData = ClientPlayerData.getData(true)
+type ProfileData = Types.ProfileData
+type ServerIdentifier = Types.ServerIdentifier
 
+local playerData = ClientPlayerData.getData()
+
+-- Gets a table of locations of friends. Warning: This can be nil.
+-- Returns a promise with a table of Server Identifiers.
 local FriendLocations = Computed(function()
-    return playerData:get().friendLocations.locations
+    local data: ProfileData = playerData:getNow()
+
+    return Table.safeIndex(data, "friendLocations", "locations") :: {[string]: ServerIdentifier}
 end)
 
 return FriendLocations
