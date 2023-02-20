48509e466f1cd133f84735f20cf178950f5b326d
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-20 17:39:20 -0500

    Add typing

--- src/shared/ReplicatedFirst/UI/Components/Testing/Hotbar/Home/HomeButton.lua
+++ src/shared/ReplicatedFirst/UI/Components/Testing/Hotbar/Home/HomeButton.lua
@@ -1,21 +1,24 @@
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local Players = game:GetService "Players"
 
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
-local UIFolder = replicatedFirstShared:WaitForChild("UI")
-local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
-local serverFolder = replicatedStorageShared:WaitForChild("Server")
-local requestsFolder = replicatedStorageShared:WaitForChild("Requests")
-local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
+local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
+local UIFolder = replicatedFirstShared:WaitForChild "UI"
+local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
+local serverFolder = replicatedStorageShared:WaitForChild "Server"
+local requestsFolder = replicatedStorageShared:WaitForChild "Requests"
+local enumsFolder = replicatedStorageShared:WaitForChild "Enums"
 
-local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
-local ClientTeleport = require(requestsFolder:WaitForChild("Teleportation"):WaitForChild("ClientTeleport"))
-local ClientServerData = require(serverFolder:WaitForChild("ClientServerData"))
-local ServerTypeGroups = require(serverFolder:WaitForChild("ServerTypeGroups"))
-local ServerGroupEnum = require(enumsFolder:WaitForChild("ServerGroup"))
-local ReponseType = require(enumsFolder:WaitForChild("ResponseType"))
+local Fusion = require(replicatedFirstShared:WaitForChild "Fusion")
+local ClientTeleport = require(requestsFolder:WaitForChild("Teleportation"):WaitForChild "ClientTeleport")
+local ClientServerData = require(serverFolder:WaitForChild "ClientServerData")
+local ServerTypeGroups = require(serverFolder:WaitForChild "ServerTypeGroups")
+local ServerGroupEnum = require(enumsFolder:WaitForChild "ServerGroup")
+local ReponseType = require(enumsFolder:WaitForChild "ResponseType")
+local Types = require(utilityFolder:WaitForChild "Types")
+
+type ServerIdentifier = Types.ServerIdentifier
 
 local Value = Fusion.Value
 local New = Fusion.New
@@ -32,47 +35,47 @@ local unwrap = Fusion.unwrap
 local player = Players.LocalPlayer
 
 local component = function(props)
-    local visible = Value(false)
-    local errored = Value(false)
+	local visible = Value(false)
+	local errored = Value(false)
 
-    if ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
-        ClientServerData.getServerInfo():andThen(function(serverInfo)
-            visible:set(serverInfo.homeOwner ~= player.UserId)
-        end)
-    else
-        visible:set(true)
-    end
+	if ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
+		ClientServerData.getServerInfo():andThen(function(serverInfo: ServerIdentifier)
+			visible:set(serverInfo.homeOwner ~= player.UserId)
+		end)
+	else
+		visible:set "true"
+	end
 
-    return New "TextButton" {
-        Size = UDim2.fromOffset(75, 75),
-        LayoutOrder = 50,
-        Visible = visible,
-        BackgroundColor3 = Computed(function()
-            if errored:get() then
-                return Color3.fromRGB(255, 102, 102)
-            else
-                return Color3.fromRGB(255, 255, 255)
-            end
-        end),
+	return New "TextButton" {
+		Size = UDim2.fromOffset(75, 75),
+		LayoutOrder = 50,
+		Visible = visible,
+		BackgroundColor3 = Computed(function()
+			if errored:get() then
+				return Color3.fromRGB(255, 102, 102)
+			else
+				return Color3.fromRGB(255, 255, 255)
+			end
+		end),
 
-        Text = "Home",
-        Font = Enum.Font.Gotham,
-        TextSize = 18,
+		Text = "Home",
+		Font = Enum.Font.Gotham,
+		TextSize = 18,
 
-        [OnEvent "MouseButton1Click"] = function()
-            ClientTeleport.toHome(player.UserId):andThen(function(response)
-                if response ~= ReponseType.success then
-                    errored:set(true)
-                end
-            end)
-        end,
+		[OnEvent "MouseButton1Click"] = function()
+			ClientTeleport.toHome(player.UserId):andThen(function(response)
+				if response ~= ReponseType.success then
+					errored:set(true)
+				end
+			end)
+		end,
 
-        [Children] = {
-            New "UICorner" {
-                CornerRadius = UDim.new(0, 5)
-            },
-        },
-    }
+		[Children] = {
+			New "UICorner" {
+				CornerRadius = UDim.new(0, 5),
+			},
+		},
+	}
 end
 
 return component
