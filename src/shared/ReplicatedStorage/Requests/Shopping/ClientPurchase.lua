71398493d5f58fc0d41f10d988f3b68bdec9ed43
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-20 20:22:10 -0500

    Promisify ClientPurchase

--- src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
+++ src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
@@ -1,42 +1,29 @@
 local REQUEST_TIMEOUT = 10
 
-local HttpService = game:GetService("HttpService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local HttpService = game:GetService "HttpService"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
-local dataFolder = replicatedStorageShared:WaitForChild("Data")
-local shopInfoFolder = dataFolder:WaitForChild("ShopInfo")
-local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
-local requestsFolder = replicatedStorageShared:WaitForChild("Requests")
+local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local replicationFolder = replicatedStorageShared:WaitForChild "Replication"
+local dataFolder = replicatedStorageShared:WaitForChild "Data"
+local shopInfoFolder = dataFolder:WaitForChild "ShopInfo"
+local enumsFolder = replicatedStorageShared:WaitForChild "Enums"
+local requestsFolder = replicatedStorageShared:WaitForChild "Requests"
 
-local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
-local PurchaseResponseType = require(enumsFolder:WaitForChild("PurchaseResponseType"))
-local ReplicaRequest = require(requestsFolder:WaitForChild("ReplicaRequest"))
-local Shops = require(shopInfoFolder:WaitForChild("Shops"))
+local ReplicaCollection = require(replicationFolder:WaitForChild "ReplicaCollection")
+local ResponseType = require(enumsFolder:WaitForChild "ResponseType")
+local ReplicaRequest = require(requestsFolder:WaitForChild "ReplicaRequest")
+local Shops = require(shopInfoFolder:WaitForChild "Shops")
 
 local Purchase = {}
 
 function Purchase.request(shopEnum, itemIndex)
-    assert(shopEnum and Shops[shopEnum], "shop must be a valid shop type")
-    assert(itemIndex and Shops[shopEnum].items[itemIndex], "item must be a valid item")
+	assert(shopEnum and Shops[shopEnum], "shop must be a valid shop type")
+	assert(itemIndex and Shops[shopEnum].items[itemIndex], "item must be a valid item")
 
-    local PurchaseRequest = ReplicaCollection.get("PurchaseRequest")
-
-    if not PurchaseRequest then
-        warn("PurchaseRequest not found")
-        return
-    end
-
-    local item = Shops[shopEnum].items[itemIndex]
-    local isForSale = item.sellingTime and item.sellingTime:isInRange()
-
-    if not isForSale then
-        warn("Item is not for sale")
-        return
-    end
-
-    return ReplicaRequest.new(PurchaseRequest, shopEnum, itemIndex) or PurchaseResponseType.timeout
+	return ReplicaCollection.get("PurchaseRequest", true):andThen(function(purchaseRequest)
+		return ReplicaRequest.new(purchaseRequest, shopEnum, itemIndex)
+	end)
 end
 
 return Purchase
