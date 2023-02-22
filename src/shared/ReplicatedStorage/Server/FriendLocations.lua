ae54be19cb86a7f6fbcd10b439719259c9fe11e3
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-02-21 21:11:32 -0500

    Remove computed impulsiveness

--- src/shared/ReplicatedStorage/Server/FriendLocations.lua
+++ src/shared/ReplicatedStorage/Server/FriendLocations.lua
@@ -1,28 +1,24 @@
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
-local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local dataFolder = replicatedStorageShared:WaitForChild("Data")
-local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
+local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local dataFolder = replicatedStorageShared:WaitForChild "Data"
+local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
 
-local ClientPlayerData = require(dataFolder:WaitForChild("ClientPlayerData"))
-local Fusion = require(replicatedFirstShared:WaitForChild("Fusion"))
-local Types = require(utilityFolder:WaitForChild("Types"))
-local Table = require(utilityFolder:WaitForChild("Table"))
-local Computed = Fusion.Computed
+local ClientPlayerData = require(dataFolder:WaitForChild "ClientPlayerData")
+local Types = require(utilityFolder:WaitForChild "Types")
+local Table = require(utilityFolder:WaitForChild "Table")
 
 type ProfileData = Types.ProfileData
 type ServerIdentifier = Types.ServerIdentifier
 
-local playerData = ClientPlayerData.getData()
+local FriendLocations = {}
 
--- Gets a table of locations of friends. Warning: This can be nil.
--- Returns a promise with a table of Server Identifiers.
-local FriendLocations = Computed(function()
-    local data: ProfileData = playerData:getNow()
+function FriendLocations.get()
+    local data: ProfileData = ClientPlayerData.getData():getNow()
 
-    return Table.safeIndex(data, "friendLocations", "locations") :: {[string]: ServerIdentifier} or {}
-end)
+    return Table.safeIndex(data, "friendLocations", "locations") :: { [string]: ServerIdentifier } or {}
+end
 
 return FriendLocations
