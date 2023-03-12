1b5bed9c4e8233cfefb7c2c586fe10dc773fff93
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-12 12:40:21 -0400

    Add utility functions

--- src/shared/ReplicatedStorage/Server/Locations.lua
+++ src/shared/ReplicatedStorage/Server/Locations.lua
@@ -1,20 +1,24 @@
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
+local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
+local settingsFolder = replicatedFirstShared:WaitForChild("Settings")
 local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
 
 local LocationTypeEnum = require(enumsFolder:WaitForChild("LocationType"))
+local GameSettings = require(settingsFolder:WaitForChild("GameSettings"))
 
 local Locations = {
     info = {
         [LocationTypeEnum.town] = {
             name = "Town",
             placeId = 10189748812,
-            -- populationInfo = {
-            --     max = 100,
-            --     recommended = 50,
-            -- },
-            -- cantJoinPlayer = bool
+            populationInfo = { -- Example; optional
+                max = GameSettings.location_maxPlayers,
+                recommended = GameSettings.location_maxRecommendedPlayers,
+            },
+            cantJoinPlayer = false
         },
     
         [LocationTypeEnum.forest] = {
@@ -28,5 +32,61 @@ local Locations = {
         LocationTypeEnum.forest,
     },
 }
+
+function Locations.getMaxPlayerCount(locationEnum)
+    assert(locationEnum, "locationEnum is nil")
+
+    local locationInfo = Locations.info[locationEnum]
+
+    if locationInfo then
+        local populationInfo = locationInfo.populationInfo
+
+        if populationInfo then
+            return populationInfo.max
+        end
+    else
+        error("Invalid location enum: " .. tostring(locationEnum))
+    end
+
+    return GameSettings.location_maxPlayers
+end
+
+function Locations.getRecommendedPlayerCount(locationEnum)
+    assert(locationEnum, "locationEnum is nil")
+
+    local locationInfo = Locations.info[locationEnum]
+
+    if locationInfo then
+        local populationInfo = locationInfo.populationInfo
+
+        if populationInfo then
+            return populationInfo.recommended
+        end
+    else
+        error("Invalid location enum: " .. tostring(locationEnum))
+    end
+
+    return GameSettings.location_maxRecommendedPlayers
+end
+
+function Locations.getWorldMaxPlayerCount()
+    local maxPlayerCount = 0
+
+    for _, locationEnum in ipairs(Locations.priority) do
+        maxPlayerCount += Locations.getMaxPlayerCount(locationEnum)
+    end
+
+    return maxPlayerCount
+end
+
+function Locations.getWorldRecommendedPlayerCount()
+    local recommendedPlayerCount = 0
+
+    for _, locationEnum in ipairs(Locations.priority) do
+        recommendedPlayerCount += Locations.getRecommendedPlayerCount(locationEnum)
+    end
+
+    return recommendedPlayerCount
+end
 
 return Locations
