c40b478e930464dd4b9cd9d9c7196637006ba912
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-13 21:55:46 -0400

    De-promisify

--- src/shared/ReplicatedStorage/Data/Inventory/ClientInventoryManager.lua
+++ src/shared/ReplicatedStorage/Data/Inventory/ClientInventoryManager.lua
@@ -1,3 +1,10 @@
+--[[
+	Provides an inventory interface for the client.
+	Wrapper for ReplicatedPlayerData.lua.
+
+	See GameSettings.lua to see how player data is structured and replicated.
+]]
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 
@@ -6,8 +13,7 @@ local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
 local dataFolder = replicatedStorageShared:WaitForChild("Data")
 local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
 
-local ClientPlayerData = require(dataFolder:WaitForChild("ClientPlayerData"))
-local Promise = require(utilityFolder:WaitForChild("Promise"))
+local ReplicatedPlayerData = require(dataFolder:WaitForChild("ReplicatedPlayerData"))
 local Types = require(utilityFolder:WaitForChild("Types"))
 
 type Promise = Types.Promise
@@ -17,21 +23,25 @@ type UserEnum = Types.UserEnum
 local ClientInventoryManager = {}
 
 --[[
-    Returns a promise that resolves to the player's inventory.
+	Gets a player's inventory. If the player data is not replicated, this will return nil unless wait is true.
 ]]
-function ClientInventoryManager.getInventory(player: LocalPlayerParam): Promise
-	return ClientPlayerData.getData(player):andThen(function(data)
+function ClientInventoryManager.getInventory(player: Player | number | nil, wait: boolean)
+	local data = ReplicatedPlayerData.get(player, wait)
+
+	if data then
 		return data.inventory
-	end)
+	end
 end
 
 --[[
-    Returns a promise that resolves to the player's inventory category.
+	Gets a player's inventory category. If the player data is not replicated, this will return nil unless wait is true.
 ]]
-function ClientInventoryManager.getInventoryCategory(player: LocalPlayerParam, category: UserEnum): Promise
-	return ClientInventoryManager.getInventory(player):andThen(function(inventory)
+function ClientInventoryManager.getInventoryCategory(player: Player | number | nil, category: UserEnum, wait: boolean)
+	local inventory = ClientInventoryManager.getInventory(player, wait)
+
+	if inventory then
 		return inventory[category]
-	end)
+	end
 end
 
 return ClientInventoryManager
