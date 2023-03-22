bc136cf269cc04a52e3798966a4ce79547004b1f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-21 20:34:20 -0400

    Add PlaceItemResponseType.lua

---
+++ src/shared/ReplicatedStorage/Enums/PlaceItemResponseType.lua
@@ -0,0 +1,5 @@
+return {
+    error = "PIRT_E", -- Something went wrong that were not in control of
+    invalid = "PIRT_I", -- The request was invalid. This error code is flexible and can be used for many different reasons
+    success = "PIRT_SUCCESS", -- The request was successful :D
+}
