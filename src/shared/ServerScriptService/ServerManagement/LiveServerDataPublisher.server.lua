1e036675b9a3eeeaba15509a500d20c15488b42b
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-21 20:35:51 -0400

    Remove promise use and update syntax

--- src/shared/ServerScriptService/ServerManagement/LiveServerDataPublisher.server.lua
+++ src/shared/ServerScriptService/ServerManagement/LiveServerDataPublisher.server.lua
@@ -1,8 +1,8 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local ServerStorage = game:GetService("ServerStorage")
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local RunService = game:GetService "RunService"
+local Players = game:GetService "Players"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ServerStorage = game:GetService "ServerStorage"
 
 local replicatedStorageShared = ReplicatedStorage.Shared
 local replicatedFirstShared = ReplicatedFirst.Shared
@@ -17,90 +17,43 @@ local ServerGroupEnum = require(enumsFolder.ServerGroup)
 local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
 local Promise = require(utilityFolder.Promise)
 
-Promise.resolve()
-    :andThen(function()
-        if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
-            return {
-                jobId = game.JobId,
-            }
-        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
-            return LocalServerInfo.getServerIdentifier()
-                :andThen(function(serverInfo)
-                    return {
-                        worldIndex = serverInfo.worldIndex,
-                        locationEnum = serverInfo.locationEnum,
-                    }
-                end)
-        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
-            return LocalServerInfo.getServerIdentifier()
-                :andThen(function(serverInfo)
-                    return {
-                        homeOwner = serverInfo.homeOwner,
-                    }
-                end)
-        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
-            return LocalServerInfo.getServerIdentifier()
-                :andThen(function(serverInfo)
-                    return {
-                        partyType = serverInfo.partyType,
-                        partyIndex = serverInfo.partyIndex,
-                    }
-                end)
-        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isGame) then
-            return LocalServerInfo.getServerIdentifier()
-                :andThen(function(serverInfo)
-                    return {
-                        gameType = serverInfo.gameType,
-                        gameIndex = serverInfo.gameIndex,
-                    }
-                end)
-        end
-    end)
-    :andThen(function(indexInfo)
-        local runServiceConnection = RunService.Heartbeat:Connect(function()
-            if LiveServerData.canPublish() then
-                local serverInfo
-        
-                local function getUserIds()
-                    local userIds = {}
-        
-                    for _, player in pairs(Players:GetPlayers()) do
-                        table.insert(userIds, player.UserId)
-                    end
-        
-                    return userIds
-                end
-        
-                serverInfo = {}
-        
-                --#region Optional custom server info
-        
-                if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
-        
-                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
-        
-                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
-        
-                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
-        
-                elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isGame) then
-        
-                end
-        
-                --#endregion
-        
-                serverInfo.players = getUserIds()
-        
-                LiveServerData.publish(serverInfo, indexInfo)
+local serverIdentifier = LocalServerInfo.getServerIdentifier()
+
+local runServiceConnection = RunService.Heartbeat:Connect(function()
+    if LiveServerData.canPublish() then
+        local serverInfo
+
+        local function getUserIds()
+            local userIds = {}
+
+            for _, player in pairs(Players:GetPlayers()) do
+                table.insert(userIds, player.UserId)
             end
-        end)
-        
-        game:BindToClose(function()
-            runServiceConnection:Disconnect() -- Disconnect heartbeat connection so that no other publish requests are made after this
 
-            task.delay(1, function()
-                LiveServerData.publish(nil, indexInfo)
-            end)
-        end)
+            return userIds
+        end
+
+        serverInfo = {}
+
+        --Optional custom server info
+
+        if ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isParty) then
+        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.isGame) then
+        end
+
+        serverInfo.players = getUserIds()
+
+        LiveServerData.publish(serverIdentifier, serverInfo)
+    end
+end)
+
+game:BindToClose(function()
+    runServiceConnection:Disconnect() -- Disconnect heartbeat connection so that no other publish requests are made after this
+
+    task.delay(1, function()
+        LiveServerData.publish(serverIdentifier, nil)
     end)
-
+end)
