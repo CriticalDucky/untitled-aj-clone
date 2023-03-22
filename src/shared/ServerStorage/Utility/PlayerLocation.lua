6860fcddd6e3073a13aade8f46014730fefc31dd
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-21 20:37:06 -0400

    Remove Promise use

--- src/shared/ServerStorage/Utility/PlayerLocation.lua
+++ src/shared/ServerStorage/Utility/PlayerLocation.lua
@@ -1,93 +1,91 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
-local replicatedFirstShared = ReplicatedFirst.Shared
 local replicatedStorageShared = ReplicatedStorage.Shared
 local enumsFolder = replicatedStorageShared.Enums
-local utilityFolder = replicatedFirstShared.Utility
 
 local LiveServerData = require(replicatedStorageShared.Server.LiveServerData)
 local ServerTypeEnum = require(enumsFolder.ServerType)
-local Types = require(utilityFolder.Types)
-
-type Promise = Types.Promise
 
 local PlayerLocation = {}
 
-function PlayerLocation.get(playerId): Promise
-    return LiveServerData.get():andThen(function(serverData)
-        local playerLocationTable
+-- Gets the server identifier of the specified player, if they are online.
+-- If they are not online, returns nil.
+function PlayerLocation.get(userId: number)
+	local liveServerData = LiveServerData.get()
 
-        for serverType, serverTypeData in pairs(serverData) do
-            if serverType == ServerTypeEnum.routing then
-                for jobId, serverInfo in pairs(serverTypeData) do
-                    if table.find(serverInfo.players, playerId) then
-                        playerLocationTable = {
-                            serverType = serverType,
-                            jobId = jobId,
-                        }
+	if not liveServerData then return nil end
 
-                        break
-                    end
-                end
-            elseif serverType == ServerTypeEnum.location then
-                for worldIndex, worldData in pairs(serverTypeData) do
-                    for locationEnum, serverInfo in pairs(worldData) do
-                        if table.find(serverInfo.players, playerId) then
-                            playerLocationTable = {
-                                serverType = serverType,
-                                worldIndex = worldIndex,
-                                locationEnum = locationEnum,
-                            }
+	local playerServerIdentifier
 
-                            break
-                        end
-                    end
-                end
-            elseif serverType == ServerTypeEnum.home then
-                for userId, serverInfo in pairs(serverTypeData) do
-                    if table.find(serverInfo.players, playerId) then
-                        playerLocationTable = {
-                            serverType = serverType,
-                            homeOwner = userId,
-                        }
+	for serverType, serverTypeData in liveServerData do
+		if serverType == ServerTypeEnum.routing then
+			for jobId, serverInfo in pairs(serverTypeData) do
+				if table.find(serverInfo.players, userId) then
+					playerServerIdentifier = {
+						serverType = serverType,
+						jobId = jobId,
+					}
 
-                        break
-                    end
-                end
-            elseif serverType == ServerTypeEnum.party then
-                for partyType, partyTypeData in pairs(serverTypeData) do
-                    for partyIndex, serverInfo in pairs(partyTypeData) do
-                        if table.find(serverInfo.players, playerId) then
-                            playerLocationTable = {
-                                serverType = serverType,
-                                partyType = partyType,
-                                partyIndex = partyIndex,
-                            }
+					break
+				end
+			end
+		elseif serverType == ServerTypeEnum.location then
+			for worldIndex, worldData in pairs(serverTypeData) do
+				for locationEnum, serverInfo in pairs(worldData) do
+					if table.find(serverInfo.players, userId) then
+						playerServerIdentifier = {
+							serverType = serverType,
+							worldIndex = worldIndex,
+							locationEnum = locationEnum,
+						}
 
-                            break
-                        end
-                    end
-                end
-            elseif serverType == ServerTypeEnum.game then
-                for gameType, gameTypeData in pairs(serverTypeData) do
-                    for gameIndex, serverInfo in pairs(gameTypeData) do
-                        if table.find(serverInfo.players, playerId) then
-                            playerLocationTable = {
-                                serverType = serverType,
-                                gameType = gameType,
-                                gameIndex = gameIndex,
-                            }
+						break
+					end
+				end
+			end
+		elseif serverType == ServerTypeEnum.home then
+			for userId, serverInfo in pairs(serverTypeData) do
+				if table.find(serverInfo.players, userId) then
+					playerServerIdentifier = {
+						serverType = serverType,
+						homeOwner = userId,
+					}
 
-                            break
-                        end
-                    end
-                end
-            end
-        end
+					break
+				end
+			end
+		elseif serverType == ServerTypeEnum.party then
+			for partyType, partyTypeData in pairs(serverTypeData) do
+				for partyIndex, serverInfo in pairs(partyTypeData) do
+					if table.find(serverInfo.players, userId) then
+						playerServerIdentifier = {
+							serverType = serverType,
+							partyType = partyType,
+							partyIndex = partyIndex,
+						}
 
-        return playerLocationTable
-    end)
+						break
+					end
+				end
+			end
+		elseif serverType == ServerTypeEnum.game then
+			for gameType, gameTypeData in pairs(serverTypeData) do
+				for gameIndex, serverInfo in pairs(gameTypeData) do
+					if table.find(serverInfo.players, userId) then
+						playerServerIdentifier = {
+							serverType = serverType,
+							gameType = gameType,
+							gameIndex = gameIndex,
+						}
+
+						break
+					end
+				end
+			end
+		end
+	end
+
+	return playerServerIdentifier
 end
 
 return PlayerLocation
--- src/shared/ServerStorage/Utility/ReplicaResponse.lua
+++ src/shared/ServerStorage/Utility/ReplicaResponse.lua
@@ -1,6 +1,6 @@
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
 
-local Types = require(ReplicatedFirst:WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("Types"))
+local Types = require(ReplicatedFirst:WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild "Types")
 
 type Promise = Types.Promise
 
@@ -8,20 +8,14 @@ local ReplicaResponse = {}
 
 --[[
     This function is used for replica responses on a server. Pass in a Replica,
-	and a callback that takes a player and any number of arguments, and returns a Promise.
-	
+	and a callback that takes a player and any number of arguments, and returns any value(s).
+
 	When the replica is fired, the callback will be called with the player and the arguments.
 	Values returned from the callback will be passed to the client.
 ]]
-function ReplicaResponse.listen(requestReplica, callback: (Player, any) -> Promise)
+function ReplicaResponse.listen(requestReplica, callback: (Player, ...any) -> any)
 	requestReplica:ConnectOnServerEvent(function(player, requestCode, ...)
-		local function onResponse(...)
-			requestReplica:FireClient(player, requestCode, ...)
-		end
-
-		callback(player, ...)
-			:andThen(onResponse)
-			:catch(onResponse)
+		requestReplica:FireClient(player, requestCode, callback(player, ...))
 	end)
 end
 
