e6eedb3659a9ec1fe0513a8dee65acfb59c2ced9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-23 15:31:27 -0400

    Secularize EntranceManager

--- src/location/ServerScriptService/EntranceManagement/EntranceManager.server.lua
+++ src/location/ServerScriptService/EntranceManagement/EntranceManager.server.lua
@@ -1,5 +1,5 @@
-local ServerStorage = game:GetService("ServerStorage")
-local Players = game:GetService("Players")
+local ServerStorage = game:GetService "ServerStorage"
+local Players = game:GetService "Players"
 
 local serverStorageShared = ServerStorage.Shared
 local entranceDataFolder = ServerStorage.EntranceData
@@ -10,23 +10,32 @@ local Teleport = require(teleportation.Teleport)
 
 local playersTouched = {}
 
-for enum, entranceComponents in pairs(Entrances.groups) do
-    entranceComponents.exit.Touched:Connect(function(touchedPart)
-        local player = Players:GetPlayerFromCharacter(touchedPart.Parent)
+for locationEnum, entranceComponents in pairs(Entrances.groups) do
+	entranceComponents.exit.Touched:Connect(function(touchedPart)
+		local player = Players:GetPlayerFromCharacter(touchedPart.Parent)
 
-        if player and not playersTouched[player] then
-            playersTouched[player] = true
+		if player and not playersTouched[player] then
+			playersTouched[player] = true
 
-            Teleport.toLocation(player, enum, nil, function()
-                playersTouched[player] = nil
-            end):catch(function(err)
-                warn(err)
-                playersTouched[player] = nil
-            end)
-        end
-    end)
+			local success, result = Teleport.toLocation(player, locationEnum)
+
+			if not success then
+				warn(result)
+                -- Just never try again if it fails, so we don't spam the player with errors
+				return
+			end
+
+            for _, promise in result do
+                promise:andThen(function()
+                    playersTouched[player] = nil
+                end):catch(function(result)
+                    warn("Failed to teleport player: " .. result)
+                end)
+            end
+		end
+	end)
 end
 
 Players.PlayerRemoving:Connect(function(player)
-    playersTouched[player] = nil
+	playersTouched[player] = nil
 end)
