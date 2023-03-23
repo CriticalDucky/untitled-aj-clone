40557505de7343b2e5b544a10449d1eec125cb8f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-23 15:31:35 -0400

    Remove promise usage

--- src/Routing/ServerScriptService/Route.server.lua
+++ src/Routing/ServerScriptService/Route.server.lua
@@ -1,5 +1,5 @@
-local Players = game:GetService("Players")
-local ServerStorage = game:GetService("ServerStorage")
+local Players = game:GetService "Players"
+local ServerStorage = game:GetService "ServerStorage"
 
 local serverStorageShared = ServerStorage.Shared
 local teleportation = serverStorageShared.Teleportation
@@ -12,14 +12,21 @@ local function onRouteFailure(player)
 end
 
 local function routeToWorld(player)
-	return ServerData.findAvailableWorld():andThen(function(worldIndex)
-		return Teleport.toWorld(player, worldIndex, function(player)
+	local success, worldIndex = ServerData.findAvailableWorld()
+
+	if not success then return onRouteFailure(player) end
+
+	local succees, result = Teleport.toWorld(player, worldIndex)
+
+	if not succees then return onRouteFailure(player) end
+
+	for _, promise in pairs(result) do
+		promise:catch(function(err)
+			warn("RouteToWorld error: " .. tostring(err))
+
 			onRouteFailure(player)
 		end)
-	end):catch(function(err)
-		warn("RouteToWorld error: " .. tostring(err))
-        onRouteFailure(player)
-    end)
+	end
 end
 
 local function playerAdded(player: Player)
@@ -28,20 +35,29 @@ local function playerAdded(player: Player)
 	if followUserId == 0 or Players:GetPlayerByUserId(followUserId) then
 		routeToWorld(player)
 	elseif followUserId ~= 0 then
-		Teleport.Authorize
-			.toPlayer(player, followUserId)
-			:andThen(function()
-				Teleport.toPlayer(player, followUserId, function()
-					routeToWorld(player)
-				end):catch(function(err)
-					warn("RouteToPlayer error 1: " .. tostring(err))
-					routeToWorld(player)
-				end)
-			end)
-			:catch(function(err)
-				warn("RouteToPlayer error 2: " .. tostring(err))
+		local isAllowed = Teleport.Authorize.toPlayer(player, followUserId)
+
+		if not isAllowed then
+			warn("Player " .. player.Name .. " tried to follow " .. followUserId .. " but was not allowed")
+
+			return routeToWorld(player)
+		end
+
+		local success, result = Teleport.toPlayer(player, followUserId)
+
+		if not success then
+			warn("Player " .. player.Name .. " tried to follow " .. followUserId .. " but failed to teleport")
+
+			return routeToWorld(player)
+		end
+
+		for _, promise in pairs(result) do
+			promise:catch(function(err)
+				warn("Route tp player error: " .. tostring(err))
+
 				routeToWorld(player)
 			end)
+		end
 	end
 end
 
