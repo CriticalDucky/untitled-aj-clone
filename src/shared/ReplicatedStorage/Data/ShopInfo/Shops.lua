793e20852f742e0e162da3a35d5b3b6de9b55c49
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 16:42:22 -0400

    Fix group call error

--- src/shared/ReplicatedStorage/Data/ShopInfo/Shops.lua
+++ src/shared/ReplicatedStorage/Data/ShopInfo/Shops.lua
@@ -1,49 +1,43 @@
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local replicatedStorageShared = ReplicatedStorage:WaitForChild("Shared")
-local replicatedFirstShared = ReplicatedFirst:WaitForChild("Shared")
-local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
-local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
+local enumsFolder = replicatedStorageShared:WaitForChild "Enums"
+local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
 
-local ShopTypeEnum = require(enumsFolder:WaitForChild("ShopType"))
-local ItemCategory = require(enumsFolder:WaitForChild("ItemCategory"))
-local AccessoryTypeEnum = require(enumsFolder:WaitForChild("AccessoryType"))
-local FurnitureTypeEnum = require(enumsFolder:WaitForChild("FurnitureType"))
-local HomeTypeEnum = require(enumsFolder:WaitForChild("HomeType"))
-local Time = require(utilityFolder:WaitForChild("Time"))
+local ShopTypeEnum = require(enumsFolder:WaitForChild "ShopType")
+local ItemCategory = require(enumsFolder:WaitForChild "ItemCategory")
+local AccessoryTypeEnum = require(enumsFolder:WaitForChild "AccessoryType")
+local FurnitureTypeEnum = require(enumsFolder:WaitForChild "FurnitureType")
+local HomeTypeEnum = require(enumsFolder:WaitForChild "HomeType")
+local Time = require(utilityFolder:WaitForChild "Time")
 
 local timeRange = Time.newRange
-local group = Time.newGroup
+local group = Time.newRangeGroup
 
 return {
-    [ShopTypeEnum.test1] = {
-        name = "Test Shop",
-        items = {
-            { -- Beach Ball
-                itemCategory = ItemCategory.furniture,
-                item = FurnitureTypeEnum.beachBall,
-                sellingTime = group {
-                    timeRange(
-                        {
-                            year = 2020,
-                            month = 1,
-                            day = 1,
-                            hour = 0,
-                            min = 0,
-                            sec = 0
-                        },
-
-                        {
-                            year = 2025,
-                            month = 1,
-                            day = 1,
-                            hour = 0,
-                            min = 0,
-                            sec = 0
-                        }
-                    )
-                }
-            }
-        },
-    }
+	[ShopTypeEnum.test1] = {
+		name = "Test Shop",
+		items = {
+			{ -- Beach Ball
+				itemCategory = ItemCategory.furniture,
+				item = FurnitureTypeEnum.beachBall,
+				sellingTime = group(timeRange({
+					year = 2020,
+					month = 1,
+					day = 1,
+					hour = 0,
+					min = 0,
+					sec = 0,
+				}, {
+					year = 2025,
+					month = 1,
+					day = 1,
+					hour = 0,
+					min = 0,
+					sec = 0,
+				})),
+			},
+		},
+	},
 }
