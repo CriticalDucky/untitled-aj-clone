d3d7cbe0630675e8b76d70bfda04ddd35a0f8cd8
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 16:41:22 -0400

    Fix TimeRange bug

--- src/shared/ReplicatedFirst/Utility/Time.lua
+++ src/shared/ReplicatedFirst/Utility/Time.lua
@@ -13,7 +13,7 @@ type TimeRange = Types.TimeRange
 local unixTimeValue = Value(os.time())
 
 local function date(string)
-    return tonumber(os.date(string, unixTimeValue:get()))
+	return tonumber(os.date(string, unixTimeValue:get()))
 end
 
 if RunService:IsClient() then
@@ -48,86 +48,88 @@ local Time = {}
 -- TODO: TimeRanges deserve their own documentation, but it'll get it later.
 local TimeRange = {}
 TimeRange.__index = TimeRange
+TimeRange.isATimeRange = true
 
 function TimeRange.new(introduction, closing): TimeRange
-    local self = setmetatable({}, TimeRange)
+	local self = setmetatable({}, TimeRange)
 
-    self.introduction = introduction
-    self.closing = closing
+	self.introduction = introduction
+	self.closing = closing
 
-    return self
+	return self
 end
 
 function TimeRange.newGroup(...: TimeRange): TimeRange
-    local self = setmetatable({}, TimeRange)
+	local self = setmetatable({}, TimeRange)
 
-    self.timeRanges = { ... }
+	self.timeRanges = { ... }
 
-    return self
+	return self
+end
+
+-- Returns a boolean indicating whether the given TimeRange is in fact a TimeRange (via duck typing)
+function TimeRange.is(TimeRange: TimeRange | any): boolean
+	return typeof(TimeRange) == "table" and TimeRange.isATimeRange == true
 end
 
 function TimeRange:isInRange(time: TimeInfo?)
-    local time = if time then Time.getUnixFromTimeInfo(time) else Time.getUnix()
+	local time = if time then Time.getUnixFromTimeInfo(time) else Time.getUnix()
 
-    if self.timeRanges then
-        for _, timeRange in ipairs(self.timeRanges) do
-            if timeRange:isInRange(time) then
-                return true
-            end
-        end
+	if self.timeRanges then
+		for _, timeRange in ipairs(self.timeRanges) do
+			if timeRange:isInRange(time) then return true end
+		end
 
-        return false
-    else
-        return time >= Time.getUnixFromTimeInfo(self.introduction) and time <= Time.getUnixFromTimeInfo(self.closing)
-    end
+		return false
+	else
+		return time >= Time.getUnixFromTimeInfo(self.introduction) and time <= Time.getUnixFromTimeInfo(self.closing)
+	end
 end
 
 function TimeRange:distanceToClosing(time)
-    local time = time or Time.getUnix()
+	local time = time or Time.getUnix()
 
-    if self.timeRanges then
-        local distance = 0
+	if self.timeRanges then
+		local distance = 0
 
-        for _, timeRange in ipairs(self.timeRanges) do
-            if timeRange:isInRange(time) then
-                local distanceToClosing = timeRange:distanceToClosing()
+		for _, timeRange in ipairs(self.timeRanges) do
+			if timeRange:isInRange(time) then
+				local distanceToClosing = timeRange:distanceToClosing()
 
-                if distanceToClosing > distance then
-                    distance = distanceToClosing
-                end
-            end
-        end
+				if distanceToClosing > distance then distance = distanceToClosing end
+			end
+		end
 
-        return distance
-    else
-        return Time.getUnixFromTimeInfo(self.closing) - time
-    end
+		return distance
+	else
+		return Time.getUnixFromTimeInfo(self.closing) - time
+	end
 end
 
 function TimeRange:distanceToIntroduction(time)
-    local time = time or Time.getUnix()
+	local time = time or Time.getUnix()
 
-    if self.timeRanges then
-        local distance = math.huge
+	if self.timeRanges then
+		local distance = math.huge
 
-        for _, timeRange in ipairs(self.timeRanges) do
-            if not timeRange:isInRange(time) then
-                local distanceToIntroduction = timeRange:distanceToIntroduction()
+		for _, timeRange in ipairs(self.timeRanges) do
+			if not timeRange:isInRange(time) then
+				local distanceToIntroduction = timeRange:distanceToIntroduction()
 
-                if distanceToIntroduction < distance and distanceToIntroduction > 0 then
-                    distance = distanceToIntroduction
-                end
-            end
-        end
+				if distanceToIntroduction < distance and distanceToIntroduction > 0 then
+					distance = distanceToIntroduction
+				end
+			end
+		end
 
-        if distance == math.huge then
-            return 0
-        else
-            return distance
-        end
-    else
-        return math.max(Time.getUnixFromTimeInfo(self.introduction) - time, 0)
-    end
+		if distance == math.huge then
+			return 0
+		else
+			return distance
+		end
+	else
+		return math.max(Time.getUnixFromTimeInfo(self.introduction) - time, 0)
+	end
 end
 
 --[[
@@ -143,42 +145,42 @@ end
     Gets the current year. Returns a number.
 ]]
 function Time.currentYear()
-    return date("%Y")
+	return date "%Y"
 end
 
 --[[
     Gets the current month. Returns a number from 1 to 12.
 ]]
 function Time.currentMonth()
-    return date("%m")
+	return date "%m"
 end
 
 --[[
     Gets the current day. Returns a number from 1 to 31.
 ]]
 function Time.currentDay()
-    return date("%d")
+	return date "%d"
 end
 
 --[[
     Gets the current hour. Returns a number from 0 to 23.
 ]]
 function Time.currentHour()
-    return date("%H")
+	return date "%H"
 end
 
 --[[
     Gets the current minute. Returns a number from 0 to 59.
 ]]
 function Time.currentMinute()
-    return date("%M")
+	return date "%M"
 end
 
 --[[
     Gets the current second. Returns a number from 0 to 59.
 ]]
 function Time.currentSecond()
-    return date("%S")
+	return date "%S"
 end
 
 --[[
@@ -201,26 +203,20 @@ end
     undesirable, so make sure your time info is complete with 0 values.
 ]]
 function Time.getUnixFromTimeInfo(timeInfo: TimeInfo): number
-    if type(timeInfo) == "function" then
-        timeInfo = timeInfo()
-    end
+	if type(timeInfo) == "function" then timeInfo = timeInfo() end
 
-    if type(timeInfo) == "number" then
-        return timeInfo
-    end
-    
-    if not type(timeInfo) == "table" then
-        error("Error: timeInfo is not a table")
-    end
+	if type(timeInfo) == "number" then return timeInfo end
 
-    return os.time({
-        year = timeInfo.year or Time.currentYear(),
-        month = timeInfo.month or Time.currentMonth(),
-        day = timeInfo.day or Time.currentDay(),
-        hour = timeInfo.hour or Time.currentHour(),
-        min = timeInfo.min or Time.currentMinute(),
-        sec = timeInfo.sec or Time.currentSecond(),
-    })
+	if not type(timeInfo) == "table" then error "Error: timeInfo is not a table" end
+
+	return os.time {
+		year = timeInfo.year or Time.currentYear(),
+		month = timeInfo.month or Time.currentMonth(),
+		day = timeInfo.day or Time.currentDay(),
+		hour = timeInfo.hour or Time.currentHour(),
+		min = timeInfo.min or Time.currentMinute(),
+		sec = timeInfo.sec or Time.currentSecond(),
+	}
 end
 
 --[[
@@ -249,7 +245,7 @@ end
     ```
 ]]
 function Time.newRange(introduction: TimeInfo, closing: TimeInfo): TimeRange
-    return TimeRange.new(introduction, closing)
+	return TimeRange.new(introduction, closing)
 end
 
 --[[
@@ -258,7 +254,7 @@ end
 
     Example usage:
     ```lua
-    local timeRangeGroup = Time.newGroup(
+    local timeRangeGroup = Time.newRangeGroup(
         Time.newRange({
             year = 2021,
             month = 1,
@@ -294,8 +290,14 @@ end
     print(timeRangeGroup:isInRange()) -- true if the current time is between the time range group's *sectioned* 20 second interval
     ```
 ]]
-function Time.newGroup(...: TimeRange)
-    return TimeRange.newGroup(...)
+function Time.newRangeGroup(...: TimeRange)
+	assert(select("#", ...) > 0, "Error: Time.newRangeGroup requires at least one time range")
+
+	for _, timeRange in { ... } do
+		assert(TimeRange.is(timeRange), "Error: Time.newRangeGroup requires time ranges")
+	end
+
+	return TimeRange.newGroup(...)
 end
 
 return Time
