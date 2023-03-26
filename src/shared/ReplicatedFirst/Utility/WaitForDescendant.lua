7760aac3cfcee2c3e97fcf04db41cc08374ece1d
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 16:41:48 -0400

    Add identifier immediate checking

--- src/shared/ReplicatedFirst/Utility/WaitForDescendant.lua
+++ src/shared/ReplicatedFirst/Utility/WaitForDescendant.lua
@@ -3,19 +3,25 @@ local function waitForDescendant (descendantOf, identifier)
 
 	assert(typeof(descendantOf) == "Instance", "Invalid type for argument 1 (descendatOf)")
 	assert(idType == "string" or idType == "function", "Invalid type for argument 2 (identifier)")
-	
+
 	local TIMEOUT = 10
 
 	if idType == "string" and descendantOf:FindFirstChild(identifier, true) then
 		return descendantOf:FindFirstChild(identifier, true)
+	elseif idType == "function" then
+		for _, descendant in ipairs(descendantOf:GetDescendants()) do
+			if identifier(descendant) then -- that was the bug lol
+				return descendant
+			end
+		end
 	else
 		local object
-		
+
 		task.spawn(function()
 			task.wait(TIMEOUT)
 
 			if not object then
-				warn("Infinite yield possible on "..tostring(descendantOf)..":WaitForDescendant("..tostring(identifier)..")")
+				warn("Infinite yield possible on "..tostring(descendantOf)..":WaitForDescendant("..tostring(identifier).."). Traceback:", debug.traceback())
 			end
 		end)
 
