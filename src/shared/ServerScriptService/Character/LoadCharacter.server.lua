233cced42d70021bb6462f22dcb358146efb7ede
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 16:43:56 -0400

    Format LoadCharacter

--- src/shared/ServerScriptService/Character/LoadCharacter.server.lua
+++ src/shared/ServerScriptService/Character/LoadCharacter.server.lua
@@ -1,7 +1,7 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ServerStorage = game:GetService("ServerStorage")
-local Players = game:GetService("Players")
-local ReplicatedFirst = game:GetService("ReplicatedFirst")
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+local ServerStorage = game:GetService "ServerStorage"
+local Players = game:GetService "Players"
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
 
 local replicatedStorageShared = ReplicatedStorage.Shared
 local enumsFolder = replicatedStorageShared.Enums
@@ -13,37 +13,37 @@ local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
 local WaitForDescendant = require(utilityFolder.WaitForDescendant)
 
 local function playerAdded(player: Player)
-    local spawnpoint do
-        if ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
-            local entranceDataFolder = ServerStorage.EntranceData
-            local Entrances = require(entranceDataFolder.Entrances)
+	local spawnpoint
 
-            local joinData = player:GetJoinData()
-            local locationFrom = joinData and joinData.TeleportData and joinData.TeleportData.locationFrom
-    
-            if locationFrom then
-                local entranceGroup = Entrances.groups[locationFrom]
-                spawnpoint = if entranceGroup then entranceGroup.entrance else Entrances.main
-            else
-                spawnpoint = Entrances.main
-            end
-        elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.hasWorldInfo) then
-            spawnpoint = WaitForDescendant(workspace, function(descendant)
-                local name = descendant.Name
+	if ServerTypeGroups.serverInGroup(ServerGroupEnum.isLocation) then
+		local entranceDataFolder = ServerStorage.EntranceData
+		local Entrances = require(entranceDataFolder.Entrances)
 
-                return string.find(name, "Spawn")
-            end)
-        end
-    end
+		local joinData = player:GetJoinData()
+		local locationFrom = joinData and joinData.TeleportData and joinData.TeleportData.locationFrom
 
-    player.RespawnLocation = spawnpoint
-    player:LoadCharacter()
+		if locationFrom then
+			local entranceGroup = Entrances.groups[locationFrom]
+			spawnpoint = if entranceGroup then entranceGroup.entrance else Entrances.main
+		else
+			spawnpoint = Entrances.main
+		end
+	elseif ServerTypeGroups.serverInGroup(ServerGroupEnum.hasWorldInfo) then
+		spawnpoint = WaitForDescendant(workspace, function(descendant)
+			local name = descendant.Name
+
+			return string.find(name, "Spawn")
+		end)
+	end
+
+	player.RespawnLocation = spawnpoint
+	player:LoadCharacter()
 end
 
 if not ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
-    for _, player in pairs(Players:GetPlayers()) do
-        playerAdded(player)
-    end
+	for _, player in pairs(Players:GetPlayers()) do
+		playerAdded(player)
+	end
 
-    Players.PlayerAdded:Connect(playerAdded)
+	Players.PlayerAdded:Connect(playerAdded)
 end
