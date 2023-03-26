db07d4764588c3c2110ae09338d7d1f734180e94
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 16:43:21 -0400

    Fix group calling

--- src/shared/ReplicatedStorage/Server/Parties.lua
+++ src/shared/ReplicatedStorage/Server/Parties.lua
@@ -1,8 +1,10 @@
-local PartyTypeEnum = require(game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Enums"):WaitForChild("PartyType"))
-local Time = require(game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild("Time"))
+local PartyTypeEnum =
+	require(game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Enums"):WaitForChild "PartyType")
+local Time =
+	require(game:GetService("ReplicatedFirst"):WaitForChild("Shared"):WaitForChild("Utility"):WaitForChild "Time")
 
 local timeRange = Time.newRange
-local group = Time.newGroup
+local group = Time.newRangeGroup
 
 --[[
 
@@ -14,7 +16,7 @@ local group = Time.newGroup
         max = 100,
         recommended = 50,
     },
-    enabledTime = group {
+    enabledTime = group (
         timeRange(
             {
                 year = 2020,
@@ -34,171 +36,137 @@ local group = Time.newGroup
                 sec = 0
             }
         )
-    }
+    )
 },
 
 ]]
 
-return {
-    [PartyTypeEnum.baseballParty] = {
-        name = "Baseball Party",
-        placeId = 11353468067,
-        chanceWeight = 1,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
+local parties = {
+	[PartyTypeEnum.baseballParty] = {
+		name = "Baseball Party",
+		placeId = 11353468067,
+		chanceWeight = 1,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
+	[PartyTypeEnum.beachParty] = {
+		name = "Beach Party",
+		placeId = 11353468067,
+		chanceWeight = 30,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 
-    [PartyTypeEnum.beachParty] = {
-        name = "Beach Party",
-        placeId = 11353468067,
-        chanceWeight = 30,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
+	[PartyTypeEnum.birthdayParty] = {
+		name = "Birthday Party",
+		placeId = 11353468067,
+		chanceWeight = 1,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
+	[PartyTypeEnum.campingParty] = {
+		name = "Camping Party",
+		placeId = 11353468067,
+		chanceWeight = 1,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 
-    [PartyTypeEnum.birthdayParty] = {
-        name = "Birthday Party",
-        placeId = 11353468067,
-        chanceWeight = 1,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
+	[PartyTypeEnum.catParty] = {
+		name = "Cat Party",
+		placeId = 11353468067,
+		chanceWeight = 1,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
-
-    [PartyTypeEnum.campingParty] = {
-        name = "Camping Party",
-        placeId = 11353468067,
-        chanceWeight = 1,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
-
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
-
-    [PartyTypeEnum.catParty] = {
-        name = "Cat Party",
-        placeId = 11353468067,
-        chanceWeight = 1,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
-
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
-
-    [PartyTypeEnum.circusParty] = {
-        name = "Circus Party",
-        placeId = 11353468067,
-        chanceWeight = 1,
-        enabledTime = group {
-            timeRange(
-                {
-                    year = 2020,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                },
-
-                {
-                    year = 2025,
-                    month = 1,
-                    day = 1,
-                    hour = 0,
-                    min = 0,
-                    sec = 0
-                }
-            )
-        }
-    },
+	[PartyTypeEnum.circusParty] = {
+		name = "Circus Party",
+		placeId = 11353468067,
+		chanceWeight = 1,
+		enabledTime = group(timeRange({
+			year = 2020,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		}, {
+			year = 2025,
+			month = 1,
+			day = 1,
+			hour = 0,
+			min = 0,
+			sec = 0,
+		})),
+	},
 }
+
+return parties
