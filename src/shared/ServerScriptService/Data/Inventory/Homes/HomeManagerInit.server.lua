37894057f2e19fd8d41503a9edb7994d30c15387
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-26 20:56:55 -0400

    Add error handling

--- src/shared/ServerScriptService/Data/Inventory/Homes/HomeManagerInit.server.lua
+++ src/shared/ServerScriptService/Data/Inventory/Homes/HomeManagerInit.server.lua
@@ -1,5 +1,18 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
 
 local dataFolder = ServerStorage.Shared.Data
+local serverFolder = ReplicatedStorage.Shared.Server
+local enumsFolder = ReplicatedStorage.Shared.Enums
 
 local HomeManager = require(dataFolder.Inventory.HomeManager)
+local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
+local ServerGroupEnum = require(enumsFolder.ServerGroup)
+local Teleport = require(ServerStorage.Shared.Teleportation.Teleport)
+
+if ServerTypeGroups.serverInGroup(ServerGroupEnum.isHome) then
+    if not HomeManager.loadHome() then
+        Teleport.bootServer("The home failed to start up. (err code LH1)")
+    end
+end
+
