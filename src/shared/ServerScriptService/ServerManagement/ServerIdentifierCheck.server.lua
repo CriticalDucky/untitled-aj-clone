31c4ea348df23bdb410ee5c1e42c47df6cb62c9e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-27 10:36:44 -0400

    Use LocalServerInfo.getServerIdentifier

--- src/shared/ServerScriptService/ServerManagement/ServerIdentifierCheck.server.lua
+++ src/shared/ServerScriptService/ServerManagement/ServerIdentifierCheck.server.lua
@@ -11,11 +11,12 @@ local ServerData = require(serverStorageShared.ServerManagement.ServerData)
 local Teleport = require(teleportationFolder.Teleport)
 local ServerGroupEnum = require(enumsFolder.ServerGroup)
 local ServerTypeGroups = require(serverFolder.ServerTypeGroups)
+local LocalServerInfo = require(serverFolder:WaitForChild "LocalServerInfo")
 
 if not ServerTypeGroups.serverInGroup(ServerGroupEnum.isRouting) then
-	local success, result = ServerData.getServerIdentifier()
+	local result = LocalServerInfo.getServerIdentifier()
 
-    if not success or not result then
+    if not result then
         Teleport.bootServer "An internal server error occurred. (err code SCF1)"
         return
     end
