1482e08f191c0d3bb1f23348c78f838536f145c8
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-28 20:51:23 -0400

    Add PlayMinigameResponseType.lua

---
+++ src/shared/ReplicatedStorage/Enums/PlayMinigameResponseType.lua
@@ -0,0 +1,5 @@
+return {
+	error = "PMG_E", -- Something went wrong that were not in control of
+	invalid = "PMG_I", -- The request was invalid. This error code is flexible and can be used for many different reasons
+	success = "PMG_SUCCESS", -- The request was successful :D
+}
