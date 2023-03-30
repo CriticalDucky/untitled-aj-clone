0f064f6b02b879976a6d8634d8b21fa5342459ec
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-30 06:32:24 -0400

    Restructure to fix bugs

--- src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
+++ src/shared/ReplicatedStorage/Server/LocalServerInfo.lua
@@ -1,4 +1,3 @@
-local Players = game:GetService "Players"
 local ReplicatedFirst = game:GetService "ReplicatedFirst"
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 local RunService = game:GetService "RunService"
@@ -7,12 +6,15 @@ local replicatedFirstShared = ReplicatedFirst.Shared
 local replicatedStorageShared = ReplicatedStorage.Shared
 local enumsFolder = replicatedStorageShared.Enums
 local serverFolder = replicatedStorageShared.Server
+local utilityFolder = replicatedFirstShared.Utility
 
 local ServerTypeEnum = require(enumsFolder:WaitForChild "ServerType")
 local Locations = require(serverFolder:WaitForChild "Locations")
 local Parties = require(serverFolder:WaitForChild "Parties")
 local Minigames = require(serverFolder:WaitForChild "Minigames")
 local GameSettings = require(replicatedFirstShared:WaitForChild("Settings"):WaitForChild "GameSettings")
+local MinigameServerType = require(enumsFolder:WaitForChild "MinigameServerType")
+local Table = require(utilityFolder:WaitForChild "Table")
 
 local serverType
 do
@@ -59,62 +61,91 @@ LocalServerInfo.serverType = serverType
 	```
 ]]
 function LocalServerInfo.getServerIdentifier()
-    local serverIdentifier
+	local serverIdentifier
+	local SessionInfo
 
-	if RunService:IsServer() then
-		local ServerStorage = game:GetService "ServerStorage"
+	if not (serverType == ServerTypeEnum.routing or serverType == ServerTypeEnum.minigame) then
+		if RunService:IsServer() then
+			local ServerStorage = game:GetService "ServerStorage"
 
-		local serverStorageShared = ServerStorage.Shared
+			local serverStorageShared = ServerStorage.Shared
 
-		local ServerData = require(serverStorageShared.ServerManagement.ServerData)
+			local ServerData = require(serverStorageShared.ServerManagement.ServerData)
 
-		serverIdentifier = select(2, ServerData.getServerIdentifier())
+			serverIdentifier = select(2, ServerData.getServerIdentifier())
+		else
+			local ReplicatedServerData = require(serverFolder:WaitForChild "ReplicatedServerData")
 
-		if serverIdentifier then
-			serverIdentifier.jobId = game.JobId
-			serverIdentifier.privateServerId = game.PrivateServerId
-		end
-	elseif RunService:IsClient() then
-		local ReplicatedServerData = require(serverFolder:WaitForChild "ReplicatedServerData")
-		local ReplicaCollection = require(replicatedStorageShared:WaitForChild("Replication"):WaitForChild "ReplicaCollection")
-
-		serverIdentifier = ReplicatedServerData.getServerIdentifier()
-
-		if serverIdentifier then
-			local PrivateServerInfo = ReplicaCollection.get("PrivateServerInfo").Data
-
-			serverIdentifier.privateServerId = PrivateServerInfo.privateServerId
+			serverIdentifier = ReplicatedServerData.getServerIdentifier()
 		end
 	end
 
-	if serverIdentifier then
-		serverIdentifier.serverType = serverType
+	if RunService:IsClient() then
+		local ReplicaCollection =
+			require(replicatedStorageShared:WaitForChild("Replication"):WaitForChild "ReplicaCollection")
+
+		SessionInfo = ReplicaCollection.get("SessionInfo").Data
 	end
 
-	if serverType == ServerTypeEnum.minigame then
-        if serverIdentifier then
-            return serverIdentifier
-        end
+	if serverIdentifier then serverIdentifier.serverType = serverType end
 
+	if serverType == ServerTypeEnum.routing then
 		serverIdentifier = {
-			serverType = ServerTypeEnum.minigame,
+			serverType = ServerTypeEnum.routing,
 		}
 
-        for minigameType, minigameInfo in pairs(Minigames) do
-            if minigameInfo.placeId == game.PlaceId then
-                serverIdentifier.minigameType = minigameType
-            end
-        end
+		if RunService:IsServer() then
+			serverIdentifier.jobId = game.JobId
+		elseif RunService:IsClient() then
+			serverIdentifier.jobId = SessionInfo.jobId
+		end
+	elseif serverType == ServerTypeEnum.minigame then
+		local minigameType
+		local minigameInfo
 
-        if not serverIdentifier.minigameType then
-            warn("Could not find minigameType for minigame server")
-            return nil
-        end
+		for k, v in pairs(Minigames) do
+			if v.placeId == game.PlaceId then
+				minigameInfo = v
+				minigameType = k
+			end
+		end
 
-        return serverIdentifier
+		if not minigameType then
+			error "Something went wrong when getting the serverIdentifier for a minigame server."
+		end
+
+		if minigameInfo.minigameServerType == MinigameServerType.instance then
+			if RunService:IsClient() then
+				serverIdentifier = {
+					serverType = ServerTypeEnum.minigame,
+					minigameType = minigameType,
+					privateServerId = SessionInfo.privateServerId,
+				}
+			else
+				serverIdentifier = {
+					serverType = ServerTypeEnum.minigame,
+					minigameType = minigameType,
+					privateServerId = game.JobId,
+				}
+			end
+		else
+			if RunService:IsClient() then
+				local ReplicatedServerData = require(serverFolder:WaitForChild "ReplicatedServerData")
+
+				serverIdentifier = ReplicatedServerData.getServerIdentifier()
+			else
+				local ServerStorage = game:GetService "ServerStorage"
+
+				local serverStorageShared = ServerStorage.Shared
+
+				local ServerData = require(serverStorageShared.ServerManagement.ServerData)
+
+				serverIdentifier = select(2, ServerData.getServerIdentifier())
+			end
+		end
 	end
 
-    return serverIdentifier
+	return serverIdentifier
 end
 
 return LocalServerInfo
