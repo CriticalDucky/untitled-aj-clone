30a9354026fa7577d565a45737c22a0754b65a6c
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-30 06:31:06 -0400

    Add alreadyInPlace

--- src/shared/ReplicatedStorage/Enums/PlayMinigameResponseType.lua
+++ src/shared/ReplicatedStorage/Enums/PlayMinigameResponseType.lua
@@ -2,4 +2,5 @@ return {
 	error = "PMG_E", -- Something went wrong that were not in control of
 	invalid = "PMG_I", -- The request was invalid. This error code is flexible and can be used for many different reasons
 	success = "PMG_SUCCESS", -- The request was successful :D
+	alreadyInPlace = "PMG_AIP", -- The player is already in the place
 }
