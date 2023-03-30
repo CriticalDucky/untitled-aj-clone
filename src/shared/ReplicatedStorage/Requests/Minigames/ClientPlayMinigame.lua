1ea1646f02a4f3b75ea92f6099a54b9e91696b0e
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-30 06:31:52 -0400

    Add validation

--- src/shared/ReplicatedStorage/Requests/Minigames/ClientPlayMinigame.lua
+++ src/shared/ReplicatedStorage/Requests/Minigames/ClientPlayMinigame.lua
@@ -7,16 +7,31 @@ local requestsFolder = replicatedStorageShared:WaitForChild("Requests")
 local replicationFolder = replicatedStorageShared:WaitForChild("Replication")
 local enumsFolder = replicatedStorageShared:WaitForChild("Enums")
 local utilityFolder = replicatedFirstShared:WaitForChild("Utility")
+local serverFolder = replicatedStorageShared:WaitForChild("Server")
 
 local ReplicaRequest = require(requestsFolder:WaitForChild("ReplicaRequest"))
 local ReplicaCollection = require(replicationFolder:WaitForChild("ReplicaCollection"))
 local MinigameType = require(enumsFolder:WaitForChild("MinigameType"))
 local Table = require(utilityFolder:WaitForChild("Table"))
+local LocalServerInfo = require(serverFolder:WaitForChild("LocalServerInfo"))
+local PlayMinigameResponseType = require(enumsFolder:WaitForChild("PlayMinigameResponseType"))
 
 local ClientPlayMinigame = {}
 
 function ClientPlayMinigame.request(minigameType, ...)
     assert(Table.hasValue(MinigameType, minigameType), "ClientPlayMinigame.request() called with invalid minigameType: " .. tostring(minigameType))
+
+    local serverIdentifier = LocalServerInfo.getServerIdentifier()
+
+    if not serverIdentifier then
+        return
+    end
+
+    if serverIdentifier.minigameType == minigameType then
+        warn("ClientPlayMinigame.request() called with own minigameType: " .. tostring(minigameType))
+
+        return false, PlayMinigameResponseType.alreadyInPlace
+    end
 
     return ReplicaRequest.new(ReplicaCollection.get("PlayMinigameRequest"), minigameType, ...)
 end
