fb8b293aff338f6afc12d6bf27fb0e132784b020
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-30 20:48:58 -0400

    Add client setting change requests

--- src/shared/ReplicatedStorage/Data/Settings/ClientPlayerSettings.lua
+++ src/shared/ReplicatedStorage/Data/Settings/ClientPlayerSettings.lua
@@ -12,23 +12,46 @@ local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
 local dataFolder = replicatedStorageShared:WaitForChild "Data"
 local enumsFolder = replicatedStorageShared:WaitForChild "Enums"
 local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
+local replicationFolder = replicatedStorageShared:WaitForChild "Replication"
+local requestsFolder = replicatedStorageShared:WaitForChild "Requests"
 
 local ReplicatedPlayerData = require(dataFolder:WaitForChild "ReplicatedPlayerData")
+local ReplicaCollection = require(replicationFolder:WaitForChild "ReplicaCollection")
 local Types = require(utilityFolder:WaitForChild "Types")
+local ReplicaRequest = require(requestsFolder:WaitForChild "ReplicaRequest")
 
 local ClientPlayerSettings = {}
 
 --[[
-	Gets a player's setting. If the player is not available, this will return nil.
+	Gets a player's setting. If the player data is not available, this will return nil.
 ]]
-function ClientPlayerSettings.getSetting(settingName: string, player: Player | number | nil, wait: boolean?)
-	local data = ReplicatedPlayerData.get(player, wait)
-
-	if data then
+function ClientPlayerSettings.getSetting(settingName: string, player: Player | number | nil, wait: boolean?): any?
+	local function getSetting(data)
 		local playerSettings = data.playerSettings
 
 		if playerSettings then return playerSettings[settingName] end
 	end
+
+	local data = ReplicatedPlayerData.get(player, wait)
+
+	if data then
+		return getSetting(data)
+	elseif wait then
+		data = select(2, ReplicatedPlayerData.requestData(if typeof(player) == "number" then player else player.UserId))
+
+		if data then
+			return getSetting(data)
+		end
+	end
+end
+
+--[[
+	Sets a player's setting. Will return false if validation fails.
+]]
+function ClientPlayerSettings.setSetting(settingName: string, value: any): boolean
+	local setSettingReplica = ReplicaCollection.get("SetSettingRequest")
+
+	return unpack(ReplicaRequest.new(setSettingReplica, settingName, value))
 end
 
 return ClientPlayerSettings
