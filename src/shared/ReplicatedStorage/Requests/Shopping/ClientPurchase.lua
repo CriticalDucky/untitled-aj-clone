0f48075ab5705530112daa72c4d5e1bb8ff28be0
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-03-31 18:45:05 -0400

    Depromisify

--- src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
+++ src/shared/ReplicatedStorage/Requests/Shopping/ClientPurchase.lua
@@ -1,28 +1,28 @@
-local REQUEST_TIMEOUT = 10
-
-local HttpService = game:GetService "HttpService"
+local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
 local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
 local replicationFolder = replicatedStorageShared:WaitForChild "Replication"
 local dataFolder = replicatedStorageShared:WaitForChild "Data"
 local shopInfoFolder = dataFolder:WaitForChild "ShopInfo"
 local enumsFolder = replicatedStorageShared:WaitForChild "Enums"
 local requestsFolder = replicatedStorageShared:WaitForChild "Requests"
+local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
 
 local ReplicaCollection = require(replicationFolder:WaitForChild "ReplicaCollection")
 local ReplicaRequest = require(requestsFolder:WaitForChild "ReplicaRequest")
 local Shops = require(shopInfoFolder:WaitForChild "Shops")
 
-local Purchase = {}
+local ClientPurchase = {}
 
-function Purchase.request(shopEnum, itemIndex)
+function ClientPurchase.request(shopEnum, itemIndex)
 	assert(shopEnum and Shops[shopEnum], "shop must be a valid shop type")
 	assert(itemIndex and Shops[shopEnum].items[itemIndex], "item must be a valid item")
 
-	return ReplicaCollection.get("PurchaseRequest"):andThen(function(purchaseRequest)
-		return ReplicaRequest.new(purchaseRequest, shopEnum, itemIndex)
-	end)
+	local purchaseRequestReplica = ReplicaCollection.get("PurchaseRequest")
+
+	return ReplicaRequest.new(purchaseRequestReplica, shopEnum, itemIndex)
 end
 
-return Purchase
+return ClientPurchase
