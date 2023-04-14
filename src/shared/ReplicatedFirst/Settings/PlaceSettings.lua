850e6529e6455aaaf21fe92242e106b573d1d4bb
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-14 12:43:09 -0400

    Split GameSettings into separate modules

---
+++ src/shared/ReplicatedFirst/Settings/PlaceSettings.lua
@@ -0,0 +1,15 @@
+local placeSettings = { -- Constants for the game
+	location_maxPlayers = 20, -- The max amount of players that can be in a location at once
+	location_maxRecommendedPlayers = 15, -- The recommended amount of players in a location at once
+	world_maxRecommendedPlayers = 50, -- The recommended amount of players in a world at once
+	party_maxPlayers = 20, -- The max amount of players that can be in a party at once
+	party_maxRecommendedPlayers = 15, -- The recommended amount of players in a party at once
+	home_maxNormalPlayers = 20, -- The max amount of normal players that can be in a home at once
+
+	maxFurniturePlaced = 500, -- The max amount of furniture that can be placed in a home
+
+	homePlaceId = 10564407502, -- The place ID of home
+	routePlaceId = 10189729412, -- The place ID of the routing server
+}
+
+return placeSettings
