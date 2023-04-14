4743006ba75524220230d5223ddc7601974ba342
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-14 12:42:31 -0400

    Adjust FusionTemplate

--- src/shared/ReplicatedFirst/Interface/Components/Template/FusionTemplate.lua
+++ src/shared/ReplicatedFirst/Interface/Components/Template/FusionTemplate.lua
@@ -1,43 +1,60 @@
---[[
-	This component creates a...
-
-	Props:
-	```lua
-	{
-		-- Format: key: type
-		PropName: string
-		[Children]: table
-	}
-	```
-
-	Example:
-	```lua
-		
-	```
-]]
+--#region Imports
 local ReplicatedFirst = game:GetService "ReplicatedFirst"
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 
 local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
 local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
 local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
+local componentsFolder = replicatedFirstShared:WaitForChild("Interface"):WaitForChild "Components"
 
-local Component = require(utilityFolder:WaitForChild "GetComponent")
+-- Optional: Remove imports that you don't need
 local Fusion = require(replicatedFirstShared:WaitForChild "Fusion")
-local Value = Fusion.Value
 local New = Fusion.New
+local Hydrate = Fusion.Hydrate
+local Ref = Fusion.Ref
 local Children = Fusion.Children
-local Computed = Fusion.Computed
+local Out = Fusion.Out
 local OnEvent = Fusion.OnEvent
 local OnChange = Fusion.OnChange
+local Attribute = Fusion.Attribute
+local AttributeChange = Fusion.AttributeChange
+local AttributeOut = Fusion.AttributeOut
+local Value = Fusion.Value
+local Computed = Fusion.Computed
+local ForPairs = Fusion.ForPairs
+local ForKeys = Fusion.ForKeys
+local ForValues = Fusion.ForValues
 local Observer = Fusion.Observer
 local Tween = Fusion.Tween
 local Spring = Fusion.Spring
-local Hydrate = Fusion.Hydrate
-local unwrap = Fusion.unwrap
+local peek = Fusion.peek
+local cleanup = Fusion.cleanup
+local doNothing = Fusion.doNothing
 
-local component = function(props)
-	props = props or {}
+type CanBeState<T> = Fusion.CanBeState<T>
+-- #endregion
+
+export type Props = {
+	-- Default props
+	Name: CanBeState<string>?,
+	LayoutOrder: CanBeState<number>?,
+	Position: CanBeState<UDim2>?,
+	AnchorPoint: CanBeState<Vector2>?,
+	Size: CanBeState<UDim2>?,
+	AutomaticSize: CanBeState<Enum.AutomaticSize>?,
+	ZIndex: CanBeState<number>?,
+}
+
+--[[
+	This component creates a...
+
+	Example usage:
+	```lua
+		
+	```
+]]
+local function Component(props: Props)
+	
 end
 
-return component
+return Component
