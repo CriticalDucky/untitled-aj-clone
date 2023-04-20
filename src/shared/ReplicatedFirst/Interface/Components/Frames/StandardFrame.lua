075e4c0a47050eed0ffbcec0276b582fcf372efa
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-20 12:36:52 -0400

    Add preliminary component code

--- src/shared/ReplicatedFirst/Interface/Components/Frames/StandardFrame.lua
+++ src/shared/ReplicatedFirst/Interface/Components/Frames/StandardFrame.lua
@@ -6,6 +6,9 @@ local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
 local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
 local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
 local componentsFolder = replicatedFirstShared:WaitForChild("Interface"):WaitForChild "Components"
+local settingsFolder = replicatedFirstShared:WaitForChild "Settings"
+
+local InterfaceConstants = require(settingsFolder:WaitForChild "InterfaceConstants")
 
 -- Optional: Remove imports that you don't need
 local Fusion = require(replicatedFirstShared:WaitForChild "Fusion")
@@ -60,7 +63,50 @@ export type Props = {
 	This component creates a standard outlined frame that all stylized menus must use.
 ]]
 local function Component(props: Props)
-	
+	local MARGIN = 4
+	local OUTER_ROUNDNESS = InterfaceConstants.roundness.menuOuter
+	local INNER_ROUNDNESS = OUTER_ROUNDNESS - MARGIN
+	local DEFUALT_PADDING = 40
+	local DEFUALT_OUTLINE_COLOR = Color3.fromRGB(0, 0, 0)
+
+	local frame = New "Frame" {
+		Name = props.Name or "StandardFrame",
+		LayoutOrder = props.LayoutOrder,
+		Position = props.Position,
+		AnchorPoint = props.AnchorPoint,
+		Size = props.Size,
+		AutomaticSize = props.AutomaticSize,
+		ZIndex = props.ZIndex,
+
+		BackgroundColor3 = props.OutlineColor or DEFUALT_OUTLINE_COLOR,
+
+		[Children] = {
+			New "UICorner" {
+				CornerRadius = UDim.new(0, InterfaceConstants.roundness.menuOuter),
+			},
+
+			New "Frame" {
+				Name = "InnerFrame",
+				Position = UDim2.fromOffset(MARGIN, MARGIN),
+				Size = UDim2.fromOffset(-MARGIN * 2, -MARGIN * 2),
+				BackgroundColor3 = props.BackgroundColor or Color3.fromRGB(255, 255, 255),
+				Rotation = props.Rotation or 0,
+
+				[Children] = {
+					New "UICorner" {
+						CornerRadius = UDim.new(0, INNER_ROUNDNESS),
+					},
+
+					New "UIPadding" {
+						PaddingTop = props.PaddingTop or UDim.new(0, DEFUALT_PADDING),
+						PaddingBottom = props.PaddingBottom or UDim.new(0, DEFUALT_PADDING),
+						PaddingLeft = props.PaddingLeft or UDim.new(0, DEFUALT_PADDING),
+						PaddingRight = props.PaddingRight or UDim.new(0, DEFUALT_PADDING),
+					},
+				},
+			},
+		}
+	}
 end
 
 return Component
