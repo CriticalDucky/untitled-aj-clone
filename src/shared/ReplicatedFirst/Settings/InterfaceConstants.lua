7f017046aa6604226a0dc4cb67d0aa308ed6693f
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-20 12:37:12 -0400

    Add interface constants

---
+++ src/shared/ReplicatedFirst/Settings/InterfaceConstants.lua
@@ -0,0 +1,20 @@
+return {
+	colors = {
+		menuBackground = Color3.fromRGB(228, 184, 148),
+	},
+
+	roundness = {
+		menuOuter = 12,
+	},
+
+	fonts = {
+		body = {
+			font = Enum.Font.Gotham,
+			size = 18,
+		},
+		title = {
+			font = Enum.Font.GothamBold,
+			size = 24,
+		},
+	},
+}
