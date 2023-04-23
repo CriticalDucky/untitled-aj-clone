d5a335084d8e8d1f85c9772db213da8ef3b47fb9
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-23 19:06:36 -0400

    Add sound system script

---
+++ src/shared/ReplicatedFirst/Utility/SoundSystem.lua
@@ -0,0 +1,9 @@
+--[[
+    This script manages the sound that the player hears. What the script does:
+
+    * Change the background music with fade
+    * Change the sound effects volume
+    * Change the music volume
+    * Play a sound effect
+    * Stop music
+]]
