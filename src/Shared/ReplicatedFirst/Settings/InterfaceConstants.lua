7375520dea2d67e6394bf23362be6fda86730570
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-30 04:14:43 -0400

    Add constants

--- src/Shared/ReplicatedFirst/Settings/InterfaceConstants.lua
+++ src/Shared/ReplicatedFirst/Settings/InterfaceConstants.lua
@@ -1,19 +1,32 @@
+local patrickHandBold = Font.new("rbxasset://fonts/families/PatrickHand.json")
+patrickHandBold.Bold = true
+
 return {
 	colors = {
-		menuBackground = Color3.fromHex("#E4C5B0"),
-		menuTitle = Color3.fromHex("#F1E3D7"),
+		menuBackground = Color3.fromHex "#E4C5B0",
+		menuTitle = Color3.fromHex "#F1E3D7",
+		menuGreen1 = Color3.fromHex "#687822",
 
-		green1 = Color3.fromHex("#687822"),
+		buttonBluePrimary = Color3.fromHex "#53A3BC",
+		buttonBlueSecondary = Color3.fromHex "#3D788A",
 	},
 
 	fonts = {
 		body = {
-			font = Enum.Font.Gotham,
-			size = 18,
+			font = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Regular),
+			size = 20,
 		},
-		title = {
-			font = Enum.Font.GothamBold,
-			size = 24,
+		header = {
+			font = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Bold),
+			size = 20,
+		},
+		button = {
+			font = patrickHandBold,
+			size = 36,
+		},
+		thickTitle = {
+			font = patrickHandBold,
+			size = 60,
 		},
 	},
 }
