ae1a1e0838e2d1f868a1387ac8a1687bbf19fd35
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-30 01:58:32 -0400

    Take scripts out of Cmdr

--- src/shared/ServerScriptService/Commands/Registration.server.lua
+++ src/shared/ServerScriptService/Commands/Registration.server.lua
@@ -2,7 +2,9 @@ local ServerScriptService = game:GetService("ServerScriptService")
 
 local commandsFolder = ServerScriptService.Shared.Commands
 local hooksFolder = commandsFolder.Cmdr.Hooks
+local customCommandsFolder = commandsFolder.Custom
 
 local Cmdr = require(commandsFolder.Cmdr)
 
 Cmdr:RegisterHooksIn(hooksFolder)
+Cmdr:RegisterCommandsIn(customCommandsFolder)
