86f1852882165b025c4708e515ee6c008af612b6
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-04-30 04:14:51 -0400

    Add diagnistic disable

--- src/Shared/ReplicatedFirst/Utility/Loadstring/FiOne.lua
+++ src/Shared/ReplicatedFirst/Utility/Loadstring/FiOne.lua
@@ -15,7 +15,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]] --
----@diagnostic disable-next-line
 local bit = bit or bit32 or require('bit')
 
 if not table.create then function table.create(_) return {} end end
