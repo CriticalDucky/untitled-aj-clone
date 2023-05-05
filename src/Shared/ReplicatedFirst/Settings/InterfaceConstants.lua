48225f99cbb44361cd2d6c27976007b8002ae544
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-05-05 16:53:50 -0400

    Add floating button colors

--- src/Shared/ReplicatedFirst/Settings/InterfaceConstants.lua
+++ src/Shared/ReplicatedFirst/Settings/InterfaceConstants.lua
@@ -9,6 +9,10 @@ return {
 
 		buttonBluePrimary = Color3.fromHex "#53A3BC",
 		buttonBlueSecondary = Color3.fromHex "#3D788A",
+
+		floatingIconButtonNormal = Color3.new(0, 0.384313, 0.549019),
+		floatingIconButtonHover = Color3.new(0, 0.470588, 0.666667),
+		floatingIconButtonPress = Color3.new(0, 0.568627, 0.811764),
 	},
 
 	fonts = {
