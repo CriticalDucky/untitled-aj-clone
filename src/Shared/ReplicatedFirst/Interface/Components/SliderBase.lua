fe0f654fe1da7a1f059aa80aba42a86c13577406
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-05-18 06:28:51 -0400

    Fix bugs

--- src/Shared/ReplicatedFirst/Interface/Components/SliderBase.lua
+++ src/Shared/ReplicatedFirst/Interface/Components/SliderBase.lua
@@ -10,6 +10,7 @@ local MOVEMENT_INPUTS = {
 }
 
 --#region Imports
+local Players = game:GetService "Players"
 local ReplicatedFirst = game:GetService "ReplicatedFirst"
 local ReplicatedStorage = game:GetService "ReplicatedStorage"
 local UserInputService = game:GetService "UserInputService"
@@ -20,6 +21,7 @@ local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
 local replicatedFirstVendor = ReplicatedFirst:WaitForChild "Vendor"
 local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
 local componentsFolder = replicatedFirstShared:WaitForChild("Interface"):WaitForChild "Components"
+local playerGui = Players.LocalPlayer:WaitForChild "PlayerGui"
 
 local buttonInput = require(componentsFolder:WaitForChild "ButtonInput")
 
@@ -61,6 +63,7 @@ export type Props = {
 	ZIndex: CanBeState<number>?,
 
 	-- Custom props
+	BackgroundInputShrink: CanBeState<Vector2>?, -- This size will be subtracted from the background input's size to create a smaller hitbox/draggable area.
 	BackgroundBody: CanBeState<GuiObject>?, -- The actual design of the slider background
 	SliderBody: CanBeState<GuiObject>?, -- The actual design of the slider
 	SliderSize: CanBeState<UDim2>?, -- The size of the slider, which centers with the input body
@@ -87,18 +90,29 @@ local function Component(props: Props)
 
 	local backgroundAbsolutePosition = Value(Vector2.new(0, 0))
 	local backgroundAbsoluteSize = Value(Vector2.new(0, 0))
+	local backgroundAbsoluteSizeComputed = Computed(
+		function(use) return use(backgroundAbsoluteSize) - (use(props.BackgroundInputShrink) or Vector2.new(0, 0)) end
+	)
+	local backgroundAbsolutePositionComputed = Computed(
+		function(use)
+			return use(backgroundAbsolutePosition) + ((use(props.BackgroundInputShrink) or Vector2.new(0, 0)) / 2)
+		end
+	)
 
 	local function updateProgress(input: InputObject)
 		mousePosition = Vector2.new(input.Position.X, input.Position.Y)
 
 		if MOVEMENT_INPUTS[input.UserInputType] and selected then
-			local backgroundAbsolutePosition = peek(backgroundAbsolutePosition)
-			local backgroundAbsoluteSize = peek(backgroundAbsoluteSize)
+			local backgroundPosition = peek(backgroundAbsolutePositionComputed)
+			local backgroundSize = peek(backgroundAbsoluteSizeComputed)
 
-			local offset = if selected == "Slider" then (sliderInputOffset + peek(sliderAbsoluteSize)/2) else Vector2.new(0,0)
+			local offset = if selected == "Slider"
+				then (sliderInputOffset + peek(sliderAbsoluteSize) / 2)
+				else Vector2.new(0, 0)
 
-			local relativePosition = (mousePosition + offset - backgroundAbsolutePosition) / backgroundAbsoluteSize
-			local clampedRelativePosition = Vector2.new(math.clamp(relativePosition.X, 0, 1), math.clamp(relativePosition.Y, 0, 1))
+			local relativePosition = (mousePosition + offset - backgroundPosition) / backgroundSize
+			local clampedRelativePosition =
+				Vector2.new(math.clamp(relativePosition.X, 0, 1), math.clamp(relativePosition.Y, 0, 1))
 
 			-- Update the slider position
 			props.InputProgressChanged(math.clamp(clampedRelativePosition.X, 0, 1))
@@ -134,8 +148,13 @@ local function Component(props: Props)
 		[Children] = {
 			buttonInput {
 				Name = "BackgroundInput",
-				Size = UDim2.fromScale(1, 1),
+				Size = Computed(function(use)
+					local inputShrinkVector2 = use(props.BackgroundInputShrink) or Vector2.new(0, 0)
+					return UDim2.fromScale(1, 1) - UDim2.fromOffset(inputShrinkVector2.X, inputShrinkVector2.Y)
+				end),
 				ZIndex = 10000,
+				AnchorPoint = Vector2.new(0.5, 0.5),
+				Position = UDim2.fromScale(0.5, 0.5),
 
 				Disabled = props.Disabled,
 
@@ -153,41 +172,55 @@ local function Component(props: Props)
 			props.BackgroundBody,
 
 			New "Frame" {
-				Name = "SliderFrame",
+				Name = "SliderContainer",
 				BackgroundTransparency = 1,
-				Size = props.SliderSize or UDim2.fromOffset(20, 20),
-				AnchorPoint = Vector2.new(0.5, 0.5),
-				Position = Computed(function(use)
-					return UDim2.fromScale(use(props.ProgressAlpha) or 0, 0.5)
+				Size = Computed(function(use)
+					local BackgroundInputShrink = use(props.BackgroundInputShrink) or Vector2.new(0, 0)
+					return UDim2.fromScale(1, 1) - UDim2.fromOffset(BackgroundInputShrink.X, BackgroundInputShrink.Y)
 				end),
+				AnchorPoint = Vector2.new(0.5, 0.5),
+				Position = UDim2.fromScale(0.5, 0.5),
 				ZIndex = 20000,
 
-				[Out "AbsolutePosition"] = sliderAbsolutePosition,
-				[Out "AbsoluteSize"] = sliderAbsoluteSize,
-
 				[Children] = {
-					buttonInput {
-						Name = "SliderInput",
-						Size = UDim2.fromScale(1, 1),
-						ZIndex = 10000,
-	
-						Disabled = props.Disabled,
-	
-						InputBegan = function(input: InputObject)
-							if SELECT_INPUTS[input.UserInputType] then
-								selected = "Slider"
-								sliderInputOffset = peek(sliderAbsolutePosition) - Vector2.new(input.Position.X, input.Position.Y)
-								updateProgress(input)
-							end
-						end,
-	
-						isHeldDown = props.isHeldDownSlider,
-						isHovering = props.isHoveringSlider,
-					},
+					New "Frame" {
+						Name = "SliderFrame",
+						BackgroundTransparency = 1,
+						Size = props.SliderSize or UDim2.fromOffset(20, 20),
+						AnchorPoint = Vector2.new(0.5, 0.5),
+						Position = Computed(
+							function(use) return UDim2.fromScale(use(props.ProgressAlpha) or 0, 0.5) end
+						),
 
-					props.SliderBody,
+						[Out "AbsolutePosition"] = sliderAbsolutePosition,
+						[Out "AbsoluteSize"] = sliderAbsoluteSize,
+
+						[Children] = {
+							buttonInput {
+								Name = "SliderInput",
+								Size = UDim2.fromScale(1, 1),
+								ZIndex = 10000,
+
+								Disabled = props.Disabled,
+
+								InputBegan = function(input: InputObject)
+									if SELECT_INPUTS[input.UserInputType] then
+										selected = "Slider"
+										sliderInputOffset = peek(sliderAbsolutePosition)
+											- Vector2.new(math.round(input.Position.X), math.round(input.Position.Y))
+										updateProgress(input)
+									end
+								end,
+
+								isHeldDown = props.isHeldDownSlider,
+								isHovering = props.isHoveringSlider,
+							},
+
+							props.SliderBody,
+						},
+					},
 				},
-			}
+			},
 		},
 	}
 
