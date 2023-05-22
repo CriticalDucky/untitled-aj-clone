4c94b88013167650320e3b9b76fcb092cfe8cf66
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-05-22 19:51:31 -0400

    Add preliminary settings menu

---
+++ src/Shared/ReplicatedFirst/Interface/Components/SettingsMenu.lua
@@ -0,0 +1,62 @@
+--#region Imports
+local ReplicatedFirst = game:GetService "ReplicatedFirst"
+local ReplicatedStorage = game:GetService "ReplicatedStorage"
+
+local replicatedStorageShared = ReplicatedStorage:WaitForChild "Shared"
+local replicatedFirstShared = ReplicatedFirst:WaitForChild "Shared"
+local replicatedFirstVendor = ReplicatedFirst:WaitForChild "Vendor"
+local utilityFolder = replicatedFirstShared:WaitForChild "Utility"
+local componentsFolder = replicatedFirstShared:WaitForChild("Interface"):WaitForChild "Components"
+
+-- Optional: Remove imports that you don't need
+local Fusion = require(replicatedFirstVendor:WaitForChild "Fusion")
+local New = Fusion.New
+local Hydrate = Fusion.Hydrate
+local Ref = Fusion.Ref
+local Children = Fusion.Children
+local Cleanup = Fusion.Cleanup
+local Out = Fusion.Out
+local OnEvent = Fusion.OnEvent
+local OnChange = Fusion.OnChange
+local Attribute = Fusion.Attribute
+local AttributeChange = Fusion.AttributeChange
+local AttributeOut = Fusion.AttributeOut
+local Value = Fusion.Value
+local Computed = Fusion.Computed
+local ForPairs = Fusion.ForPairs
+local ForKeys = Fusion.ForKeys
+local ForValues = Fusion.ForValues
+local Observer = Fusion.Observer
+local Tween = Fusion.Tween
+local Spring = Fusion.Spring
+local peek = Fusion.peek
+local cleanup = Fusion.cleanup
+local doNothing = Fusion.doNothing
+
+type CanBeState<T> = Fusion.CanBeState<T>
+-- #endregion
+
+export type Props = {
+	-- Default props
+	Name: CanBeState<string>?,
+	LayoutOrder: CanBeState<number>?,
+	Position: CanBeState<UDim2>?,
+	AnchorPoint: CanBeState<Vector2>?,
+	Size: CanBeState<UDim2>?,
+	AutomaticSize: CanBeState<Enum.AutomaticSize>?,
+	ZIndex: CanBeState<number>?,
+}
+
+--[[
+	This component creates a...
+
+	Example usage:
+	```lua
+		
+	```
+]]
+local function Component(props: Props)
+	
+end
+
+return Component
