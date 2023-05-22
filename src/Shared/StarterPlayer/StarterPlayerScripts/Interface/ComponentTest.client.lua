10970d5008459df5c5f84d25835a851a4b33ed43
CriticalDucky <96558282+CriticalDucky@users.noreply.github.com>
2023-05-22 19:51:48 -0400

    Add test and format

--- src/Shared/StarterPlayer/StarterPlayerScripts/Interface/ComponentTest.client.lua
+++ src/Shared/StarterPlayer/StarterPlayerScripts/Interface/ComponentTest.client.lua
@@ -43,101 +43,124 @@ local floatingIconButton = require(componentsFolder:WaitForChild "FloatingIconBu
 local sliderBase = require(componentsFolder:WaitForChild "SliderBase")
 local playerGui = game:GetService("Players").LocalPlayer:WaitForChild "PlayerGui"
 local bubbleSlider = require(componentsFolder:WaitForChild "BubbleSlider")
+local bubbleToggle = require(componentsFolder:WaitForChild "BubbleToggle")
+local outlinedMenu = require(componentsFolder:WaitForChild "OutlinedMenu")
 
 local disabled = Value(false)
 
 local progressAlpha = Value(0.5)
+local state = Value(false)
 
-local sliderFunction = function(value)
-	progressAlpha:set(value)
-end
+local sliderFunction = function(value) progressAlpha:set(value) end
 
--- task.spawn(function()
--- 	while true do
--- 		task.wait(3)
--- 		disabled:set(not peek(disabled))
--- 	end
--- end)
+task.spawn(function()
+	while true do
+		task.wait(3)
+		disabled:set(not peek(disabled))
+	end
+end)
 
 New "ScreenGui" {
 	Name = "TestGui",
 	Parent = playerGui,
 
 	[Children] = {
-		outlinedFrame {
-			Position = UDim2.new(0.5, 0, 0.5, 0),
-			Size = UDim2.new(0, 400, 0, 250),
+		-- outlinedFrame {
+		-- 	Position = UDim2.new(0.5, 0, 0.5, 0),
+		-- 	Size = UDim2.new(0, 400, 0, 250),
+		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+
+		-- 	OutlineColor = InterfaceConstants.colors.menuGreen1,
+		-- 	BackgroundColor = InterfaceConstants.colors.menuBackground,
+
+		-- 	PaddingTop = UDim.new(0, 20),
+
+		-- 	Children = {
+		-- 		-- bubbleButton {
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	SizeX = 50,
+		-- 		-- 	Text = "Hello world!",
+		-- 		-- 	OnClick = function() end,
+		-- 		-- 	Disabled = disabled,
+		-- 		-- },
+
+		-- 		-- bannerButton {
+		-- 		-- 	BorderColor = InterfaceConstants.colors.menuGreen1,
+		-- 		-- 	Size = UDim2.fromOffset(250, 150),
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	ZoomOnHover = true,
+		-- 		-- 	Image = "rbxassetid://3317811687",
+		-- 		-- },
+
+		-- 		-- floatingIconButton {
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 	Size = UDim2.fromOffset(48, 48),
+
+		-- 		-- 	Image = "rbxassetid://13370286402",
+		-- 		-- 	OutlineImage = "rbxassetid://13370286263"
+		-- 		-- }
+
+		-- 		-- sliderBase {
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 	Size = UDim2.fromOffset(200, 20),
+
+		-- 		-- 	BackgroundBody = New "Frame" {
+		-- 		-- 		AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 		Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 		Size = UDim2.fromScale(1, 1),
+
+		-- 		-- 		BackgroundColor3 = Color3.new(0.678431, 0.678431, 0.678431),
+		-- 		-- 	},
+		-- 		-- 	SliderBody = New "Frame" {
+		-- 		-- 		AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 		Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 		Size = UDim2.fromScale(1, 1),
+		-- 		-- 		BackgroundTransparency = 0.2,
+
+		-- 		-- 		BackgroundColor3 = Color3.new(1, 1, 1),
+		-- 		-- 	},
+		-- 		-- 	SliderSize = UDim2.fromOffset(24, 24),
+		-- 		-- 	ProgressAlpha = progressAlpha,
+		-- 		-- 	InputProgressChanged = sliderFunction,
+		-- 		-- 	BackgroundInputShrink = Vector2.new(32,8)
+		-- 		-- }
+
+		-- 		-- bubbleSlider {
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+		-- 		-- 	SizeX = UDim.new(0, 250),
+		-- 		-- 	Disabled = disabled,
+
+		-- 		-- 	ProgressAlpha = progressAlpha,
+		-- 		-- 	InputProgressChanged = sliderFunction,
+		-- 		-- }
+
+		-- 		-- bubbleToggle {
+		-- 		-- 	AnchorPoint = Vector2.new(0.5, 0.5),
+		-- 		-- 	Position = UDim2.fromScale(0.5, 0.5),
+
+		-- 		-- 	State = state,
+		-- 		-- 	OnClick = function() state:set(not peek(state)) end,
+		-- 		-- 	Disabled = disabled,
+		-- 		-- },
+
+				
+		-- 	},
+		-- },
+
+		outlinedMenu {
 			AnchorPoint = Vector2.new(0.5, 0.5),
+			Position = UDim2.fromScale(0.5, 0.5),
+			Size = UDim2.fromOffset(400, 350),
 
-			OutlineColor = InterfaceConstants.colors.menuGreen1,
-			BackgroundColor = InterfaceConstants.colors.menuBackground,
-
-			PaddingTop = UDim.new(0, 20),
-
-			Children = {
-				-- bubbleButton {
-				-- 	Position = UDim2.fromScale(0.5, 0.5),
-				-- 	AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 	SizeX = 50,
-				-- 	Text = "Hello world!",
-				-- 	OnClick = function() end,
-				-- 	Disabled = disabled,
-				-- },
-
-				-- bannerButton {
-				-- 	BorderColor = InterfaceConstants.colors.menuGreen1,
-				-- 	Size = UDim2.fromOffset(250, 150),
-				-- 	Position = UDim2.fromScale(0.5, 0.5),
-				-- 	AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 	ZoomOnHover = true,
-				-- 	Image = "rbxassetid://3317811687",
-				-- },
-
-				-- floatingIconButton {
-				-- 	AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 	Position = UDim2.fromScale(0.5, 0.5),
-				-- 	Size = UDim2.fromOffset(48, 48),
-
-				-- 	Image = "rbxassetid://13370286402",
-				-- 	OutlineImage = "rbxassetid://13370286263"
-				-- }
-
-				-- sliderBase {
-				-- 	AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 	Position = UDim2.fromScale(0.5, 0.5),
-				-- 	Size = UDim2.fromOffset(200, 20),
-
-				-- 	BackgroundBody = New "Frame" {
-				-- 		AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 		Position = UDim2.fromScale(0.5, 0.5),
-				-- 		Size = UDim2.fromScale(1, 1),
-
-				-- 		BackgroundColor3 = Color3.new(0.678431, 0.678431, 0.678431),
-				-- 	},
-				-- 	SliderBody = New "Frame" {
-				-- 		AnchorPoint = Vector2.new(0.5, 0.5),
-				-- 		Position = UDim2.fromScale(0.5, 0.5),
-				-- 		Size = UDim2.fromScale(1, 1),
-				-- 		BackgroundTransparency = 0.2,
-
-				-- 		BackgroundColor3 = Color3.new(1, 1, 1),
-				-- 	},
-				-- 	SliderSize = UDim2.fromOffset(24, 24),
-				-- 	ProgressAlpha = progressAlpha,
-				-- 	InputProgressChanged = sliderFunction,
-				-- 	BackgroundInputShrink = Vector2.new(32,8)
-				-- }
-
-				bubbleSlider {
-					AnchorPoint = Vector2.new(0.5, 0.5),
-					Position = UDim2.fromScale(0.5, 0.5),
-					SizeX = UDim.new(0, 250),
-					Disabled = disabled,
-					
-					ProgressAlpha = progressAlpha,
-					InputProgressChanged = sliderFunction,
-				}
-			},
-		},
+			TitleText = "LETS GO!",
+			onExitButtonClicked = function()
+				print("Exit button clicked!")
+			end
+		}
 	},
 }
